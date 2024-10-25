1678334f215ae46bec8eb8c9d9f52c29a51e8681
bluecube97 <jph04071@gmail.com>
2024-10-26 05:07:05 +0900

    주식 투자
    https://www.acmicpc.net/problem/13416

--- bj_24_10_25/bj_24_10_25/Program.cs
+++ bj_24_10_25/bj_24_10_25/Program.cs
@@ -1,3 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main()
+    {
+        int T = int.Parse(Console.ReadLine()); // 테스트 케이스의 개수
 
-Console.WriteLine("Hello, World!");
+        for (int t = 0; t < T; t++)
+        {
+            int N = int.Parse(Console.ReadLine()); // 주식 데이터의 일수
+            int maxProfit = 0;
+
+            for (int day = 0; day < N; day++)
+            {
+                // 각 회사의 주식 손익 입력
+                string[] profits = Console.ReadLine().Split();
+                int profitA = int.Parse(profits[0]);
+                int profitB = int.Parse(profits[1]);
+                int profitC = int.Parse(profits[2]);
+
+                // 그날 가능한 최대 이익을 추가 (0을 넘지 않으면 주식을 사지 않음)
+                int bestProfit = Math.Max(0, Math.Max(profitA, Math.Max(profitB, profitC)));
+                maxProfit += bestProfit;
+            }
+
+            Console.WriteLine(maxProfit);
+        }
+    }
+}
