a9f0c08a36b46e8a54388c088636199fcb22d719
bluecube97 <jph04071@gmail.com>
2024-10-26 05:16:24 +0900

    빙고 막기
    https://www.acmicpc.net/problem/32246

---
+++ bj_24_10_25_1/bj_24_10_25_1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(N == 2 ? 3 : N);
+    }
+}
