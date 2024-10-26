765045eba55928df345e26578d1be348700eddb7
bluecube97 <jph04071@gmail.com>
2024-10-26 20:40:07 +0900

    전투 드로이드 가격
    https://www.acmicpc.net/problem/5361

---
+++ bj_24_10_26/bj_24_10_26/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int testCase = int.Parse(Console.ReadLine());
+
+        double blasterRifle = 350.34;
+        double visionSensor = 230.90;
+        double hearingSensor = 190.55;
+        double arm = 125.30;
+        double leg = 180.90;
+
+        for (int i = 0; i < testCase; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int blasterRifleCount = int.Parse(input[0]);
+            int visionSensorCount = int.Parse(input[1]);
+            int hearingSensorCount = int.Parse(input[2]);
+            int armCount = int.Parse(input[3]);
+            int legCount = int.Parse(input[4]);
+
+            double total = blasterRifle * blasterRifleCount + visionSensor * visionSensorCount +
+                           hearingSensor * hearingSensorCount + arm * armCount + leg * legCount;
+            Console.WriteLine("$" + total.ToString("F2"));
+        }
+    }
+}
