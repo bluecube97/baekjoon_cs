ad7fbbf082bbb384b6d5fdbade6e03040d38d6fe
bluecube97 <jph04071@gmail.com>
2024-10-28 05:24:06 +0900

    주사위 게임
    https://www.acmicpc.net/problem/10103

---
+++ bj_24_10_27/bj_24_10_27/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int round = int.Parse(Console.ReadLine());
+
+        int changtotal = 100;
+        int sangtotal = 100;
+
+        for (int i = 0; i < round; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int chang = int.Parse(input[0]);
+            int sang = int.Parse(input[1]);
+
+            if (chang > sang)
+            {
+                sangtotal -= chang;
+            }
+            else if (chang < sang)
+            {
+                changtotal -= sang;
+            }
+        }
+
+        Console.WriteLine(changtotal);
+        Console.WriteLine(sangtotal);
+    }
+}
