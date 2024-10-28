d8002f447cee0dd5492c2a0556c3b0eadefed9dd
bluecube97 <jph04071@gmail.com>
2024-10-28 20:49:16 +0900

    전자레인지
    https://www.acmicpc.net/problem/10162

---
+++ bj_24_10_28/bj_24_10_28/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        int A = 0;
+        int B = 0;
+        int C = 0;
+
+        if (T % 300 != 0 && T % 60 != 0 && T % 10 != 0)
+        {
+            Console.WriteLine(-1);
+        }
+        else
+        {
+            if (T >= 300)
+            {
+                A = T / 300;
+                T %= 300;
+            }
+
+            if (T >= 60)
+            {
+                B = T / 60;
+                T %= 60;
+            }
+
+            if (T >= 10)
+            {
+                C = T / 10;
+                T %= 10;
+            }
+            Console.WriteLine(A + " " + B + " " + C);
+        }
+    }
+}
