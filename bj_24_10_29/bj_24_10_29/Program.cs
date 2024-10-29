e313ae275401f4fa62f1ed4c5b9883fa1da80cef
bluecube97 <jph04071@gmail.com>
2024-10-29 23:33:06 +0900

    헤라클레스와 히드라
    https://www.acmicpc.net/problem/10205

---
+++ bj_24_10_29/bj_24_10_29/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int k = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < k; i++)
+        {
+            int head = int.Parse(Console.ReadLine());
+            string behavior = Console.ReadLine();
+            int behaivorCount = behavior.Length;
+
+            for (int j = 0; j < behaivorCount; j++)
+            {
+                if (head == 0)
+                {
+                    break;
+                }
+                if (behavior[j] == 'c')
+                {
+                    head++;
+                }
+                else if (behavior[j] == 'b')
+                {
+                    head--;
+                }
+            }
+            Console.WriteLine("Data Set " + (i + 1) + ":");
+            Console.WriteLine(head);
+            Console.WriteLine();
+        }
+    }
+}
