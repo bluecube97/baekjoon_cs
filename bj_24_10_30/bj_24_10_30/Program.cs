222cd869e44e5f4115acb2038ec3c06ac8e62c91
bluecube97 <jph04071@gmail.com>
2024-10-31 00:21:50 +0900

    카드 게임
    https://www.acmicpc.net/problem/5522

---
+++ bj_24_10_30/bj_24_10_30/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int card = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            card += int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(card);
+    }
+}
