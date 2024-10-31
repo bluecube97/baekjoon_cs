e7cad44aadb8d7f910545fa36eb4784e584cd5c4
bluecube97 <jph04071@gmail.com>
2024-10-31 17:01:03 +0900

    Rust Study
    https://www.acmicpc.net/problem/30033

---
+++ bj_24_10_31/bj_24_10_31/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int day = int.Parse(Console.ReadLine());
+        int success = 0;
+
+        string[] plan = Console.ReadLine().Split(' ');
+        string[] study = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < day; i++)
+        {
+            if (int.Parse(plan[i]) <= int.Parse(study[i]))
+            {
+                success++;
+            }
+        }
+
+        Console.WriteLine(success);
+    }
+}
