b50700a7b01fb306d3cc66caa6b5df97fc0a052f
bluecube97 <jph04071@gmail.com>
2024-11-01 15:05:27 +0900

    별 찍기 - 16
    https://www.acmicpc.net/problem/10991

---
+++ bj_24_11_01/bj_24_11_01/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int star = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < star; i++)
+        {
+            for (int j = star - i - 1; j > 0; j--)
+            {
+                Console.Write(" ");
+            }
+            Console.Write("*");
+
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(" *");
+            }
+
+            if (i == star - 1) break;
+            Console.WriteLine();
+        }
+    }
+}
