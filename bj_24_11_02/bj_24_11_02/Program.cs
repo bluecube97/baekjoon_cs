f3e991c24917f61a7bf0e9a257e15926364d9b20
bluecube97 <jph04071@gmail.com>
2024-11-03 02:44:47 +0900

    기숙사 바닥
    https://www.acmicpc.net/problem/2858

---
+++ bj_24_11_02/bj_24_11_02/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int R = int.Parse(input[0]);
+        int B = int.Parse(input[1]);
+
+        (int L, int W) = FindRoomSize(R, B);
+
+        Console.WriteLine($"{L} {W}");
+    }
+
+    static (int, int) FindRoomSize(int R, int B)
+    {
+        int totalTiles = R + B;
+
+        for (int L = 3; L <= totalTiles; L++)
+        {
+            if (totalTiles % L == 0)
+            {
+                int W = totalTiles / L;
+
+                if (2 * (L + W) - 4 == R && (L - 2) * (W - 2) == B)
+                {
+                    return L >= W ? (L, W) : (W, L);
+                }
+            }
+        }
+
+        return (0, 0);
+    }
+}
