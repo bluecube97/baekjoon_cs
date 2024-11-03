0ce0fc3c10091bf8919cdd92141688f623b9d5b0
bluecube97 <jph04071@gmail.com>
2024-11-03 23:56:11 +0900

    개표
    https://www.acmicpc.net/problem/10102

---
+++ bj_24_11_03/bj_24_11_03/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int V = int.Parse(Console.ReadLine());
+        string vote = Console.ReadLine();
+
+        int A = 0;
+        int B = 0;
+
+        foreach (char c in vote)
+        {
+            if (c == 'A')
+            {
+                A++;
+            }
+            else
+            {
+                B++;
+            }
+        }
+
+        if (A > B)
+        {
+            Console.WriteLine("A");
+        }
+        else if (A < B)
+        {
+            Console.WriteLine("B");
+        }
+        else
+        {
+            Console.WriteLine("Tie");
+        }
+    }
+}
