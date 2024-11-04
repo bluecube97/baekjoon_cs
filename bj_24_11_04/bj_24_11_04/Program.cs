713f5319aef8c3f3f62dccf81f3c7b00a416b8f4
bluecube97 <jph04071@gmail.com>
2024-11-04 20:21:10 +0900

    할로윈의 사탕
    https://www.acmicpc.net/problem/10178

---
+++ bj_24_11_04/bj_24_11_04/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int testCase = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCase; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int c = int.Parse(input[0]);
+            int v = int.Parse(input[1]);
+
+            int yourCandy = c / v;
+            int remainCandy = c % v;
+
+            Console.WriteLine("You get {0} piece(s) and your dad gets {1} piece(s).", yourCandy, remainCandy);
+        }
+    }
+}
