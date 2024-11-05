c73548d738d7f43cf278a57b59bb20ec8a12a56e
bluecube97 <jph04071@gmail.com>
2024-11-05 14:40:38 +0900

    암호
    https://www.acmicpc.net/problem/1718

---
+++ bj_24_11_05/bj_24_11_05/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        string key = Console.ReadLine();
+
+        string result = "";
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == ' ')
+            {
+                result += ' ';
+                continue;
+            }
+
+            int keyIndex = i % key.Length;
+            int c = str[i] - key[keyIndex] + 'a' - 1;
+            if (c < 'a') c += 26;
+            result += (char)c;
+        }
+
+        Console.WriteLine(result);
+    }
+}
