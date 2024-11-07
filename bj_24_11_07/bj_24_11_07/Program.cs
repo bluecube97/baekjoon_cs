5f698fcc93b344ac87b46111985a5be71a1170b4
bluecube97 <jph04071@gmail.com>
2024-11-08 01:47:57 +0900

    Sort 마스터 배지훈
    https://www.acmicpc.net/problem/17263

---
+++ bj_24_11_07/bj_24_11_07/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] arr = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+        PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+
+        foreach (int i in arr)
+        {
+            pq.Enqueue(i, -i);
+        }
+
+        Console.WriteLine(pq.Dequeue());
+    }
+}
