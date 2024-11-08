9b8a6b24abddc4f3e9e1b9f9efc022d99aa38660
bluecube97 <jph04071@gmail.com>
2024-11-08 17:57:58 +0900

    종이접기
    https://www.acmicpc.net/problem/16504

---
+++ bj_24_11_08/bj_24_11_08/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        long sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            long[] arr = Console.ReadLine().Split().Select(long.Parse).ToArray();
+            foreach (var num in arr)
+            {
+                sum += num;
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
