b835e3df2a41a1c8b6c01faf055089d9a0ad9cd1
bluecube97 <jph04071@gmail.com>
2024-11-09 22:07:40 +0900

    추첨상 사수 대작전! (Easy)
    https://www.acmicpc.net/problem/20410

---
+++ bj_24_11_09/bj_24_11_09/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int m = input[0];
+        int seed = input[1];
+        int x1 = input[2];
+        int x2 = input[3];
+
+        for (int a = 0; a < m; a++)
+        {
+            for (int c = 0; c < m; c++)
+            {
+                if (x2 == (a * x1 + c) % m && x1 == (a * seed + c) % m)
+                {
+                    Console.WriteLine($"{a} {c}");
+                    return;
+                }
+            }
+        }
+    }
+}
