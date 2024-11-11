95af818702f513d717156cff76c0d16e0b2c5b42
bluecube97 <jph04071@gmail.com>
2024-11-11 23:22:33 +0900

    감정이입
    https://www.acmicpc.net/problem/14623

---
+++ bj_24_11_11/bj_24_11_11/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string B1 = Console.ReadLine();
+        string B2 = Console.ReadLine();
+
+        long num1 = Convert.ToInt64(B1, 2);
+        long num2 = Convert.ToInt64(B2, 2);
+
+        long product = num1 * num2;
+
+        string result = Convert.ToString(product, 2);
+
+        Console.WriteLine(result);
+    }
+}
