6046e2a3fd1e903b3ea21859a4a34873d5a33670
bluecube97 <jph04071@gmail.com>
2024-11-12 16:24:42 +0900

    2021은 무엇이 특별할까?
    https://www.acmicpc.net/problem/24039

---
+++ bj_24_11_12/bj_24_11_12/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int result = NextYear(N);
+        Console.WriteLine(result);
+    }
+
+    static int NextYear(int N)
+    {
+        List<int> primes = GeneratePrimes(10000);
+        for (int i = 0; i < primes.Count - 1; i++)
+        {
+            int specialYear = primes[i] * primes[i + 1];
+            if (specialYear > N)
+            {
+                return specialYear;
+            }
+        }
+        return -1;
+    }
+
+    static List<int> GeneratePrimes(int limit)
+    {
+        bool[] isPrime = new bool[limit + 1];
+        for (int i = 2; i <= limit; i++)
+        {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= limit; i++)
+        {
+            if (isPrime[i])
+            {
+                for (int j = i * i; j <= limit; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        List<int> primes = new List<int>();
+        for (int i = 2; i <= limit; i++)
+        {
+            if (isPrime[i])
+            {
+                primes.Add(i);
+            }
+        }
+
+        return primes;
+    }
+}
