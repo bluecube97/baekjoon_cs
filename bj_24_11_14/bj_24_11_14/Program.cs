2e38758bcb5e5dd06e6fdeecd4016cae04aa5908
bluecube97 <jph04071@gmail.com>
2024-11-14 21:22:05 +0900

    삼각형
    https://www.acmicpc.net/problem/29751

---
+++ bj_24_11_14/bj_24_11_14/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        float w = input[0];
+        float h = input[1];
+
+        float dimension = w * h / 2;
+        Console.WriteLine($"{dimension:F1}");;
+    }
+}
