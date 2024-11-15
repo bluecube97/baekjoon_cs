9f77a8f308e6eaa314ebb57b740bfe670b9eff18
bluecube97 <jph04071@gmail.com>
2024-11-15 14:44:08 +0900

    삼각 무늬 - 1
    https://www.acmicpc.net/problem/11320

---
+++ bj_24_11_15/bj_24_11_15/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int testCase = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCase; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int a = input[0];
+            int b = input[1];
+
+            int n = a / b;
+            int result = 0;
+
+            for (int j = 1; j <= n; j++)
+            {
+                result += 2 * j - 1;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
