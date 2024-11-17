cc56975c359c675735f69ebf245959c1bb855694
bluecube97 <jph04071@gmail.com>
2024-11-17 21:53:31 +0900

    재귀의 귀재
    https://www.acmicpc.net/problem/25501

---
+++ bj_24_11_17/bj_24_11_17/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static int recursionCount = 0;
+
+    static void Main(string[] args)
+    {
+        int testCase = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCase; i++)
+        {
+            recursionCount = 0;
+            string input = Console.ReadLine();
+            Console.Write(isPalindrome(input));
+            Console.WriteLine(" " + recursionCount);
+        }
+    }
+    
+    static int recursion(string s, int l, int r)
+    {
+        recursionCount++;
+        if (l >= r)
+        {
+            return 1;
+        }
+        else if (s[l] != s[r])
+        {
+            return 0;
+        }
+        else
+        {
+            return recursion(s, l + 1, r - 1);
+        }
+    }
+
+    static int isPalindrome(string s)
+    {
+        return recursion(s, 0, s.Length - 1);
+    }
+}
