0b77a6feee247543c04dd65afa60343eee9c0c0b
bluecube97 <jph04071@gmail.com>
2024-11-19 12:28:49 +0900

    쿠폰
    https://www.acmicpc.net/problem/10179

---
+++ bj_24_11_19/bj_24_11_19/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int testCase = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCase; i++)
+        {
+            double cost = double.Parse(Console.ReadLine());
+            double discountCost = cost * 8.0 / 10.0;
+            Console.WriteLine($"${discountCost:F2}");
+        }
+    }
+}
