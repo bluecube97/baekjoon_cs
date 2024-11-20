488f61b2787cb5d6bb1790f9a4daa12c89ce2894
bluecube97 <jph04071@gmail.com>
2024-11-20 16:05:19 +0900

    입실 관리
    https://www.acmicpc.net/problem/5524

---
+++ bj_24_11_20/bj_24_11_20/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < N; i++)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(input.ToLower());
+        }
+    }
+}
