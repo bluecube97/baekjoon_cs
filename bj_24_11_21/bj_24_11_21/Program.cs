913d23795005e19f09c23f8aaa43a52867a64385
bluecube97 <jph04071@gmail.com>
2024-11-21 13:04:48 +0900

    푸앙이와 종윤이
    https://www.acmicpc.net/problem/25591

---
+++ bj_24_11_21/bj_24_11_21/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] intInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int x = intInput[0];
+        int y = intInput[1];
+
+        int a = 100 - x;
+        int b = 100 - y;
+        int c = 100 - (a + b);
+        int d = a * b;
+        int q = d / 100;
+        int r = d % 100;
+
+        Console.WriteLine($"{a} {b} {c} {d} {q} {r}");
+        Console.WriteLine($"{c + q} {r}");
+    }
+}
