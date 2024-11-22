2e157e687208421f67005e544b78b3270a6f9e50
bluecube97 <jph04071@gmail.com>
2024-11-22 21:23:38 +0900

    코드마스터 2024
    https://www.acmicpc.net/problem/32215

---
+++ bj_24_11_22/bj_24_11_22/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+        int k = input[2];
+
+        Console.WriteLine(m * k + m);
+    }
+}
