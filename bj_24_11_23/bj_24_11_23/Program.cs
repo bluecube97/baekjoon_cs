217c575b3c94ae82d8e833998b66f5db288c9851
bluecube97 <jph04071@gmail.com>
2024-11-23 20:17:43 +0900

    카카오뷰 큐레이팅 효용성 분석
    https://www.acmicpc.net/problem/24544

---
+++ bj_24_11_23/bj_24_11_23/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] interest = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] myView = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int allSum = 0;
+        int mySum = 0;
+
+        foreach (var i in interest)
+        {
+            allSum += i;
+        }
+
+        int index = 0;
+
+        foreach (var i in interest)
+        {
+            if (myView[index] == 0)
+            {
+                mySum += i;
+            }
+            index++;
+        }
+
+        Console.WriteLine(allSum);
+        Console.WriteLine(mySum);
+    }
+}
