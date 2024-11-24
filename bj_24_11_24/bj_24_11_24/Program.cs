02e6617df977a49c01427cd330e8942271939bb1
bluecube97 <jph04071@gmail.com>
2024-11-24 14:51:54 +0900

    소가 길을 건너간 이유 1
    https://www.acmicpc.net/problem/14467

---
+++ bj_24_11_24/bj_24_11_24/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        int[] cow = new int[11];
+        bool[] isNewCow = Enumerable.Repeat(true, 11).ToArray();
+        int crossCount = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int num = input[0];
+            int location = input[1];
+
+            if (cow[num] != location && isNewCow[num] == false)
+            {
+                crossCount++;
+            }
+            else
+            {
+                isNewCow[num] = false;
+            }
+            cow[num] = location;
+        }
+
+        Console.WriteLine(crossCount);
+    }
+}
