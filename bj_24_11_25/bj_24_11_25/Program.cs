3dd3b09553d1f26c9ec6e052405926dc57e9ca2e
bluecube97 <jph04071@gmail.com>
2024-11-25 16:35:56 +0900

    부재중 전화
    https://www.acmicpc.net/problem/1333

---
+++ bj_24_11_25/bj_24_11_25/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int N = input[0];
+        int L = input[1];
+        int D = input[2];
+
+        int totalTime = N * L + (N - 1) * 5;
+
+        for (int t = 0; ; t += D)
+        {
+            bool isQuiet = true;
+
+            for (int i = 0; i < N; i++)
+            {
+                int songStart = i * (L + 5);
+                int songEnd = songStart + L;
+
+                if (t >= songStart && t < songEnd)
+                {
+                    isQuiet = false;
+                    break;
+                }
+            }
+
+            if (isQuiet)
+            {
+                Console.WriteLine(t);
+                return;
+            }
+
+            if (t > totalTime)
+            {
+                Console.WriteLine(t);
+                return;
+            }
+        }
+    }
+}
