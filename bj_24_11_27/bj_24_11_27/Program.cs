7b8e24582141667dddc7f98a9848c5c1b9da4950
bluecube97 <jph04071@gmail.com>
2024-11-27 20:13:36 +0900

    단순한 문제 (Small)
    https://www.acmicpc.net/problem/25494

---
+++ bj_24_11_27/bj_24_11_27/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+            int c = input[2];
+
+            int count = 0;
+
+            for (int x = 1; x <= a; x++)
+            {
+                for (int y = 1; y <= b; y++)
+                {
+                    for (int z = 1; z <= c; z++)
+                    {
+                        if (x % y == y % z && y % z == z % x)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
