9175eb05505aec1a5a6216d1e298c1740534910c
bluecube97 <jph04071@gmail.com>
2024-11-28 18:37:54 +0900

    악마의 제안
    https://www.acmicpc.net/problem/23972

---
+++ bj_24_11_28/bj_24_11_28/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        long[] input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+        long k = input[0];
+        long n = input[1];
+
+        if (n == 1)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        long x = (k * n + (n - 2)) / (n - 1);
+
+        Console.WriteLine(x);
+    }
+}
