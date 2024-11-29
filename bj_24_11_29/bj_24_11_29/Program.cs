5e29b098969d3ec2b69855eabc8df0afbd7b6bdc
bluecube97 <jph04071@gmail.com>
2024-11-29 20:29:45 +0900

    찬물 샤워
    https://www.acmicpc.net/problem/32216

---
+++ bj_24_11_29/bj_24_11_29/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int k = input[1];
+        int T = input[2];
+
+        int[] d = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int discomfortSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (T < k)
+            {
+                T = T + d[i] + Math.Abs(T - k);
+            }
+            else if (T > k)
+            {
+                T = T + d[i] - Math.Abs(T - k);
+            }
+            else
+            {
+                T = T + d[i];
+            }
+            discomfortSum += Math.Abs(T - k);
+        }
+
+        Console.WriteLine(discomfortSum);
+    }
+}
