7131e11f2bc6a3937c39d5fc9e38eda549ac4314
bluecube97 <jph04071@gmail.com>
2024-12-02 16:00:52 +0900

    Send me the money
    https://www.acmicpc.net/problem/15786

---
+++ bj_24_12_02/bj_24_12_02/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+
+        string pwHint = Console.ReadLine();
+
+        for (int i = 0; i < m; i++)
+        {
+            string pw = Console.ReadLine();
+            int index = 0;
+
+            foreach (var c in pw)
+            {
+                if (index != pwHint.Length && c == pwHint[index])
+                {
+                    index++;
+                }
+            }
+
+            if (index == pwHint.Length)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
