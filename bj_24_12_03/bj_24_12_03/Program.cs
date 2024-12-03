f6b26202a65cf7bca4f85346cb5d92b1842d4212
bluecube97 <jph04071@gmail.com>
2024-12-03 12:10:37 +0900

    준살 프로그래밍 대회
    https://www.acmicpc.net/problem/7513

---
+++ bj_24_12_03/bj_24_12_03/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int testCase = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCase; i++)
+        {
+            Console.WriteLine("Scenario #" + (i + 1) + ":");
+            int m = int.Parse(Console.ReadLine());
+            List<string> pwList = new List<string>();
+
+            for (int j = 0; j < m; j++)
+            {
+                pwList.Add(Console.ReadLine());
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < n; j++)
+            {
+                int[] pwHint = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                int k = pwHint[0];
+
+                for (int l = 1; l <= k; l++)
+                {
+                    Console.Write(pwList[pwHint[l]]);
+                }
+
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
