82bf309593d468ebce2da7a4fc711be183e05477
bluecube97 <jph04071@gmail.com>
2024-12-04 18:43:40 +0900

    MBTI
    https://www.acmicpc.net/problem/25640

---
+++ bj_24_12_04/bj_24_12_04/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string jinho = Console.ReadLine();
+        List<string> mbtiList = new List<string>();
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            mbtiList.Add(Console.ReadLine());
+        }
+
+        foreach (var str in mbtiList)
+        {
+            if (jinho == str)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
