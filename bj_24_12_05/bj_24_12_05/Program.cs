e8a4d7efc9148ce9a075f170bd4f90548e17c488
bluecube97 <jph04071@gmail.com>
2024-12-05 19:55:20 +0900

    수도요금
    https://www.acmicpc.net/problem/10707

---
+++ bj_24_12_05/bj_24_12_05/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        int x = p * a;
+        int y = p <= c ? b : b + (p - c) * d;
+
+        Console.WriteLine(Math.Min(x, y));
+    }
+}
