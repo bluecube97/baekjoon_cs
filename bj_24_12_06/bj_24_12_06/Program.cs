092cdfbedca0199ea7654b2099a52d8f2b5625d6
bluecube97 <jph04071@gmail.com>
2024-12-06 21:29:02 +0900

    TV 크기
    https://www.acmicpc.net/problem/1297

---
+++ bj_24_12_06/bj_24_12_06/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int d = input[0];
+        int h = input[1];
+        int w = input[2];
+
+        double d2 = Math.Sqrt(h * h + w * w);
+        double ratio = d / d2;
+
+        Console.WriteLine($"{(int)(h * ratio)} {(int)(w * ratio)}");
+    }
+}
