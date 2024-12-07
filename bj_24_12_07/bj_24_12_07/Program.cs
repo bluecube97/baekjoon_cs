29eab0e36246c98f3777c4f763847be44f369287
bluecube97 <jph04071@gmail.com>
2024-12-07 20:14:43 +0900

    알파벳 거리
    https://www.acmicpc.net/problem/5218

---
+++ bj_24_12_07/bj_24_12_07/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            string x = input[0];
+            string y = input[1];
+
+            int strLen = x.Length;
+            int[] distance = new int[strLen];
+
+            for (int j = 0; j < strLen; j++)
+            {
+                distance[j] = y[j] - x[j];
+                if (distance[j] < 0)
+                {
+                    distance[j] += 26;
+                }
+            }
+
+            Console.WriteLine("Distances: " + string.Join(" ", distance));
+        }
+    }
+}
