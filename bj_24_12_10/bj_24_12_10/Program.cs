62909d23e80e1187656c48c767d6690577e9815b
bluecube97 <jph04071@gmail.com>
2024-12-10 08:44:06 +0900

    좌표 정렬하기 2
    https://www.acmicpc.net/problem/11651

---
+++ bj_24_12_10/bj_24_12_10/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = new StreamReader(Console.OpenStandardInput());
+        var output = new StreamWriter(Console.OpenStandardOutput());
+        int n = int.Parse(input.ReadLine());
+        PriorityQueue<(int x, int y), (int y, int x)> pair = new PriorityQueue<(int x, int y), (int y, int x)>(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] line = input.ReadLine().Split();
+
+            int x = int.Parse(line[0]);
+            int y = int.Parse(line[1]);
+
+            pair.Enqueue((x, y), (y, x));
+        }
+
+        var result = new System.Text.StringBuilder();
+        for (int i = 0; i < n; i++)
+        {
+            (int x, int y) = pair.Dequeue();
+            result.AppendLine(x + " " + y);
+        }
+        output.Write(result.ToString());
+        output.Flush();
+    }
+}
