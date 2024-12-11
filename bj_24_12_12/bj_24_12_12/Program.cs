9c9e01a437ee186ef14d9bab27599c5431dd9cf8
bluecube97 <jph04071@gmail.com>
2024-12-12 00:49:11 +0900

    HI-ARC=?
    https://www.acmicpc.net/problem/28444

---
+++ bj_24_12_12/bj_24_12_12/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int h = input[0];
+        int i = input[1];
+        int a = input[2];
+        int r = input[3];
+        int c = input[4];
+
+        Console.WriteLine(h * i - a * r * c);
+    }
+}
