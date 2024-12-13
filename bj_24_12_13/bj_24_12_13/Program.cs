0995d6003116d92b7750620c4131d3db7932e321
bluecube97 <jph04071@gmail.com>
2024-12-13 11:59:04 +0900

    커트라인
    https://www.acmicpc.net/problem/25305

---
+++ bj_24_12_13/bj_24_12_13/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int k = input[1];
+
+        input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] human = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            human[i] = input[i];
+        }
+
+        Array.Sort(human);
+        Console.WriteLine(human[n - k]);
+    }
+}
