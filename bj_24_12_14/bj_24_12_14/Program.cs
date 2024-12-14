fefad3071aa8e60890a0fbe4b8a555280c493258
bluecube97 <jph04071@gmail.com>
2024-12-14 16:04:47 +0900

    가장 쉬운 문제를 찾는 문제
    https://www.acmicpc.net/problem/22966

---
+++ bj_24_12_14/bj_24_12_14/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        PriorityQueue<string, int> question = new PriorityQueue<string, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            string title = input[0];
+            int level = int.Parse(input[1]);
+
+            question.Enqueue(title, level);
+        }
+
+        Console.WriteLine(question.Dequeue());
+    }
+}
