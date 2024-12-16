51500ae01331549495f448f7210aa4e8654801b3
bluecube97 <jph04071@gmail.com>
2024-12-16 09:21:54 +0900

    호텔 방 번호
    https://www.acmicpc.net/problem/5671

---
+++ bj_24_12_16/bj_24_12_16/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var inputQuestion = new StreamReader(Console.OpenStandardInput());
+        while (inputQuestion.Peek() != -1)
+        {
+            string[] input = inputQuestion.ReadLine().Split();
+
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+            int roomCount = m - n + 1;
+
+            for (int i = n; i <= m; i++)
+            {
+                bool[] test = new bool[10];
+                string roomNumber = i.ToString();
+
+                foreach (var c in roomNumber)
+                {
+                    if (test[int.Parse(c.ToString())])
+                    {
+                        roomCount--;
+                        break;
+                    }
+
+                    test[int.Parse(c.ToString())] = true;
+                }
+            }
+
+            Console.WriteLine(roomCount);
+        }
+    }
+}
