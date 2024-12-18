17e57d7d291e37b9d6d2a1092b2ed2735ac86b90
bluecube97 <jph04071@gmail.com>
2024-12-18 19:33:34 +0900

    모음의 개수
    https://www.acmicpc.net/problem/10987

---
+++ bj_24_12_18/bj_24_12_18/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var count = Console.ReadLine().Count(c => c is 'a' or 'e' or 'i' or 'o' or 'u');
+        Console.WriteLine(count);
+    }
+}
