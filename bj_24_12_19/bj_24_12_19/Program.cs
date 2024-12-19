33f529cfaf0b17df4ec388486dc19ab4fed89a33
bluecube97 <jph04071@gmail.com>
2024-12-19 11:44:54 +0900

    나이 계산하기
    https://www.acmicpc.net/problem/16199

---
+++ bj_24_12_19/bj_24_12_19/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int birthYear = input[0];
+        int birthMonth = input[1];
+        int birthDay = input[2];
+
+        input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int currentYear = input[0];
+        int currentMonth = input[1];
+        int currentDay = input[2];
+
+        int manAge = currentYear - birthYear;
+        if (currentMonth < birthMonth || (currentMonth == birthMonth && currentDay < birthDay))
+        {
+            manAge--;
+        }
+
+        int koreaAge = currentYear - birthYear + 1;
+        int yearAge = currentYear - birthYear;
+
+        Console.WriteLine(manAge);
+        Console.WriteLine(koreaAge);
+        Console.WriteLine(yearAge);
+    }
+}
