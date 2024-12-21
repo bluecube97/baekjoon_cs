5cbb862f3b7325cfadc683e282c992bd41cf6d4d
bluecube97 <jph04071@gmail.com>
2024-12-21 17:05:30 +0900

    풀만한문제
    https://www.acmicpc.net/problem/29716

---
+++ bj_24_12_21/bj_24_12_21/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int j = input[0];
+        int n = input[1];
+        int solvableQuestion = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            string question = Console.ReadLine();
+            int questionSize = 0;
+
+            foreach (var c in question)
+            {
+                switch (c)
+                {
+                    case ' ':
+                        questionSize++;
+                        break;
+                    case >= 'a' and <= 'z':
+                        questionSize += 2;
+                        break;
+                    case >= 'A' and <= 'Z':
+                        questionSize += 4;
+                        break;
+                    case >= '0' and <= '9':
+                        questionSize += 2;
+                        break;
+                }
+            }
+
+            if (questionSize <= j)
+            {
+                solvableQuestion++;
+            }
+        }
+
+        Console.WriteLine(solvableQuestion);
+    }
+}
