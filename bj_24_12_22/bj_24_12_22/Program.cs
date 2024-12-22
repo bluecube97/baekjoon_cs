f97c59358bab1eb2c285249eac7c0baa2fbab91a
bluecube97 <jph04071@gmail.com>
2024-12-22 14:32:23 +0900

    하늘과 핑크
    https://www.acmicpc.net/problem/31473

---
+++ bj_24_12_22/bj_24_12_22/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] pink = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] sky = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int pinkSum = pink.Sum();
+        int skySum = sky.Sum();
+
+        Console.WriteLine(skySum + " " + pinkSum);
+    }
+}
