1b2da603958bdcea33cb4fb6be991a7367174f39
bluecube97 <jph04071@gmail.com>
2024-12-27 14:36:19 +0900

    등차수열에서 항 번호 찾기
    https://www.acmicpc.net/problem/14913

---
+++ bj_24_12_27/bj_24_12_27/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int a = input[0];
+        int d = input[1];
+        int k = input[2];
+
+        if ((k - a) % d != 0 || (k - a) / d < 0)
+        {
+            Console.WriteLine("X");
+            return;
+        }
+
+        Console.WriteLine((k - a) / d + 1);
+    }
+}
