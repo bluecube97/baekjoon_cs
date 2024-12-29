5e10f44744ddf5e1d3927eb05b3608b4a38d5882
bluecube97 <jph04071@gmail.com>
2024-12-29 18:56:02 +0900

    트럭 주차
    https://www.acmicpc.net/problem/2979

---
+++ bj_24_12_29/bj_24_12_29/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] cost = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] timeLine = new int[101];
+
+        for (int i = 0; i < 3; i++)
+        {
+            int[] time = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int t = time[0]; t < time[1]; t++)
+            {
+                timeLine[t]++;
+            }
+        }
+
+        int totalCost = 0;
+        foreach (int truckCount in timeLine)
+        {
+            if (truckCount == 1)
+                totalCost += cost[0];
+            else if (truckCount == 2)
+                totalCost += cost[1] * 2;
+            else if (truckCount == 3)
+                totalCost += cost[2] * 3;
+        }
+
+        Console.WriteLine(totalCost);
+    }
+}
