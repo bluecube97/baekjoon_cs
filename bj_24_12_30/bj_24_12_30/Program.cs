30dd580d2110c4b92498e9af18b379b311edcdec
bluecube97 <jph04071@gmail.com>
2024-12-30 18:44:01 +0900

    2018 연세대학교 프로그래밍 경진대회
    https://www.acmicpc.net/problem/15667

---
+++ bj_24_12_30/bj_24_12_30/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int left = 1, right = n;
+        while (left <= right)
+        {
+            int mid = (left + right) / 2;
+            int total = 1 + mid + mid * mid;
+
+            if (total == n)
+            {
+                Console.WriteLine(mid);
+                return;
+            }
+            else if (total < n)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+    }
+}
