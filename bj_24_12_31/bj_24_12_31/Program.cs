8edaf4e77529fd7f35df291910bc719ce746ad38
bluecube97 <jph04071@gmail.com>
2024-12-31 20:10:03 +0900

    컵라면 측정하기
    https://www.acmicpc.net/problem/16479

---
+++ bj_24_12_31/bj_24_12_31/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int k = int.Parse(Console.ReadLine());
+        double[] D = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        double d1 = D[0];
+        double d2 = D[1];
+
+        double hSquare =  (k * k) - Math.Abs((d1 - d2) / 2) * Math.Abs((d1 - d2) / 2);
+        Console.WriteLine(hSquare);
+    }
+}
