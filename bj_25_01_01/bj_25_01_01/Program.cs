893ef9c6a8378e1214c7a65d0bb9f827f3382cd0
bluecube97 <jph04071@gmail.com>
2025-01-01 20:53:53 +0900

    시험 점수
    https://www.acmicpc.net/problem/5596

---
+++ bj_25_01_01/bj_25_01_01/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int info = input[0];
+        int math = input[1];
+        int science = input[2];
+        int english = input[3];
+
+        input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int info2 = input[0];
+        int math2 = input[1];
+        int science2 = input[2];
+        int english2 = input[3];
+
+        int S = info + math + science + english;
+        int T = info2 + math2 + science2 + english2;
+
+        Console.WriteLine(S >= T ? S : T);
+    }
+}
