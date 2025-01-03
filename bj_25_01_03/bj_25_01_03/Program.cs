02b47f121a88629ff25f606928ad435908bf0de8
bluecube97 <jph04071@gmail.com>
2025-01-03 19:02:27 +0900

    그대로 출력하기 2
    https://www.acmicpc.net/problem/11719

---
+++ bj_25_01_03/bj_25_01_03/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var inputQuestion = new StreamReader(Console.OpenStandardInput());
+        while (inputQuestion.Peek() != -1)
+        {
+            string input = inputQuestion.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
