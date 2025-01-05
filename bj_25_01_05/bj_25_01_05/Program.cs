ce73755b5ea82ee52048c9098d8db86bc3a63e52
bluecube97 <jph04071@gmail.com>
2025-01-05 16:19:22 +0900

    나는 친구가 적다 (Small)
    https://www.acmicpc.net/problem/16171

---
+++ bj_25_01_05/bj_25_01_05/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+        string k = Console.ReadLine();
+
+        int sLen = s.Length;
+
+        for (int i = 0; i < sLen; i++)
+        {
+            if (!((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')))
+            {
+                s = s.Remove(i, 1);
+                i--;
+                sLen--;
+            }
+        }
+
+        if (s.Contains(k))
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+            Console.WriteLine(0);
+        }
+    }
+}
