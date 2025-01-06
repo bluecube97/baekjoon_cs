c88616a90bb07cf475ef64b7dae8accb262801e1
bluecube97 <jph04071@gmail.com>
2025-01-07 07:55:54 +0900

    Pen Pineapple Apple Pen
    https://www.acmicpc.net/problem/15881

---
+++ bj_25_01_07/bj_25_01_07/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        int ppapCount = 0;
+
+        for (int i = 0; i < n - 3; i++)
+        {
+            if (input[i] == 'p' && input[i + 1] == 'P' && input[i + 2] == 'A' && input[i + 3] == 'p')
+            {
+                ppapCount++;
+                i += 3;
+            }
+        }
+
+        Console.WriteLine(ppapCount);
+    }
+}
