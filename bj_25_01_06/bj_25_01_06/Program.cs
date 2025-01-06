d8270f2046cd22b82eb9c949bec0c6f4ca0d42b1
bluecube97 <jph04071@gmail.com>
2025-01-06 18:24:33 +0900

    서버
    https://www.acmicpc.net/problem/10409

---
+++ bj_25_01_06/bj_25_01_06/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int n = input[0];
+        int t = input[1];
+
+        int[] task = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for (int i = 0; i < n; i++)
+        {
+            t -= task[i];
+            if (t < 0)
+            {
+                Console.WriteLine(i);
+                return;
+            }
+        }
+
+        Console.WriteLine(n);
+    }
+}
