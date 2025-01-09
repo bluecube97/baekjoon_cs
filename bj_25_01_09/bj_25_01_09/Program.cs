a7852e34879c316507dd19cbc2b2c736865e20f5
bluecube97 <jph04071@gmail.com>
2025-01-09 18:17:54 +0900

    N M 찍기
    https://www.acmicpc.net/problem/18883

---
+++ bj_25_01_09/bj_25_01_09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n * m; i++)
+        {
+            sb.Append(i);
+            if (i % m == 0)
+            {
+                sb.Append('\n');
+            }
+            else
+            {
+                sb.Append(' ');
+            }
+        }
+
+        Console.Write(sb);
+    }
+}
