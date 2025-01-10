ac81ef3aee8b7af509089a3fce1d220cd90fadd7
bluecube97 <jph04071@gmail.com>
2025-01-10 11:08:18 +0900

    Serious Problem
    https://www.acmicpc.net/problem/17094

---
+++ bj_25_01_10/bj_25_01_10/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int len = int.Parse(Console.ReadLine());
+        string str = Console.ReadLine();
+
+        int twoCount = 0;
+        int eCount = 0;
+
+        foreach (var c in str)
+        {
+            if (c == '2')
+            {
+                twoCount++;
+            }
+            else if (c == 'e')
+            {
+                eCount++;
+            }
+        }
+
+        if (twoCount > eCount)
+        {
+            Console.WriteLine("2");
+        }
+        else if (twoCount < eCount)
+        {
+            Console.WriteLine("e");
+        }
+        else
+        {
+            Console.WriteLine("yee");
+        }
+    }
+}
