e40444486cd111dfa47dc348672b5d8a38cc0318
bluecube97 <jph04071@gmail.com>
2025-01-12 12:38:52 +0900

    코인의 신 건모
    https://www.acmicpc.net/problem/32866

---
+++ bj_25_01_12/bj_25_01_12/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        double n = int.Parse(Console.ReadLine());
+        double x = int.Parse(Console.ReadLine());
+
+        double discountPrice = n * (1 - x / 100);
+        double result = (n - discountPrice) / discountPrice * 100;
+
+        Console.WriteLine($"{result:F6}");
+    }
+}
