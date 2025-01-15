42ee9b9a8bddd69b2cacbd51683650e5a18260d4
bluecube97 <jph04071@gmail.com>
2025-01-15 14:06:37 +0900

    자동완성
    https://www.acmicpc.net/problem/24883

---
+++ bj_25_01_15/bj_25_01_15/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        char alphabet = Console.ReadLine()[0];
+
+        if (alphabet == 'N' || alphabet == 'n')
+        {
+            Console.WriteLine("Naver D2");
+        }
+        else
+        {
+            Console.WriteLine("Naver Whale");
+        }
+    }
+}
