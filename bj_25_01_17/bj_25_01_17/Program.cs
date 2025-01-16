d2a13934c229161ab7213454377b3831e90117da
bluecube97 <jph04071@gmail.com>
2025-01-17 01:30:14 +0900

    나는 행복합니다~
    https://www.acmicpc.net/problem/14652

---
+++ bj_25_01_17/bj_25_01_17/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+        int k = input[2];
+
+        int x = k / m;
+        int y = k % m;
+
+        Console.WriteLine($"{x} {y}");
+    }
+}
