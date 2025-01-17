5581a38a796eec66900fa1230d977304228567b6
bluecube97 <jph04071@gmail.com>
2025-01-18 03:02:18 +0900

    브실혜성
    https://www.acmicpc.net/problem/29722

---
+++ bj_25_01_18/bj_25_01_18/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string date = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+
+        int year = int.Parse(date.Substring(0, 4));
+        int month = int.Parse(date.Substring(5, 2));
+        int day = int.Parse(date.Substring(8, 2));
+
+        day += n;
+
+        while (day > 30 || month > 12)
+        {
+            if (day > 30)
+            {
+                month++;
+                day -= 30;
+            }
+            if (month > 12)
+            {
+                year++;
+                month -= 12;
+            }
+        }
+
+        Console.WriteLine($"{year}-{month:D2}-{day:D2}");
+    }
+}
