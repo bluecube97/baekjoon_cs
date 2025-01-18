a1546a74dee9b7841b17e734488d13712c17fb3a
bluecube97 <jph04071@gmail.com>
2025-01-18 18:56:04 +0900

    반복
    https://www.acmicpc.net/problem/19564

---
+++ bj_25_01_08/bj_25_01_08/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int k = 1;
+        char prev = 'a';
+        prev--;
+
+        foreach (var c in input)
+        {
+            if (c <= prev)
+            {
+                k++;
+            }
+
+            prev = c;
+        }
+
+        Console.Write(k);
+    }
+}
