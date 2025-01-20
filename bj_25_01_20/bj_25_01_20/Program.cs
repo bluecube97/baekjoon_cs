8ae36dea6f07d7c04622f6eb5230be44e58c0cbc
bluecube97 <jph04071@gmail.com>
2025-01-20 23:08:49 +0900

    가지 산사태
    https://www.acmicpc.net/problem/27940

---
+++ bj_25_01_20/bj_25_01_20/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var inputStreamReader = new StreamReader(Console.OpenStandardInput());
+        int[] input = inputStreamReader.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+        int k = input[2];
+
+        int[] floor = new int[n];
+
+        for (int i = 0; i < m; i++)
+        {
+            input = inputStreamReader.ReadLine().Split().Select(int.Parse).ToArray();
+            int t = input[0];
+            int r = input[1];
+
+            floor[0] += r;
+
+            if (floor[0] > k)
+            {
+                Console.WriteLine($"{i + 1} {1}");
+                return;
+            }
+        }
+
+        Console.WriteLine(-1);
+    }
+}
