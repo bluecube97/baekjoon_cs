a595bdf7adc2b77e9aa7303677d50d33e4a00779
bluecube97 <jph04071@gmail.com>
2025-01-22 21:06:17 +0900

    진흥원 세미나
    https://www.acmicpc.net/problem/30087

---
+++ bj_25_01_22/bj_25_01_22/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            FindSeminarClass(Console.ReadLine());
+        }
+    }
+
+    static void FindSeminarClass(string name)
+    {
+        switch (name)
+        {
+            case "Algorithm":
+                Console.WriteLine(204);
+                break;
+            case "DataAnalysis":
+                Console.WriteLine(207);
+                break;
+            case "ArtificialIntelligence":
+                Console.WriteLine(302);
+                break;
+            case "CyberSecurity":
+                Console.WriteLine("B101");
+                break;
+            case "Network":
+                Console.WriteLine(303);
+                break;
+            case "Startup":
+                Console.WriteLine(501);
+                break;
+            case "TestStrategy":
+                Console.WriteLine(105);
+                break;
+        }
+    }
+}
