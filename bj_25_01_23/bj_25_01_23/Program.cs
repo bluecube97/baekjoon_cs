f57164475755ddef2f92dd75e92d47152f21ce9b
bluecube97 <jph04071@gmail.com>
2025-01-24 04:06:06 +0900

    일곱 난쟁이
    https://www.acmicpc.net/problem/2309

---
+++ bj_25_01_23/bj_25_01_23/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] height = new int[9];
+        List<int> result = new List<int>();
+        for (int i = 0; i < 9; i++)
+        {
+            height[i] = int.Parse(Console.ReadLine());
+        }
+
+        int heightSum = height.Sum();
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = i + 1; j < 9; j++)
+            {
+                if (heightSum - height[i] - height[j] == 100)
+                {
+                    for (int k = 0; k < 9; k++)
+                    {
+                        if (k == i || k == j)
+                        {
+                            continue;
+                        }
+
+                        result.Add(height[k]);
+                    }
+
+                    result.Sort();
+                    foreach (var resultHeight in result)
+                    {
+                        Console.WriteLine(resultHeight);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
