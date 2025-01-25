104367f6eb0c7092efe8925053b4dc81420c6545
bluecube97 <jph04071@gmail.com>
2025-01-25 22:23:50 +0900

    PlayStation이 아니에요
    https://www.acmicpc.net/problem/32132

---
+++ bj_25_01_25/bj_25_01_25/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+
+        for (int i = 0; i < input.Length - 2; i++)
+        {
+            if (input[i] != null && input[i].Equals('P') && input[i + 1].Equals('S') && (input[i + 2].Equals('4') || input[i + 2].Equals('5')))
+            {
+                input = input.Remove(i + 2, 1);
+                i--;
+            }
+        }
+
+        Console.WriteLine(input);
+    }
+}
