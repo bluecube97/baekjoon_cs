b6290f972d434be436a644361ebafef8699ec8d1
bluecube97 <jph04071@gmail.com>
2025-01-28 22:43:44 +0900

    대회 or 인턴
    https://www.acmicpc.net/problem/2875

---
+++ bj_25_01_28/bj_25_01_28/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+        int k = input[2];
+
+        for (int i = 0; i < k; i++)
+        {
+            if (n > 2 * m)
+            {
+                n--;
+            }
+            else
+            {
+                m--;
+            }
+        }
+
+        Console.WriteLine(Math.Min(n / 2, m));
+    }
+}
