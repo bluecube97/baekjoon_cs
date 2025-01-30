57c86d4ea4e5640ae21256849c362b0399765331
bluecube97 <jph04071@gmail.com>
2025-01-30 12:38:10 +0900

    대표값2
    https://www.acmicpc.net/problem/2587

---
+++ bj_25_01_30/bj_25_01_30/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < 5; i++)
+        {
+            list.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine(list.Average());
+        list.Sort();
+        Console.WriteLine(list[2]);
+    }
+}
