13e3fd42747221317f685635bfdee1772b9e2e36
bluecube97 <jph04071@gmail.com>
2025-01-31 21:58:09 +0900

    2018년을 되돌아보며
    https://www.acmicpc.net/problem/16674

---
+++ bj_25_01_31/bj_25_01_31/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string num = Console.ReadLine();
+        int two = 0;
+        int zero = 0;
+        int one = 0;
+        int eight = 0;
+
+        foreach (var c in num)
+        {
+            if (c == '2')
+            {
+                two++;
+            }
+            else if (c == '0')
+            {
+                zero++;
+            }
+            else if (c == '1')
+            {
+                one++;
+            }
+            else if (c == '8')
+            {
+                eight++;
+            }
+            else
+            {
+                Console.WriteLine("0");
+                return;
+            }
+        }
+
+        if (two != 0 && zero != 0 && one != 0 && eight != 0)
+        {
+            if (two == zero && zero == one && one == eight)
+            {
+                Console.WriteLine("8");
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
+        }
+        else
+        {
+            Console.WriteLine("1");
+        }
+    }
+}
