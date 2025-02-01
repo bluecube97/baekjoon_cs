f1eac0ff47f7043b5aa9f0c20bfb6115fce690bd
bluecube97 <jph04071@gmail.com>
2025-02-01 21:08:29 +0900

    K512컵 개최!
    https://www.acmicpc.net/problem/31880

---
+++ bj_25_02_01/bj_25_02_01/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+
+        int[] aArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] bArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        long luck = aArr.Sum();
+
+        foreach (var i in bArr)
+        {
+            if (i == 0)
+            {
+                continue;
+            }
+            luck *= i;
+        }
+
+        Console.WriteLine(luck);
+    }
+}
