9f5dba9ad983c4b24c8535b6df461fa71f21ba59
bluecube97 <jph04071@gmail.com>
2025-02-02 14:41:06 +0900

    홀수
    https://www.acmicpc.net/problem/2576

---
+++ bj_25_02_02/bj_25_02_02/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < 7; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 != 0)
+            {
+                list.Add(num);
+            }
+        }
+
+        if (list.Count == 0)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        Console.WriteLine(list.Sum());
+        Console.WriteLine(list.Min());
+    }
+}
