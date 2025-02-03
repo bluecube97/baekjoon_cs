a964d9aea496ed4e595c893b08cf905b1c2b3e4a
bluecube97 <jph04071@gmail.com>
2025-02-04 03:04:56 +0900

    J박스
    https://www.acmicpc.net/problem/5354

---
+++ bj_25_02_03/bj_25_02_03/Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int testCase = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < testCase; i++)
+        {
+            int jBox = int.Parse(Console.ReadLine());
+
+            if (jBox == 1)
+            {
+                Console.WriteLine('#');
+                Console.WriteLine();
+                continue;
+            }
+
+            for (int j = 0; j < jBox; j++)
+            {
+                Console.Write('#');
+            }
+
+            Console.WriteLine();
+
+            for (int j = 0; j < jBox - 2; j++)
+            {
+                for (int k = 0; k < jBox; k++)
+                {
+                    if (k == 0 || k == jBox - 1)
+                    {
+                        Console.Write('#');
+                    }
+                    else
+                    {
+                        Console.Write('J');
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int j = 0; j < jBox; j++)
+            {
+                Console.Write('#');
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
