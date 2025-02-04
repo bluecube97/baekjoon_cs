42bc1d7a890218720849a587aec4ea0700a9cb05
bluecube97 <jph04071@gmail.com>
2025-02-04 21:16:12 +0900

    지각
    https://www.acmicpc.net/problem/10419

---
+++ bj_25_02_04/bj_25_02_04/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < T; i++)
+        {
+            int d = int.Parse(Console.ReadLine());
+            int t = 0;
+            int s = 0;
+
+            while (d >= t + s)
+            {
+                t++;
+                s = t * t;
+            }
+
+            t--;
+            Console.WriteLine(t);
+        }
+    }
+}
