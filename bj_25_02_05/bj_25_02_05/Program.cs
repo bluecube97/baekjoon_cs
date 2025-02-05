0159c990376ef93ba136620f03a074127378fe25
bluecube97 <jph04071@gmail.com>
2025-02-05 23:39:55 +0900

    햄버거 만들기
    https://www.acmicpc.net/problem/25628

---
+++ bj_25_02_05/bj_25_02_05/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = input[0];
+        int b = input[1];
+
+        Console.WriteLine(a / 2 < b ? a / 2 : b);
+    }
+}
