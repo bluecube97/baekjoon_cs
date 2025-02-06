9bffe7e4bf5d2bcc5abdcf61c38b8b29a0d48567
bluecube97 <jph04071@gmail.com>
2025-02-07 00:39:39 +0900

    수 뒤집기
    https://www.acmicpc.net/problem/3062

---
+++ bj_25_02_07/bj_25_02_07/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string num = Console.ReadLine();
+            char[] reverseArr = num.ToCharArray();
+            Array.Reverse(reverseArr);
+            string reverse = new string(reverseArr);
+            string sum = (int.Parse(num) + int.Parse(reverse)).ToString();
+            bool isSymmetric = true;
+
+            for (int j = 0; j < sum.Length / 2; j++)
+            {
+                if (sum[j] != sum[sum.Length - 1 - j])
+                {
+                    Console.WriteLine("NO");
+                    isSymmetric = false;
+                    break;
+                }
+            }
+
+            if (isSymmetric)
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
