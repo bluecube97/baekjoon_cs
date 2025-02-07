1b03409c3ff5c50e25b5f38c2464d2d4f69dd087
bluecube97 <jph04071@gmail.com>
2025-02-07 19:31:19 +0900

    주사위 게임
    https://www.acmicpc.net/problem/10262

---
+++ bj_25_02_08/bj_25_02_08/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] ggung = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] seok = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int ggungScore = ggung[0] + ggung[1] + ggung[2] + ggung[3];
+        int seokScore = seok[0] + seok[1] + seok[2] + seok[3];
+
+        if (ggungScore > seokScore)
+        {
+            Console.WriteLine("Gunnar");
+        }
+        else if (ggungScore < seokScore)
+        {
+            Console.WriteLine("Emma");
+        }
+        else
+        {
+            Console.WriteLine("Tie");
+        }
+    }
+}
