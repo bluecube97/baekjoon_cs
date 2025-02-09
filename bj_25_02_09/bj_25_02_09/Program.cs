d331f13b54d6d21cbd8ce5d1f2543e6c074f19b4
bluecube97 <jph04071@gmail.com>
2025-02-10 00:09:25 +0900

    신기한 수
    https://www.acmicpc.net/problem/17618

---
+++ bj_25_02_09/bj_25_02_09/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            string num = i.ToString();
+            int sum = 0;
+
+            for (int j = 0; j < num.Length; j++)
+            {
+                sum += num[j] - '0';
+            }
+
+            if (int.Parse(num) % sum == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
