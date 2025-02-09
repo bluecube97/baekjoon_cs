7ea58a31fdf4deaeac075101b1d6b70f45e0ce5e
bluecube97 <jph04071@gmail.com>
2025-02-10 00:09:56 +0900

    수열의 변화
    https://www.acmicpc.net/problem/1551

---
+++ bj_25_02_08_1/bj_25_02_08_1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            if (Console.ReadLine().Equals("anj"))
+            {
+                Console.WriteLine("뭐야;");
+                return;
+            }
+        }
+
+        Console.WriteLine("뭐야?");
+    }
+}
