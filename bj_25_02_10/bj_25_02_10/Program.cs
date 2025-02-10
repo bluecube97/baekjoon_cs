f56d0877b2275afaf6b62e87dde8d860e788ccf5
bluecube97 <jph04071@gmail.com>
2025-02-10 23:24:18 +0900

    성적 통계
    https://www.acmicpc.net/problem/5800

---
+++ bj_25_02_10/bj_25_02_10/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int k = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < k; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int[] score = new int[n];
+
+            for (int j = 1; j <= n; j++)
+            {
+                score[j - 1] = input[j];
+            }
+
+            int maxScore = score.Max();
+            int minScore = score.Min();
+            Array.Sort(score);
+            int largestGap = 0;
+
+            for (int j = 1; j < n; j++)
+            {
+                int gap = score[j] - score[j - 1];
+                if (gap > largestGap)
+                {
+                    largestGap = gap;
+                }
+            }
+
+            Console.WriteLine("Class {0}", i + 1);
+            Console.WriteLine("Max {0}, Min {1}, Largest gap {2}", maxScore, minScore, largestGap);
+        }
+    }
+}
