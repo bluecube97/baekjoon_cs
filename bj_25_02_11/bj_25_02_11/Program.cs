6952a0516b34e0ced7cd7e387be98be2d8f0984c
bluecube97 <jph04071@gmail.com>
2025-02-11 09:24:02 +0900

    숨바꼭질
    https://www.acmicpc.net/problem/1697

---
+++ bj_25_02_11/bj_25_02_11/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        int n = int.Parse(input[0]);
+        int k = int.Parse(input[1]);
+
+        if (n == k)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+
+        const int MAX = 100000;
+        bool[] visited = new bool[MAX + 1];
+        Queue<(int position, int time)> queue = new Queue<(int, int)>();
+
+        queue.Enqueue((n, 0));
+        visited[n] = true;
+
+        while (queue.Count > 0)
+        {
+            var (current, time) = queue.Dequeue();
+
+            foreach (int next in new int[] { current - 1, current + 1, current * 2 })
+            {
+                if (next == k)
+                {
+                    Console.WriteLine(time + 1);
+                    return;
+                }
+
+                if (next >= 0 && next <= MAX && !visited[next])
+                {
+                    visited[next] = true;
+                    queue.Enqueue((next, time + 1));
+                }
+            }
+        }
+    }
+}
