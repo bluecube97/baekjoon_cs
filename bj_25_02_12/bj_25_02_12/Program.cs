83b886c1a1c821dfb0f426202b099cfb5506588b
bluecube97 <jph04071@gmail.com>
2025-02-14 00:15:48 +0900

    가희와 방어율 무시
    https://www.acmicpc.net/problem/25238

---
+++ bj_25_02_12/bj_25_02_12/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        float[] input = Console.ReadLine().Split().Select(float.Parse).ToArray();
+        float a = input[0];
+        float b = input[1];
+
+        Console.WriteLine(a - a * b / 100 < 100 ? 1 : 0);
+    }
+}
