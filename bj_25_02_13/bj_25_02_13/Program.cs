a770635f12bf388bbee88641c4386a05352daade
bluecube97 <jph04071@gmail.com>
2025-02-14 00:14:55 +0900

    특식 배부
    https://www.acmicpc.net/problem/27110

---
+++ bj_25_02_13/bj_25_02_13/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] chicken = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = chicken[0];
+        int b = chicken[1];
+        int c = chicken[2];
+
+        if (a > n)
+        {
+            a = n;
+        }
+
+        if (b > n)
+        {
+            b = n;
+        }
+
+        if (c > n)
+        {
+            c = n;
+        }
+
+        Console.WriteLine(a + b + c);
+    }
+}
