4790cd896ee339f5d9ec5aaee215f1c1c1eb6131
bluecube97 <jph04071@gmail.com>
2025-02-15 01:33:37 +0900

    탄산 음료
    https://www.acmicpc.net/problem/5032

---
+++ bj_25_02_14/bj_25_02_14/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int e = input[0];
+        int f = input[1];
+        int c = input[2];
+
+        int cola = 0;
+        int emptyBottle = e + f;
+
+        while (emptyBottle >= c)
+        {
+            int drink = emptyBottle / c;
+            cola += drink;
+            emptyBottle %= c;
+            emptyBottle += drink;
+        }
+
+        Console.WriteLine(cola);
+    }
+}
