5eff4a017501a1edb66b99b9b11fd81c36ee81f2
bluecube97 <jph04071@gmail.com>
2025-02-17 21:47:02 +0900

    지능형 기차 2
    https://www.acmicpc.net/problem/2460

---
+++ bj_25_02_17/bj_25_02_17/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int max = int.MinValue;
+        int people = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int drop = input[0];
+            int get = input[1];
+
+            people -= drop;
+            people += get;
+
+            if (people > max)
+            {
+                max = people;
+            }
+        }
+
+        Console.WriteLine(max);
+    }
+}
