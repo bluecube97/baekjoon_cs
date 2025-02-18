5b0b1151c239fb512b17986c387625cc07f0fed5
bluecube97 <jph04071@gmail.com>
2025-02-18 18:34:46 +0900

    수들의 합
    https://www.acmicpc.net/problem/1789

---
+++ bj_25_02_18/bj_25_02_18/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        long s = long.Parse(Console.ReadLine());
+        int n = 1;
+
+        while (s >= n)
+        {
+            s -= n;
+            n++;
+        }
+
+        Console.WriteLine(n - 1);
+    }
+}
