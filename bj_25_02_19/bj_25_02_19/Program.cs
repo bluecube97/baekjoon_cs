2b856ecaf0054d702e7900842c87e4e439c3dde3
bluecube97 <jph04071@gmail.com>
2025-02-19 14:46:20 +0900

    과자
    https://www.acmicpc.net/problem/10156

---
+++ bj_25_02_19/bj_25_02_19/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int k = input[0];
+        int n = input[1];
+        int m = input[2];
+
+        Console.WriteLine(k * n - m > 0 ? k * n - m : 0);
+    }
+}
