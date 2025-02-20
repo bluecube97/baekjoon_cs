f0c4664aefb7f69fede8bec02b64c35b4d4af481
bluecube97 <jph04071@gmail.com>
2025-02-20 20:02:52 +0900

    0 = not cute / 1 = cute
    https://www.acmicpc.net/problem/10886

---
+++ bj_25_02_20/bj_25_02_20/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] vote = new int[2];
+
+        for (int i = 0; i < n; i++)
+        {
+            int voted = int.Parse(Console.ReadLine());
+            vote[voted]++;
+        }
+
+        if (vote[0] > vote[1])
+        {
+            Console.WriteLine("Junhee is not cute!");
+        }
+        else
+        {
+            Console.WriteLine("Junhee is cute!");
+        }
+    }
+}
