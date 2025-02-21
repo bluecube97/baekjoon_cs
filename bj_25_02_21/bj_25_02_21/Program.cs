46edd3abb2a29e9a3e888774587071d72ba7b290
bluecube97 <jph04071@gmail.com>
2025-02-21 21:39:57 +0900

    학술대회 참가신청
    https://www.acmicpc.net/problem/32969

---
+++ bj_25_02_21/bj_25_02_21/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string title = Console.ReadLine();
+
+        if (title.Contains("social") || title.Contains("history") || title.Contains("language") || title.Contains("literacy"))
+        {
+            Console.WriteLine("digital humanities");
+        }
+        else if (title.Contains("bigdata") || title.Contains("public") || title.Contains("society"))
+        {
+            Console.WriteLine("public bigdata");
+        }
+    }
+}
