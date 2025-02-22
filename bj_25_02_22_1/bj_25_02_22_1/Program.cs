4919ee94c12f4601522d9931f1061ac86f95619b
bluecube97 <jph04071@gmail.com>
2025-02-22 17:07:34 +0900

    페르시아의 왕들
    https://www.acmicpc.net/problem/10599

---
+++ bj_25_02_22_1/bj_25_02_22_1/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+            int c = input[2];
+            int d = input[3];
+
+            if (a == 0 && b == 0 && c == 0 && d == 0)
+            {
+                break;
+            }
+
+            Console.WriteLine("{0} {1}", c - b, d - a);
+        }
+    }
+}
