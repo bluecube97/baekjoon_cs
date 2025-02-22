7aead34a91e01f60464f9bcc87d8d476ac5cb2ff
bluecube97 <jph04071@gmail.com>
2025-02-22 17:04:58 +0900

    기상청 인턴 신현수
    https://www.acmicpc.net/problem/2435

---
+++ bj_25_02_22/bj_25_02_22/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int k = input[1];
+
+        int[] temp = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int maxTemp = int.MinValue;
+
+        for (int i = 0; i < n - k; i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < k; j++)
+            {
+                sum += temp[i + j];
+            }
+
+            if (sum > maxTemp)
+            {
+                maxTemp = sum;
+            }
+        }
+
+        Console.WriteLine(maxTemp);
+    }
+}
