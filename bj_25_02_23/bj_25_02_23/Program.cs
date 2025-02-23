cbe883d089a73e53d56eff73b4e58e9b44553dce
bluecube97 <jph04071@gmail.com>
2025-02-23 16:43:00 +0900

    행사장 대여 (Small)
    https://www.acmicpc.net/problem/14732

---
+++ bj_25_02_23/bj_25_02_23/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        bool[,] grid = new bool[501, 501];
+        int n  = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] coord = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int x1 = coord[0];
+            int y1 = coord[1];
+            int x2 = coord[2];
+            int y2 = coord[3];
+
+            for (int x = x1; x < x2; x++)
+            {
+                for (int y = y1; y < y2; y++)
+                {
+                    grid[x, y] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int x = 0; x < 501; x++)
+        {
+            for (int y = 0; y < 501; y++)
+            {
+                if (grid[x, y])
+                {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
