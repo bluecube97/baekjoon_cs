ec36e86ad8452c4556033b9ea479faa1b84f4d9f
bluecube97 <jph04071@gmail.com>
2025-02-26 00:16:59 +0900

    과자 사기
    https://www.acmicpc.net/problem/17450

---
+++ bj_25_02_25/bj_25_02_25/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] s = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] n = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] u = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int sPrice = s[0];
+        int sWeight = s[1];
+        int nPrice = n[0];
+        int nWeight = n[1];
+        int uPrice = u[0];
+        int uWeight = u[1];
+
+        double sCost = calcCost(sPrice, sWeight);
+        double nCost = calcCost(nPrice, nWeight);
+        double uCost = calcCost(uPrice, uWeight);
+
+        List<double> cost = new List<double> { sCost, nCost, uCost };
+        double min = cost.Max();
+        if (min == sCost)
+        {
+            Console.WriteLine("S");
+        }
+        else if (min == nCost)
+        {
+            Console.WriteLine("N");
+        }
+        else
+        {
+            Console.WriteLine("U");
+        }
+    }
+
+    private static double calcCost(int price, int weight)
+    {
+        int totalPrice = price * 10;
+        if (totalPrice >= 5000)
+        {
+            totalPrice -= 500;
+        }
+
+        return (double)(weight * 10) / totalPrice;
+    }
+}
