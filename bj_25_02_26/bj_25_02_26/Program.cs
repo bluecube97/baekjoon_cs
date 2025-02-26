391c81ec2b10c4a7ea944ce5144db847f8584fbc
bluecube97 <jph04071@gmail.com>
2025-02-26 19:45:30 +0900

    타일 채우기 4
    https://www.acmicpc.net/problem/15700

---
+++ bj_25_02_26/bj_25_02_26/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int m = input[1];
+
+        BigInteger extent = BigInteger.Multiply(n, m);
+        Console.WriteLine(BigInteger.Divide(extent, 2));
+    }
+}
