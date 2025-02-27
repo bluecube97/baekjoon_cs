4813f12fdd59d9da8fd18970fe42e456d4b90558
bluecube97 <jph04071@gmail.com>
2025-02-27 18:34:05 +0900

    문어 숫자
    https://www.acmicpc.net/problem/1864

---
+++ bj_25_02_27/bj_25_02_27/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "#")
+            {
+                break;
+            }
+
+            int strLen = input.Length;
+            int sum = 0;
+
+            for (int i = 0; i < strLen; i++)
+            {
+                sum += getNum(input[i]) * (int)Math.Pow(8, strLen - i - 1);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+
+    private static int getNum(char c)
+    {
+        return c switch
+        {
+            '-' => 0,
+            '\\' => 1,
+            '(' => 2,
+            '@' => 3,
+            '?' => 4,
+            '>' => 5,
+            '&' => 6,
+            '%' => 7,
+            '/' => -1,
+            _ => 0
+        };
+    }
+}
