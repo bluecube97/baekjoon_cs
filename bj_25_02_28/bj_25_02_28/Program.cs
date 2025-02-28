f747b2bf737f893bfc46a225e1ecadf874f53e01
bluecube97 <jph04071@gmail.com>
2025-03-01 02:29:17 +0900

    신용카드 판별
    https://www.acmicpc.net/problem/14726

---
+++ bj_25_02_28/bj_25_02_28/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string creditCard = Console.ReadLine();
+            int[] numArr = new int[16];
+
+            for (int j = 0; j < 16; j++)
+            {
+                numArr[j] = int.Parse(creditCard[j].ToString());
+            }
+
+            for (int j = 14; j >= 0; j -= 2)
+            {
+                int num = numArr[j] * 2;
+                if (num >= 10)
+                {
+                    num = num / 10 + num % 10;
+                }
+                numArr[j] = num;
+            }
+
+            int sum = 0;
+            for (int j = 0; j < 16; j++)
+            {
+                sum += numArr[j];
+            }
+
+            if (sum % 10 == 0)
+            {
+                Console.WriteLine("T");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
