ca1bf5de73ac8d6362b89c79d38453a0a4b4b99e
bluecube97 <jph04071@gmail.com>
2025-03-02 21:39:29 +0900

    개미
    https://www.acmicpc.net/problem/11880

---
+++ bj_25_03_02/bj_25_03_02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var inputStreamReader = new StreamReader(Console.OpenStandardInput());
+        StringBuilder sb = new StringBuilder();
+        int t = int.Parse(inputStreamReader.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            long[] input = inputStreamReader.ReadLine().Split().Select(long.Parse).ToArray();
+            long a = input[0];
+            long b = input[1];
+            long c = input[2];
+
+            long x = (a + b) * (a + b) + c * c;
+            long y = (b + c) * (b + c) + a * a;
+            long z = (c + a) * (c + a) + b * b;
+
+            sb.Append(Math.Min(x, Math.Min(y, z)));
+            sb.Append('\n');
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
