62a65f82e473218793f0e4ed12f10cb60cd2ab72
bluecube97 <jph04071@gmail.com>
2025-03-03 21:44:29 +0900

    2023년은 검은 토끼의 해
    https://www.acmicpc.net/problem/27494

---
+++ bj_25_03_03/bj_25_03_03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var sir = new StreamReader(Console.OpenStandardInput());
+        int n = int.Parse(sir.ReadLine());
+        int count = 0;
+
+        for (int i = 2023; i <= n; i++)
+        {
+            int serialCount = 0;
+            string serial = i.ToString();
+
+            foreach (var num in serial)
+            {
+                if (serialCount == 0 && num == '2')
+                {
+                    serialCount++;
+                }
+                else if (serialCount == 1 && num == '0')
+                {
+                    serialCount++;
+                }
+                else if (serialCount == 2 && num == '2')
+                {
+                    serialCount++;
+                }
+                else if (serialCount == 3 && num == '3')
+                {
+                    serialCount++;
+                }
+            }
+
+            if (serialCount == 4)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
