10eb2747b4bf8c4d8564b0991eceed5751222ce9
bluecube97 <jph04071@gmail.com>
2025-03-04 19:38:21 +0900

    피자 (Small)
    https://www.acmicpc.net/problem/14606

---
+++ bj_25_03_04/bj_25_03_04/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        int n = int.Parse(isr.ReadLine());
+        Console.WriteLine(GetFun(n));
+    }
+
+    private static int GetFun(int n)
+    {
+        if (n == 1)
+        {
+            return 0;
+        }
+
+        int a = n / 2;
+        int b = n - a;
+        int fun = a * b;
+
+        return fun + GetFun(a) + GetFun(b);
+    }
+}
