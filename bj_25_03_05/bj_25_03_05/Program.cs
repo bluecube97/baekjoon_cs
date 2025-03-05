0e1d27391cfbf669ca40c3501faafa5854ab77f7
bluecube97 <jph04071@gmail.com>
2025-03-05 19:48:23 +0900

    최소, 최대 2
    https://www.acmicpc.net/problem/20053

---
+++ bj_25_03_05/bj_25_03_05/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(isr.ReadLine());
+            int[] arr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(arr.Min() + " " + arr.Max());
+        }
+    }
+}
