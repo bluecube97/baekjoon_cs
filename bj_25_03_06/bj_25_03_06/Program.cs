256c66ba09b22e138c43e8892e61dadc01880e98
bluecube97 <jph04071@gmail.com>
2025-03-06 10:41:55 +0900

    닉네임에 갓 붙이기
    https://www.acmicpc.net/problem/13163

---
+++ bj_25_03_06/bj_25_03_06/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string name = isr.ReadLine();
+            bool isFirst = true;
+            Console.Write("god");
+
+            foreach (var c in name)
+            {
+                if (!isFirst)
+                {
+                    if (c != ' ')
+                    {
+                        Console.Write(c);
+                    }
+                }
+                else if (c == ' ')
+                {
+                    isFirst = false;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
