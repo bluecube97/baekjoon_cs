6a2450419c9bb957bede187f2fab396fe77f98d8
bluecube97 <jph04071@gmail.com>
2025-03-07 20:03:48 +0900

    타슈
    https://www.acmicpc.net/problem/30018

---
+++ bj_25_03_07/bj_25_03_07/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        int n = int.Parse(isr.ReadLine());
+        int[] a = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] b = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int bicycle = a.Length;
+        int sum = 0;
+
+        for (int i = 0; i < bicycle; i++)
+        {
+            sum += Math.Abs(a[i] - b[i]);
+        }
+
+        Console.WriteLine(sum / 2);
+    }
+}
