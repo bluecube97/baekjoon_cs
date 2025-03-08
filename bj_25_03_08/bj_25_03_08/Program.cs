340392874c05a3a9b61db761c14652ec09dbcf7f
bluecube97 <jph04071@gmail.com>
2025-03-08 19:05:16 +0900

    5와 6의 차이
    https://www.acmicpc.net/problem/2864

---
+++ bj_25_03_08/bj_25_03_08/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        string[] input = isr.ReadLine().Split();
+
+        for (int i = 0; i < input[0].Length; i++)
+        {
+            if (input[0][i] == '6')
+            {
+                input[0] = input[0].Remove(i, 1).Insert(i, "5");
+            }
+        }
+
+        for (int i = 0; i < input[1].Length; i++)
+        {
+            if (input[1][i] == '6')
+            {
+                input[1] = input[1].Remove(i, 1).Insert(i, "5");
+            }
+        }
+
+        Console.Write(int.Parse(input[0]) + int.Parse(input[1]) + " ");
+
+        for (int i = 0; i < input[0].Length; i++)
+        {
+            if (input[0][i] == '5')
+            {
+                input[0] = input[0].Remove(i, 1).Insert(i, "6");
+            }
+        }
+
+        for (int i = 0; i < input[1].Length; i++)
+        {
+            if (input[1][i] == '5')
+            {
+                input[1] = input[1].Remove(i, 1).Insert(i, "6");
+            }
+        }
+
+        Console.Write(int.Parse(input[0]) + int.Parse(input[1]));
+    }
+}
