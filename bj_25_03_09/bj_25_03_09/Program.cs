f57f188dda226050396bdb178b42fb231143d9c4
bluecube97 <jph04071@gmail.com>
2025-03-09 19:11:58 +0900

    택시 거리
    https://www.acmicpc.net/problem/17247

---
+++ bj_25_03_09/bj_25_03_09/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        (int, int) start = new ValueTuple<int, int>();
+        (int, int) end = new ValueTuple<int, int>();
+        bool isStart = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] line = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int j = 0; j < m; j++)
+            {
+                if (line[j] == 1 && isStart)
+                {
+                    start.Item1 = i;
+                    start.Item2 = j;
+                    isStart = false;
+                }
+
+                if (line[j] == 1 && !isStart)
+                {
+                    end.Item1 = i;
+                    end.Item2 = j;
+                }
+            }
+        }
+
+        Console.WriteLine(Math.Abs(start.Item1 - end.Item1) + Math.Abs(start.Item2 - end.Item2));
+    }
+}
