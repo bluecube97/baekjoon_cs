3996571d527a17a3def284cea2a6e8c7638c649e
bluecube97 <jph04071@gmail.com>
2025-03-10 19:55:48 +0900

    겨울왕국 티켓 예매
    https://www.acmicpc.net/problem/18247

---
+++ bj_25_03_10/bj_25_03_10/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nm[0];
+            int m = nm[1];
+
+            if (n * m < 48 || n < 12 || m < 4)
+            {
+                Console.WriteLine(-1);
+                continue;
+            }
+
+            Console.WriteLine(m * 11 + 4);
+        }
+    }
+}
