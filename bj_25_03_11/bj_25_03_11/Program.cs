2a65904a4b53416d8d0beb69fe012d26ef16aec0
bluecube97 <jph04071@gmail.com>
2025-03-11 19:51:30 +0900

    일우는 야바위꾼
    https://www.acmicpc.net/problem/20361

---
+++ bj_25_03_11/bj_25_03_11/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nxk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nxk[0];
+        int x = nxk[1];
+        int k = nxk[2];
+
+        bool[] ball = new bool[n + 1];
+        ball[x] = true;
+
+        for (int i = 0; i < k; i++)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+
+            if (ball[a])
+            {
+                ball[a] = false;
+                ball[b] = true;
+            }
+            else if (ball[b])
+            {
+                ball[b] = false;
+                ball[a] = true;
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (ball[i])
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
