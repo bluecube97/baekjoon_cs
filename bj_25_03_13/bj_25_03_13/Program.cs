21f03ce2698a456bfb353b9eaeea3bd945b1cacf
bluecube97 <jph04071@gmail.com>
2025-03-13 07:07:25 +0900

    모험의 시작
    https://www.acmicpc.net/problem/31789

---
+++ bj_25_03_13/bj_25_03_13/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] xs = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int x = xs[0];
+        int s = xs[1];
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] cp = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int c = cp[0];
+            int p = cp[1];
+
+            if (c <= x && p > s)
+            {
+                Console.WriteLine("YES");
+                return;
+            }
+        }
+
+        Console.WriteLine("NO");
+    }
+}
