fbfcbecf776db3bbf2b8354e1823a84d699828c8
bluecube97 <jph04071@gmail.com>
2025-03-12 20:58:28 +0900

    수찬은 마린보이야!!
    https://www.acmicpc.net/problem/15921

---
+++ bj_25_03_12/bj_25_03_12/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        if (n == 0)
+        {
+            Console.WriteLine("divide by zero");
+            return;
+        }
+        int[] practice = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        float ave = (float) practice.Sum() / n;
+        float exp = (float) practice.Sum() * (1.0f / n);
+
+        if (exp == 0)
+        {
+            Console.WriteLine("divide by zero");
+            return;
+        }
+
+        Console.WriteLine("{0:F2}", ave / exp);
+    }
+}
