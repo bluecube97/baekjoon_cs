de72132b4ad3957a5ffd3d9263d114db31cfb538
bluecube97 <jph04071@gmail.com>
2025-03-15 20:08:12 +0900

    평균은 넘겠지
    https://www.acmicpc.net/problem/4344

---
+++ bj_25_03_15/bj_25_03_15/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int c = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < c; i++)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = input[0];
+            int sum = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+                int score = input[j + 1];
+                sum += score;
+            }
+
+            float avg = (float)sum / n;
+            int overAvgCount = 0;
+
+            for (int j = 0; j < n; j++)
+            {
+                int score = input[j + 1];
+                if (score > avg)
+                {
+                    overAvgCount++;
+                }
+            }
+
+            Console.WriteLine("{0:F3}%", (float)overAvgCount / n * 100);
+        }
+    }
+}
