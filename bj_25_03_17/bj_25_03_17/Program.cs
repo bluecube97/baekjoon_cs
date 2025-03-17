da2c6a0c37668c0bedcea5e0c41dab40b2a7ae37
bluecube97 <jph04071@gmail.com>
2025-03-17 22:01:59 +0900

    6174
    https://www.acmicpc.net/problem/9047

---
+++ bj_25_03_17/bj_25_03_17/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string num = isr.ReadLine();
+            int count = 0;
+
+            while (true)
+            {
+                if (num.Equals("6174"))
+                {
+                    Console.WriteLine(count);
+                    break;
+                }
+
+                char[] numArr = num.ToArray();
+                Array.Sort(numArr);
+                string a = new string(numArr);
+                string b = new string(numArr.Reverse().ToArray());
+
+                num = Math.Abs(int.Parse(b) - int.Parse(a)).ToString().PadLeft(4, '0');
+                count++;
+            }
+        }
+    }
+}
