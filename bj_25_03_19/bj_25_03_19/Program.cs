3efa51390b8da668030e2d47d1b0748aaea27ae2
bluecube97 <jph04071@gmail.com>
2025-03-19 17:38:26 +0900

    SUAPC 2024 Winter
    https://www.acmicpc.net/problem/32154

---
+++ bj_25_03_19/bj_25_03_19/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        switch (n)
+        {
+            case 1:
+                Console.WriteLine(11);
+                Console.WriteLine("A B C D E F G H J L M");
+                break;
+            case 2:
+                Console.WriteLine(9);
+                Console.WriteLine("A C E F G H I L M");
+                break;
+            case 3:
+                Console.WriteLine(9);
+                Console.WriteLine("A C E F G H I L M");
+                break;
+            case 4:
+                Console.WriteLine(9);
+                Console.WriteLine("A B C E F G H L M");
+                break;
+            case 5:
+                Console.WriteLine(8);
+                Console.WriteLine("A C E F G H L M");
+                break;
+            case 6:
+                Console.WriteLine(8);
+                Console.WriteLine("A C E F G H L M");
+                break;
+            case 7:
+                Console.WriteLine(8);
+                Console.WriteLine("A C E F G H L M");
+                break;
+            case 8:
+                Console.WriteLine(8);
+                Console.WriteLine("A C E F G H L M");
+                break;
+            case 9:
+                Console.WriteLine(8);
+                Console.WriteLine("A C E F G H L M");
+                break;
+            case 10:
+                Console.WriteLine(8);
+                Console.WriteLine("A B C F G H L M");
+                break;
+        }
+    }
+}
