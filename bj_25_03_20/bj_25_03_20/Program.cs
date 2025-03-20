d209c550a22f519cf666e6fb08df51b6cab65ce9
bluecube97 <jph04071@gmail.com>
2025-03-20 19:06:47 +0900

    남욱이의 닭장
    https://www.acmicpc.net/problem/11006

---
+++ bj_25_03_20/bj_25_03_20/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nm[0];
+            int m = nm[1];
+
+            Console.WriteLine("{0} {1}", m * 2 - n, m - (m * 2 - n));
+        }
+    }
+}
