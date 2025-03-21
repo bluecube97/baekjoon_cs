058dabd9697058e2465c7c8abea37fe9224d6910
bluecube97 <jph04071@gmail.com>
2025-03-22 05:42:04 +0900

    줄번호
    https://www.acmicpc.net/problem/4470

---
+++ bj_25_03_22/bj_25_03_22/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("{0}. {1}", i + 1, isr.ReadLine());
+        }
+    }
+}
