132aad1d90ff52fdcbc7eb7d20340147e4931585
bluecube97 <jph04071@gmail.com>
2025-03-22 20:44:40 +0900

    완전수
    https://www.acmicpc.net/problem/14563

---
+++ bj_25_03_22_1/bj_25_03_22_1/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+        int[] numArr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        for (int i = 0; i < t; i++)
+        {
+            int num = numArr[i];
+            List<int> divisors = getDivisor(num);
+
+            if (divisors.Sum() == num)
+            {
+                Console.WriteLine("Perfect");
+            }
+            else if (divisors.Sum() < num)
+            {
+                Console.WriteLine("Deficient");
+            }
+            else
+            {
+                Console.WriteLine("Abundant");
+            }
+        }
+    }
+
+    private static List<int> getDivisor(int num)
+    {
+        List<int> divisors = new List<int>();
+        for (int i = 1; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                divisors.Add(i);
+            }
+        }
+        return divisors;
+    }
+}
