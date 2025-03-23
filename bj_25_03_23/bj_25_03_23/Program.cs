8cca5cf98a38c1474e025ffd2e9ca54239df7f3e
bluecube97 <jph04071@gmail.com>
2025-03-23 19:48:33 +0900

    사나운 개
    https://www.acmicpc.net/problem/2991

---
+++ bj_25_03_23/bj_25_03_23/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] abcd = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = abcd[0], b = abcd[1], c = abcd[2], d = abcd[3];
+
+        int[] pmn = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int p = pmn[0], m = pmn[1], n = pmn[2];
+
+        Console.WriteLine(GetAttackCount(p, a, b, c, d));
+        Console.WriteLine(GetAttackCount(m, a, b, c, d));
+        Console.WriteLine(GetAttackCount(n, a, b, c, d));
+    }
+
+    static int GetAttackCount(int time, int a, int b, int c, int d)
+    {
+        int attackCount = 0;
+
+        if ((time - 1) % (a + b) + 1 <= a) attackCount++;
+        if ((time - 1) % (c + d) + 1 <= c) attackCount++;
+
+        return attackCount;
+    }
+}
