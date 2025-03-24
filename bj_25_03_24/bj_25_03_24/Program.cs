1694cf2393c17307abbb6b6c2f3d0519873f17c7
bluecube97 <jph04071@gmail.com>
2025-03-24 22:37:42 +0900

    10부제
    https://www.acmicpc.net/problem/10797

---
+++ bj_25_03_24/bj_25_03_24/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int day = int.Parse(isr.ReadLine());
+        int count = 0;
+        int[] carNum = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        foreach (var num in carNum)
+        {
+            if (num % 10 == day)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
