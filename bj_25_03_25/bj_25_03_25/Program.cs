c9025f685fd001a221beba407f2205f8cbdb50fd
bluecube97 <jph04071@gmail.com>
2025-03-25 22:30:14 +0900

    !밀비 급일
    https://www.acmicpc.net/problem/11365

---
+++ bj_25_03_25/bj_25_03_25/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        
+        while (true)
+        {
+            string line = isr.ReadLine();
+            if (line.Equals("END"))
+            {
+                break;
+            }
+
+            Console.WriteLine(line.Reverse().ToArray());
+        }
+    }
+}
