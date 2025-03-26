30c14f3619a4334a22643cb96cb63f8b0dc3177b
bluecube97 <jph04071@gmail.com>
2025-03-26 19:19:12 +0900

    체육은 수학과목 입니다
    https://www.acmicpc.net/problem/32025

---
+++ bj_25_03_26/bj_25_03_26/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int h = int.Parse(isr.ReadLine());
+        int w = int.Parse(isr.ReadLine());
+        float r = (float)Math.Min(h, w) / 2;
+
+        Console.WriteLine(r * 100);
+    }
+}
