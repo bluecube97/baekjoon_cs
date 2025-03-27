7e8bdea350bea703f2fa9049c8896216ffc4ec2f
bluecube97 <jph04071@gmail.com>
2025-03-27 19:44:34 +0900

    강당 대관
    https://www.acmicpc.net/problem/31994

---
+++ bj_25_03_27/bj_25_03_27/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string maxSeminar = "";
+        int maxPeople = int.MinValue;
+
+        for (int i = 0; i < 7; i++)
+        {
+            string[] input = isr.ReadLine().Split();
+            string seminar = input[0];
+            int people = int.Parse(input[1]);
+
+            if (people > maxPeople)
+            {
+                maxPeople = people;
+                maxSeminar = seminar;
+            }
+        }
+
+        Console.WriteLine(maxSeminar);
+    }
+}
