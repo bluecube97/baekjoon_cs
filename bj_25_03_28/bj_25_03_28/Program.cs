cea1373e7f8cee228f4a2f2fde5bb45c57be6871
bluecube97 <jph04071@gmail.com>
2025-03-28 20:10:25 +0900

    멀티탭 충분하니?
    https://www.acmicpc.net/problem/15780

---
+++ bj_25_03_28/bj_25_03_28/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] multiTab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int n = nk[0];
+        int k = nk[1];
+
+        int count = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            int hole = multiTab[i];
+
+            if (hole % 2 == 0)
+            {
+                count += hole / 2;
+            }
+            else
+            {
+                count += hole / 2 + 1;
+            }
+        }
+
+        if (count >= n)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
