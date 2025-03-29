fd72bdca7a0cb3703a23898e694ed86915b680f9
bluecube97 <jph04071@gmail.com>
2025-03-29 18:29:52 +0900

    문자열 분석
    https://www.acmicpc.net/problem/10820

---
+++ bj_25_03_29/bj_25_03_29/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (isr.Peek() >= 0)
+        {
+            string str = isr.ReadLine();
+
+            int small = 0;
+            int big = 0;
+            int num = 0;
+            int space = 0;
+
+            foreach (char c in str)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    small++;
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    big++;
+                }
+                else if (c >= '0' && c <= '9')
+                {
+                    num++;
+                }
+                else if (c == ' ')
+                {
+                    space++;
+                }
+            }
+
+            Console.WriteLine($"{small} {big} {num} {space}");
+        }
+    }
+}
