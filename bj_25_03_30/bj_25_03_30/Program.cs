2ed4c2061a0f9b37f40ba37a1b19d350ff008773
bluecube97 <jph04071@gmail.com>
2025-03-30 19:10:18 +0900

    수학은 체육과목 입니다 2
    https://www.acmicpc.net/problem/17362

---
+++ bj_25_03_30/bj_25_03_30/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        if (n % 8 == 1)
+        {
+            Console.WriteLine("1");
+        }
+        else if (n % 8 == 2 || n % 8 == 0)
+        {
+            Console.WriteLine("2");
+        }
+        else if (n % 8 == 3 || n % 8 == 7)
+        {
+            Console.WriteLine("3");
+        }
+        else if (n % 8 == 4 || n % 8 == 6)
+        {
+            Console.WriteLine("4");
+        }
+        else if (n % 8 == 5)
+        {
+            Console.WriteLine("5");
+        }
+    }
+}
