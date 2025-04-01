325eb2102784c5d3fa0391f119c82216b47a5620
bluecube97 <jph04071@gmail.com>
2025-04-01 18:44:40 +0900

    쌍의 합
    https://www.acmicpc.net/problem/5217

---
+++ bj_25_04_01/bj_25_04_01/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            int num = int.Parse(isr.ReadLine());
+            Console.Write("Pairs for {0}: ", num);
+
+            for (int j = 1; j <= num / 2; j++)
+            {
+                int a = j;
+                int b = num - j;
+
+                if (a >= b)
+                {
+                    break;
+                }
+
+                if (j > 1)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write("{0} {1}", a, b);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
