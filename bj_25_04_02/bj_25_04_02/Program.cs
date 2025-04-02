8d40db578e4d507904d16e9181f3b446c28c7414
bluecube97 <jph04071@gmail.com>
2025-04-02 18:41:39 +0900

    선택 가능성이 가장 높은 자료형
    https://www.acmicpc.net/problem/32278

---
+++ bj_25_04_02/bj_25_04_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        BigInteger n = BigInteger.Parse(isr.ReadLine());
+
+        if (n >= -32768 && n <= 32767)
+        {
+            Console.WriteLine("short");
+        }
+        else if (n >= -2147483648 && n <= 2147483647)
+        {
+            Console.WriteLine("int");
+        }
+        else if (n >= -9223372036854775808 && n <= 9223372036854775807)
+        {
+            Console.WriteLine("long long");
+        }
+    }
+}
