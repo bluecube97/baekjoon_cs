4c75ac14b3bd432135c82bb4b806de88fceaaa9a
bluecube97 <jph04071@gmail.com>
2025-04-03 19:10:06 +0900

    세 수
    https://www.acmicpc.net/problem/10817

---
+++ bj_25_04_03/bj_25_04_03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] numArr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Array.Sort(numArr);
+        Console.WriteLine(numArr[1]);
+    }
+}
