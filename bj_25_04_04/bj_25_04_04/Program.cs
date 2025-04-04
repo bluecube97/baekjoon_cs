6c95ba44eb88f631cd96ddec8f5c2576f138321c
bluecube97 <jph04071@gmail.com>
2025-04-04 19:18:44 +0900

    좋은 자동차 번호판
    https://www.acmicpc.net/problem/1871

---
+++ bj_25_04_04/bj_25_04_04/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string carNum = isr.ReadLine();
+            string first = carNum[..3];
+            string second = carNum[4..8];
+
+            int firstValue = 0;
+            int secondValue = int.Parse(second);
+
+            for (int j = 0; j < 3; j++)
+            {
+                firstValue += (first[j] - 'A') * (int) Math.Pow(26, 2 - j);
+            }
+
+            if (Math.Abs(firstValue - secondValue) <= 100)
+            {
+                Console.WriteLine("nice");
+            }
+            else
+            {
+                Console.WriteLine("not nice");
+            }
+        }
+    }
+}
