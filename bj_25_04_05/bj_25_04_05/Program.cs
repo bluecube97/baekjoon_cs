6da4e23c7f6a0e6372e435f7d45853874b300c1c
bluecube97 <jph04071@gmail.com>
2025-04-05 19:03:39 +0900

    경기 결과
    https://www.acmicpc.net/problem/5523

---
+++ bj_25_04_05/bj_25_04_05/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int aScore = 0;
+        int bScore = 0;
+
+        int n = int.Parse(isr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = ab[0];
+            int b = ab[1];
+
+            if (a > b)
+            {
+                aScore++;
+            }
+            else if (a < b)
+            {
+                bScore++;
+            }
+        }
+
+        Console.WriteLine($"{aScore} {bScore}");
+    }
+}
