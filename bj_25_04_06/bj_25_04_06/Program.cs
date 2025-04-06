1b9475523c6808f84f12904dd8938b205e608c9b
bluecube97 <jph04071@gmail.com>
2025-04-06 19:45:42 +0900

    자료구조는 정말 최고야
    https://www.acmicpc.net/problem/23253

---
+++ bj_25_04_06/bj_25_04_06/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        List<Stack<int>> bookStackList = new List<Stack<int>>(m);
+
+        for (int i = 0; i < m; i++)
+        {
+            int k = int.Parse(isr.ReadLine());
+            int[] bookArr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> bookStack = new Stack<int>(bookArr);
+
+            bookStackList.Add(bookStack);
+        }
+
+        bool isPossible = true;
+
+        foreach (var stack in bookStackList)
+        {
+            int prev = int.MinValue;
+
+            while (stack.Count > 0)
+            {
+                int curr = stack.Pop();
+                if (curr < prev)
+                {
+                    isPossible = false;
+                    break;
+                }
+                prev = curr;
+            }
+
+            if (!isPossible) break;
+        }
+
+        Console.WriteLine(isPossible ? "Yes" : "No");
+    }
+}
