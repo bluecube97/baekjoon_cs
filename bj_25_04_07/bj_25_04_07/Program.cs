cff49a5b51c8d3e4c5cedea815207e5390c0b5b1
bluecube97 <jph04071@gmail.com>
2025-04-07 19:44:43 +0900

    과제 안 내신 분..?
    https://www.acmicpc.net/problem/5597

---
+++ bj_25_04_07/bj_25_04_07/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] attArr = new int[31];
+
+        for (int i = 0; i < 28; i++)
+        {
+            int n = int.Parse(isr.ReadLine());
+            attArr[n] = 1;
+        }
+
+        for (int i = 1; i <= 30; i++)
+        {
+            if (attArr[i] == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
