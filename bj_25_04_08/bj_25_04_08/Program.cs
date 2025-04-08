99e494308039f88c8568e1f46676176deb159262
bluecube97 <jph04071@gmail.com>
2025-04-08 19:53:05 +0900

    분수좋아해?
    https://www.acmicpc.net/problem/10474

---
+++ bj_25_04_08/bj_25_04_08/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            int[] line = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            if (line[0] == 0 && line[1] == 0)
+                break;
+
+            int a = line[0];
+            int b = line[1];
+
+            int first = a / b;
+            int second = a % b;
+
+            Console.Write("{0} {1} / {2}", first, second, b);
+            Console.WriteLine();
+        }
+    }
+}
