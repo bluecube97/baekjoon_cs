2adc1131bff256a8f7c877d364a7dc839b832485
bluecube97 <jph04071@gmail.com>
2025-04-10 16:22:47 +0900

    평균 중앙값 문제
    https://www.acmicpc.net/problem/5691

---
+++ bj_25_04_10/bj_25_04_10/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            int[] line = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int a = line[0];
+            int b = line[1];
+
+            if (a == 0 && b == 0)
+                break;
+
+            Console.WriteLine(a * 2 - b);
+        }
+    }
+}
