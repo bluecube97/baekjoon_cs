a94a9d9da366742c15197bba30841c37aee684f5
bluecube97 <jph04071@gmail.com>
2025-04-11 19:47:21 +0900

    막대기
    https://www.acmicpc.net/problem/17608

---
+++ bj_25_04_11/bj_25_04_11/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] heightArr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            heightArr[i] = int.Parse(isr.ReadLine());
+        }
+
+        int maxHeight = heightArr[n - 1];
+        int count = 0;
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (heightArr[i] > maxHeight)
+            {
+                count++;
+                maxHeight = heightArr[i];
+            }
+        }
+
+        count++;
+        Console.WriteLine(count);
+    }
+}
