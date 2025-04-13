630bbbf8fad851bfffe6a82335bd4eb32ee1a84b
bluecube97 <jph04071@gmail.com>
2025-04-13 21:07:48 +0900

    Yangjojang of The Year
    https://www.acmicpc.net/problem/11557

---
+++ bj_25_04_13/bj_25_04_13/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(isr.ReadLine());
+
+            (string, int)[] univ = new (string, int)[n];
+
+            for (int j = 0; j < n; j++)
+            {
+                string[] sl = isr.ReadLine().Split();
+                string s = sl[0];
+                int l = int.Parse(sl[1]);
+
+                univ[j] = (s, l);
+            }
+
+            Array.Sort(univ, (y, x) => x.Item2.CompareTo(y.Item2));
+            Console.WriteLine(univ[0].Item1);
+        }
+    }
+}
