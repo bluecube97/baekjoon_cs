012d0508247d29f2d8dc14156774661a6b6ad314
bluecube97 <jph04071@gmail.com>
2025-04-14 20:07:28 +0900

    피로도
    https://www.acmicpc.net/problem/22864

---
+++ bj_25_04_14/bj_25_04_14/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] abcm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = abcm[0];
+        int b = abcm[1];
+        int c = abcm[2];
+        int m = abcm[3];
+
+        int tired = 0;
+        int work = 0;
+
+        for (int i = 0; i < 24; i++)
+        {
+            if (tired + a <= m)
+            {
+                tired += a;
+                work += b;
+            }
+            else
+            {
+                tired -= c;
+                if (tired < 0)
+                {
+                    tired = 0;
+                }
+            }
+        }
+
+        Console.WriteLine(work);
+    }
+}
