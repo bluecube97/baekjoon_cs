cbd0d1ddd3c0a35bb4b8dbcce36252818ec05525
bluecube97 <jph04071@gmail.com>
2025-04-15 20:45:47 +0900

    계산기 프로그램
    https://www.acmicpc.net/problem/5613

---
+++ bj_25_04_15/bj_25_04_15/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int result = int.Parse(isr.ReadLine());
+        while (true)
+        {
+            string oper = isr.ReadLine();
+
+            if (oper == "=")
+            {
+                Console.WriteLine(result);
+                break;
+            }
+
+            int num = int.Parse(isr.ReadLine());
+
+            switch (oper)
+            {
+                case "+":
+                    result += num;
+                    break;
+                case "-":
+                    result -= num;
+                    break;
+                case "*":
+                    result *= num;
+                    break;
+                case "/":
+                    result /= num;
+                    break;
+            }
+        }
+    }
+}
