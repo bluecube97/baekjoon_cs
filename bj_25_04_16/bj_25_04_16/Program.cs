d68cc23a7ddabbd0c59e68ca292aa30386475453
bluecube97 <jph04071@gmail.com>
2025-04-16 19:14:42 +0900

    세금
    https://www.acmicpc.net/problem/20492

---
+++ bj_25_04_16/bj_25_04_16/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        int a = n * 78 / 100;
+        int b = n - (n * 20 / 100 * 22 / 100);
+
+        Console.WriteLine($"{a} {b}");
+    }
+}
