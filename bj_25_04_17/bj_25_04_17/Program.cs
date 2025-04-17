2c2404c59c9af27fda196f821b51ea70dab4ef6b
bluecube97 <jph04071@gmail.com>
2025-04-17 20:04:25 +0900

    팰린드롬
    https://www.acmicpc.net/problem/13235

---
+++ bj_25_04_17/bj_25_04_17/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string str = isr.ReadLine();
+        int len = str.Length;
+
+        string front;
+        if (len % 2 == 0)
+        {
+            front = str.Substring(0, len / 2);
+        }
+        else
+        {
+            front = str.Substring(0, len / 2 + 1);
+        }
+
+        string back = str.Substring(len / 2, len - len / 2);
+        back = new string(back.Reverse().ToArray());
+
+        if (front == back)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+    }
+}
