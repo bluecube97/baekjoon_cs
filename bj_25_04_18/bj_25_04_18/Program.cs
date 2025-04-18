af79d2a7474d2fd156b622867228f99c20e1edf8
bluecube97 <jph04071@gmail.com>
2025-04-18 19:53:05 +0900

    이동하기 3
    https://www.acmicpc.net/problem/18795

---
+++ bj_25_04_18/bj_25_04_18/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        long sum = 0;
+        long[] a = isr.ReadLine().Split().Select(long.Parse).ToArray();
+        long[] b = isr.ReadLine().Split().Select(long.Parse).ToArray();
+
+        sum += a.Sum();
+        sum += b.Sum();
+
+        Console.WriteLine(sum);
+    }
+}
