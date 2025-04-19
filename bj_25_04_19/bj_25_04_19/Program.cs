799a86781d1cb6deef772c786008615dc5b62b03
bluecube97 <jph04071@gmail.com>
2025-04-19 20:19:23 +0900

    사과
    https://www.acmicpc.net/problem/10833

---
+++ bj_25_04_19/bj_25_04_19/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int people = input[0];
+            int apple = input[1];
+
+            sum += apple % people;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
