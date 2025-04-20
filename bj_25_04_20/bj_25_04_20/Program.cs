d73d550e93453663ab9c08674c625bec3bb119b4
bluecube97 <jph04071@gmail.com>
2025-04-20 21:41:33 +0900

    퐁당퐁당 1
    https://www.acmicpc.net/problem/17944

---
+++ bj_25_04_20/bj_25_04_20/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nt = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nt[0];
+        int t = nt[1];
+
+        int cycle = 4 * n - 2;
+        int where = (t - 1) % cycle;
+
+        int result;
+        if (where < 2 * n - 1)
+        {
+            result = where + 1;
+        }
+        else
+        {
+            result = 2 * n - (where - (2 * n - 1));
+        }
+
+        Console.WriteLine(result);
+    }
+}
