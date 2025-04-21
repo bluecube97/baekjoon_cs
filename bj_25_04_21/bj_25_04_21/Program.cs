ceaa2fa989e4852cfc69bb9b87363476784bd1d4
bluecube97 <jph04071@gmail.com>
2025-04-21 23:08:01 +0900

    방학 숙제
    https://www.acmicpc.net/problem/5532

---
+++ bj_25_04_21/bj_25_04_21/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int l = int.Parse(isr.ReadLine());
+        int a = int.Parse(isr.ReadLine());
+        int b = int.Parse(isr.ReadLine());
+        int c = int.Parse(isr.ReadLine());
+        int d = int.Parse(isr.ReadLine());
+
+        int kor = 0;
+        int math = 0;
+
+        if (a % c == 0)
+        {
+            kor = a / c;
+        }
+        else
+        {
+            kor = a / c + 1;
+        }
+
+        if (b % d == 0)
+        {
+            math = b / d;
+        }
+        else
+        {
+            math = b / d + 1;
+        }
+
+        Console.WriteLine(l - Math.Max(kor, math));
+    }
+}
