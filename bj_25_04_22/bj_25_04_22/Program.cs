dd005bcc3ce0bab317e4eb140f6273e7ad914a64
bluecube97 <jph04071@gmail.com>
2025-04-22 21:50:46 +0900

    운동장 한 바퀴
    https://www.acmicpc.net/problem/16486

---
+++ bj_25_04_22/bj_25_04_22/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int d1 = int.Parse(isr.ReadLine());
+        int d2 = int.Parse(isr.ReadLine());
+
+        double len = d1 * 2 + 2 * d2 * 3.141592;
+
+        Console.WriteLine("{0:F6}", len);
+    }
+}
