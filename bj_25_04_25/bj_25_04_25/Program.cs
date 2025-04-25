a14f56a416bfc616afce7fdf70a1eb5bdbb78869
bluecube97 <jph04071@gmail.com>
2025-04-26 00:52:24 +0900

    연길이의 이상형
    https://www.acmicpc.net/problem/20540

---
+++ bj_25_04_25/bj_25_04_25/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string mbti = isr.ReadLine();
+
+        if (mbti[0] == 'E')
+        {
+            Console.Write("I");
+        }
+        if (mbti[0] == 'I')
+        {
+            Console.Write("E");
+        }
+        if (mbti[1] == 'S')
+        {
+            Console.Write("N");
+        }
+        if (mbti[1] == 'N')
+        {
+            Console.Write("S");
+        }
+        if (mbti[2] == 'T')
+        {
+            Console.Write("F");
+        }
+        if (mbti[2] == 'F')
+        {
+            Console.Write("T");
+        }
+        if (mbti[3] == 'J')
+        {
+            Console.Write("P");
+        }
+        if (mbti[3] == 'P')
+        {
+            Console.Write("J");
+        }
+        Console.WriteLine();
+    }
+}
