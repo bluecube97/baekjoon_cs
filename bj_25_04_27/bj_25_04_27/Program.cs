1423e80073a0b40b2c5ad77c3ca344599caac299
bluecube97 <jph04071@gmail.com>
2025-04-27 20:09:23 +0900

    홀짝 칵테일
    https://www.acmicpc.net/problem/21312

---
+++ bj_25_04_27/bj_25_04_27/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] drink = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = drink[0];
+        int b = drink[1];
+        int c = drink[2];
+
+        int[] candidates = new int[]
+        {
+            a, b, c,
+            a * b, a * c, b * c,
+            a * b * c
+        };
+
+        int bestOdd = candidates.Where(x => x % 2 == 1).DefaultIfEmpty(0).Max();
+
+        if (bestOdd > 0)
+        {
+            Console.WriteLine(bestOdd);
+            return;
+        }
+
+        int bestEven = candidates.Max();
+        Console.WriteLine(bestEven);
+    }
+}
