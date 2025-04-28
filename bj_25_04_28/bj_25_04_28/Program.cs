d8c0e4053162e63083ad75c28f73ec3d13720b03
bluecube97 <jph04071@gmail.com>
2025-04-28 20:18:56 +0900

    명장 남정훈
    https://www.acmicpc.net/problem/15734

---
+++ bj_25_04_28/bj_25_04_28/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int l = input[0];
+        int r = input[1];
+        int a = input[2];
+
+        for (int i = 0; i < a; i++)
+        {
+            if (l > r)
+            {
+                r++;
+            }
+            else
+            {
+                l++;
+            }
+        }
+
+        if (l > r)
+        {
+            Console.WriteLine(r * 2);
+        }
+        else
+        {
+            Console.WriteLine(l * 2);
+        }
+    }
+}
