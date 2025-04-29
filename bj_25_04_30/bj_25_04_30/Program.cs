cd8c19565df2518f5917d8fdd1cba606fcb28377
bluecube97 <jph04071@gmail.com>
2025-04-30 06:48:42 +0900

    Gauß
    https://www.acmicpc.net/problem/7523

---
+++ bj_25_04_30/bj_25_04_30/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int testcase = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < testcase; i++)
+        {
+            Console.WriteLine("Scenario #{0}:", i + 1);
+            int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            BigInteger n = nm[0];
+            BigInteger m = nm[1];
+
+            BigInteger sum = (n + m) * (m - n + 1) / 2;
+            Console.WriteLine(sum);
+            Console.WriteLine();
+        }
+    }
+}
