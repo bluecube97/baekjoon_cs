dbdce9e4665aa9561df5c9583d7d2b375d7137c2
bluecube97 <jph04071@gmail.com>
2025-04-29 19:45:18 +0900

    공 넣기
    https://www.acmicpc.net/problem/10810

---
+++ bj_25_04_29/bj_25_04_29/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        int[] basket = new int[n + 1];
+
+        for (int a = 0; a < m; a++)
+        {
+            int[] ijk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int i = ijk[0];
+            int j = ijk[1];
+            int k = ijk[2];
+
+            for (int b = i; b <= j; b++)
+            {
+                basket[b] = k;
+            }
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(basket[i] + " ");
+        }
+    }
+}
