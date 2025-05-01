14843a4f92e4953787a19f7512c71b25a2a2dd4d
bluecube97 <jph04071@gmail.com>
2025-05-01 10:05:51 +0900

    오버플로우와 모듈러
    https://www.acmicpc.net/problem/15818

---
+++ bj_25_05_01/bj_25_05_01/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        long result = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            result = result * (input[i] % m) % m;
+        }
+
+        Console.WriteLine(result);
+    }
+}
