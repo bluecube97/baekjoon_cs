45862e5a4487527f8b10760bd1351ebe40e4f90b
bluecube97 <jph04071@gmail.com>
2025-05-02 20:49:00 +0900

    이교수님의 시험
    https://www.acmicpc.net/problem/10874

---
+++ bj_25_05_02/bj_25_05_02/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] corrAns = new int[10];
+
+        for (int i = 1; i <= 10; i++)
+        {
+            corrAns[i - 1] = (i - 1) % 5 + 1;
+        }
+
+        int n = int.Parse(isr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int[] answer = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            if (answer.SequenceEqual(corrAns))
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+    }
+}
