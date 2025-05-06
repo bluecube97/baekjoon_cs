3932e1e928ef48307c62da7421211aee509b4dec
bluecube97 <jph04071@gmail.com>
2025-05-07 08:07:48 +0900

    등교
    https://www.acmicpc.net/problem/31962

---
+++ bj_25_05_07/bj_25_05_07/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nx = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nx[0];
+        int x = nx[1];
+
+        int max = int.MinValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] st = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int s = st[0];
+            int t = st[1];
+
+            if (s + t <= x)
+            {
+                max = Math.Max(max, s);
+            }
+        }
+
+        if (max == int.MinValue)
+        {
+            Console.WriteLine(-1);
+        }
+        else
+        {
+            Console.WriteLine(max);
+        }
+    }
+}
