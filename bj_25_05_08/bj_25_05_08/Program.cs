6259000c1291b9b33515ae90ff37a9ce8ed1ee6a
bluecube97 <jph04071@gmail.com>
2025-05-08 07:07:18 +0900

    왜 맘대로 예약하냐고
    https://www.acmicpc.net/problem/32941

---
+++ bj_25_05_08/bj_25_05_08/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] tx = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int t = tx[0];
+        int x = tx[1];
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int k = int.Parse(isr.ReadLine());
+            int[] time = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+            if (!time.Contains(x))
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+        }
+
+        Console.WriteLine("YES");
+    }
+}
