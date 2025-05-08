5b9c5e3950fcb8316d9d1f73346fa12f8354d40f
bluecube97 <jph04071@gmail.com>
2025-05-09 08:02:12 +0900

    별 찍기 - 15
    https://www.acmicpc.net/problem/10990

---
+++ bj_25_05_09/bj_25_05_09/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                Console.Write(" ");
+            }
+
+            Console.Write("*");
+
+            if (i != 0)
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < i - 1; j++)
+            {
+                Console.Write("  ");
+            }
+
+            if (i != 0)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
