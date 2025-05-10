0b335a24be9d9c3c023afd0a4f7e0bd0bf8fd5e9
bluecube97 <jph04071@gmail.com>
2025-05-11 02:02:52 +0900

    모형결정
    https://www.acmicpc.net/problem/18127

---
+++ bj_25_05_10/bj_25_05_10/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = ab[0];
+        int b = ab[1];
+
+        long sum = 1;
+
+        for (int i = 1; i <= b; i++)
+        {
+            sum += (a - 2) * i + 1;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
