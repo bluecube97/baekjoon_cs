68182ddf9ac796230db2e1a6b1c98262c561e771
bluecube97 <jph04071@gmail.com>
2025-05-11 21:12:35 +0900

    현대모비스와 함께하는 부품 관리
    https://www.acmicpc.net/problem/24724

---
+++ bj_25_05_11/bj_25_05_11/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(isr.ReadLine());
+            int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = ab[0];
+            int b = ab[1];
+
+            for (int j = 0; j < n; j++)
+            {
+                int[] uv = isr.ReadLine().Split().Select(int.Parse).ToArray();
+                int u = uv[0];
+                int v = uv[1];
+            }
+
+            Console.WriteLine("Material Management {0}", i + 1);
+            Console.WriteLine("Classification ---- End!");
+        }
+    }
+}
