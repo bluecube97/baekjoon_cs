1fb90f80d9aa9b356c38da817844908e4f0bd604
bluecube97 <jph04071@gmail.com>
2025-05-12 07:12:06 +0900

    주차 요금 정산하기
    https://www.acmicpc.net/problem/33753

---
+++ bj_25_05_12/bj_25_05_12/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] abc = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = abc[0];
+        int b = abc[1];
+        int c = abc[2];
+        int t = int.Parse(isr.ReadLine());
+
+        if (t <= 30)
+        {
+            Console.WriteLine(a);
+        }
+        else
+        {
+            int time = t - 30;
+            int charge = a + time / b * c;
+
+            if (time % b != 0)
+            {
+                charge += c;
+            }
+
+            Console.WriteLine(charge);
+        }
+    }
+}
