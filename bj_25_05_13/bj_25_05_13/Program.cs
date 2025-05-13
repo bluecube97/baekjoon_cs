023e05f7d6b1b5869df001c7b7d3a5e45ae05df5
bluecube97 <jph04071@gmail.com>
2025-05-13 23:12:03 +0900

    PNUPC에 한 번도 빠지지 않고 출연한 산지니가 새삼 대단하다고 느껴지네
    https://www.acmicpc.net/problem/33845

---
+++ bj_25_05_13/bj_25_05_13/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string s = isr.ReadLine();
+        string t = isr.ReadLine();
+
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (!s.Contains(t[i]))
+            {
+                Console.Write(t[i]);
+            }
+        }
+    }
+}
