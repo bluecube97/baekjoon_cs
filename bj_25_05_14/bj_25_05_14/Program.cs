6dc4195b7696026c80b86d04ad7b7d8d1264e9ba
bluecube97 <jph04071@gmail.com>
2025-05-14 21:17:48 +0900

    카드 뽑기
    https://www.acmicpc.net/problem/16204

---
+++ bj_25_05_14/bj_25_05_14/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nmk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nmk[0];
+        int m = nmk[1];
+        int k = nmk[2];
+
+        Console.WriteLine(Math.Min(m, k) + Math.Min(n - m, n - k));
+    }
+}
