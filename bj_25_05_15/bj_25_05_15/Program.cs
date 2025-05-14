93dd9f38a9c111783147761cd8e6ebf8eaace525
bluecube97 <jph04071@gmail.com>
2025-05-15 08:06:15 +0900

    Goodbye, Code Jam
    https://www.acmicpc.net/problem/29738

---
+++ bj_25_05_15/bj_25_05_15/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(isr.ReadLine());
+            if (n <= 25)
+            {
+                Console.WriteLine("Case #{0}: World Finals", i + 1);
+            }
+            else if (n <= 1000)
+            {
+                Console.WriteLine("Case #{0}: Round {1}", i + 1, 3);
+            }
+            else if (n <= 4500)
+            {
+                Console.WriteLine("Case #{0}: Round {1}", i + 1, 2);
+            }
+            else
+            {
+                Console.WriteLine("Case #{0}: Round {1}", i + 1, 1);
+            }
+        }
+    }
+}
