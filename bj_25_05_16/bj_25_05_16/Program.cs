a2a17527a2f7542f2af5cb660e23fb688efb061a
bluecube97 <jph04071@gmail.com>
2025-05-16 08:12:05 +0900

    더하기 4
    https://www.acmicpc.net/problem/11024

---
+++ bj_25_05_16/bj_25_05_16/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int[] arr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(arr.Sum());
+        }
+    }
+}
