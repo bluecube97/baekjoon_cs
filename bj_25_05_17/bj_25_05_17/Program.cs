5d77eae8a3f597095ede7eb25b7c4c7a4ba3ccd8
bluecube97 <jph04071@gmail.com>
2025-05-17 21:03:26 +0900

    가장 많은 글자
    https://www.acmicpc.net/problem/1371

---
+++ bj_25_05_17/bj_25_05_17/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] count = new int[26];
+        string line;
+
+        while ((line = isr.ReadLine()) != null)
+        {
+            foreach (var c in line)
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    count[c - 'a']++;
+                }
+            }
+        }
+
+        int max = int.MinValue;
+        foreach (var i in count)
+        {
+            max = Math.Max(max, i);
+        }
+
+        for (int i = 0; i < count.Length; i++)
+        {
+            if (count[i] == max)
+            {
+                Console.Write((char)(i + 'a'));
+            }
+        }
+    }
+}
