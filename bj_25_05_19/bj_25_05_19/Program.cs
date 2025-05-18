bce0bfb680fb50d36b8b0cf26310e973727d713e
bluecube97 <jph04071@gmail.com>
2025-05-19 07:46:54 +0900

    Card Game Contest
    https://www.acmicpc.net/problem/14551

---
+++ bj_25_05_19/bj_25_05_19/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        int answer = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            int a = int.Parse(isr.ReadLine());
+
+            if (a == 0)
+            {
+                continue;
+            }
+
+            answer *= a;
+            answer %= m;
+        }
+
+        answer %= m;
+        Console.WriteLine(answer);
+    }
+}
