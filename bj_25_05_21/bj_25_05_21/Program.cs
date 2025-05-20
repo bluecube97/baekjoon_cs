1d64055c47218a77b8c11cc4c377aab09a44f876
bluecube97 <jph04071@gmail.com>
2025-05-21 06:30:04 +0900

    승강장의 깊이
    https://www.acmicpc.net/problem/32193

---
+++ bj_25_05_21/bj_25_05_21/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        StringBuilder sb = new StringBuilder();
+
+        int n = int.Parse(isr.ReadLine());
+        int A = 0;
+        int B = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = ab[0];
+            int b = ab[1];
+
+            A += a;
+            B += b;
+
+            sb.Append(A - B);
+            sb.AppendLine();
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
