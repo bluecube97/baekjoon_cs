c5ba36ce077a141f11b8e87c4baddf1335bfa810
bluecube97 <jph04071@gmail.com>
2025-05-21 04:12:57 +0900

    네 번째 수
    https://www.acmicpc.net/problem/2997

---
+++ bj_25_05_20/bj_25_05_20/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] num = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        Array.Sort(num);
+
+        if (num[2] - num[1] == num[1] - num[0])
+        {
+            Console.WriteLine(num[2] + (num[2] - num[1]));
+        }
+        else
+        {
+            int diff = Math.Min(num[2] - num[1], num[1] - num[0]);
+
+            if (num[2] - num[1] == diff)
+            {
+                Console.WriteLine(num[0] + diff);
+            }
+            else
+            {
+                Console.WriteLine(num[2] - diff);
+            }
+        }
+    }
+}
