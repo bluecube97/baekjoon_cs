403a588b5386c29c7a393b80d1c0d107e293ef7c
bluecube97 <jph04071@gmail.com>
2025-05-22 08:26:22 +0900

    !!초콜릿 중독 주의!!
    https://www.acmicpc.net/problem/31458

---
+++ bj_25_05_22/bj_25_05_22/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        StringBuilder sb = new StringBuilder();
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string exp = isr.ReadLine();
+            int logicalNotCount = 0;
+            int factorialCount = 0;
+            int num = 0;
+            bool isFactorial = false;
+
+            foreach (var el in exp)
+            {
+                if (el == '!' && !isFactorial)
+                {
+                    logicalNotCount++;
+                }
+                else if (el == '!' && isFactorial)
+                {
+                    factorialCount++;
+                }
+                else
+                {
+                    num = el == '0' ? 0 : 1;
+                    isFactorial = true;
+                }
+            }
+
+            if (factorialCount > 0)
+            {
+                num = 1;
+            }
+
+            if (logicalNotCount % 2 != 0)
+            {
+                num = num == 0 ? 1 : 0;
+            }
+
+            sb.Append(num);
+            sb.AppendLine();
+        }
+
+        Console.Write(sb.ToString());
+    }
+}
