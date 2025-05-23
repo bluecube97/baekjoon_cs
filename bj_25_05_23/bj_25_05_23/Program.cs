86697a5a5cf821fb1d92b67ed98db32eaf909b1e
bluecube97 <jph04071@gmail.com>
2025-05-23 21:36:33 +0900

    점심시간 레이스
    https://www.acmicpc.net/problem/28236

---
+++ bj_25_05_23/bj_25_05_23/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nmk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nmk[0];
+        int m = nmk[1];
+        int k = nmk[2];
+
+        int[] classes = new int[k];
+
+        for (int i = 0; i < k; i++)
+        {
+            int[] fd = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int f = fd[0];
+            int d = fd[1];
+
+            classes[i] = m - d + f;
+        }
+
+        int minTime = int.MaxValue;
+        int minIndex = -1;
+
+        for (int i = 0; i < k; i++)
+        {
+            if (minTime > classes[i])
+            {
+                minTime = classes[i];
+                minIndex = i;
+            }
+        }
+
+        Console.WriteLine(minIndex + 1);
+    }
+}
