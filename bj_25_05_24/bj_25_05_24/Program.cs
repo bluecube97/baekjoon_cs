4f4e50d81f0d2bd18e8b357410ef411446bc5187
bluecube97 <jph04071@gmail.com>
2025-05-24 18:50:37 +0900

    타일
    https://www.acmicpc.net/problem/5556

---
+++ bj_25_05_24/bj_25_05_24/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int k = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < k; i++)
+        {
+            int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = ab[0];
+            int b = ab[1];
+
+            int level = Math.Min(Math.Min(a - 1, b - 1), Math.Min(n - a, n - b));
+            int color = level % 3 + 1;
+
+            Console.WriteLine(color);
+        }
+    }
+}
