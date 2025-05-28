2a3fc40ec30d2a56fc0f891be4ff1c48747ad25d
bluecube97 <jph04071@gmail.com>
2025-05-29 07:08:07 +0900

    욱 제
    https://www.acmicpc.net/problem/17356

---
+++ bj_25_05_29/bj_25_05_29/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = ab[0];
+        int b = ab[1];
+
+        double m = (b - a) / 400.0;
+        double winRate = 1 / (1 + Math.Pow(10, m));
+
+        Console.WriteLine(winRate);
+    }
+}
