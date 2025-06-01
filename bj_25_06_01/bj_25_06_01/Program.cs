c2c2853e877c61043b110e84532927be427d64ce
bluecube97 <jph04071@gmail.com>
2025-06-01 10:39:54 +0900

    앵그리 창영
    https://www.acmicpc.net/problem/3034

---
+++ bj_25_06_01/bj_25_06_01/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nwh = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nwh[0], w = nwh[1], h = nwh[2];
+
+        for (int i = 0; i < n; i++)
+        {
+            int len = int.Parse(isr.ReadLine());
+
+            if (Math.Pow(len, 2) <= Math.Pow(w, 2) + Math.Pow(h, 2))
+            {
+                Console.WriteLine("DA");
+            }
+            else
+            {
+                Console.WriteLine("NE");
+            }
+        }
+    }
+}
