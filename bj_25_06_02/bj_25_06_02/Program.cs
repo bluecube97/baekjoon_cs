7830e1ed0663d74adfb72d578a2617114cb7f6fb
bluecube97 <jph04071@gmail.com>
2025-06-02 08:56:55 +0900

    ABC
    https://www.acmicpc.net/problem/3047

---
+++ bj_25_06_02/bj_25_06_02/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] numArr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        Array.Sort(numArr);
+
+        int a = numArr[0];
+        int b = numArr[1];
+        int c = numArr[2];
+
+        string command = isr.ReadLine();
+
+        foreach (var ch in command)
+        {
+            if (ch == 'A')
+            {
+                Console.Write(a + " ");
+            }
+            else if (ch == 'B')
+            {
+                Console.Write(b + " ");
+            }
+            else if (ch == 'C')
+            {
+                Console.Write(c + " ");
+            }
+        }
+    }
+}
