660e66b88830838e301d78244184686487d3d539
bluecube97 <jph04071@gmail.com>
2025-06-03 11:16:26 +0900

    택시 기하학
    https://www.acmicpc.net/problem/3053

---
+++ bj_25_06_03/bj_25_06_03/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int r = int.Parse(isr.ReadLine());
+
+        Console.WriteLine("{0:F6}", Math.Pow(r, 2) * Math.PI);
+        Console.WriteLine("{0:F6}", 2 * Math.Pow(r, 2));
+    }
+}
