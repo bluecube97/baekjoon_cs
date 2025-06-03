7b723ed8faad6f91c44c7371bd285035ed54ab72
bluecube97 <jph04071@gmail.com>
2025-06-04 05:48:20 +0900

    짝수를 찾아라
    https://www.acmicpc.net/problem/3058

---
+++ bj_25_06_04/bj_25_06_04/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int[] arr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            int min = int.MaxValue;
+
+            foreach (var num in arr)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+
+                    if (num < min)
+                    {
+                        min = num;
+                    }
+                }
+            }
+
+            Console.Write(sum + " ");
+            Console.WriteLine(min);
+        }
+    }
+}
