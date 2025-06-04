9c1efdaed29e3a4d963ba20996551ddb7cadbac6
bluecube97 <jph04071@gmail.com>
2025-06-05 07:17:00 +0900

    큰 수 (BIG)
    https://www.acmicpc.net/problem/14928

---
+++ bj_25_06_05/bj_25_06_05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        BigInteger n = BigInteger.Parse(isr.ReadLine());
+        Console.WriteLine(n % 20000303);
+    }
+}
