29fe39faf4b58bd3a6aa2fdaa44684f936194eaf
bluecube97 <jph04071@gmail.com>
2025-06-06 08:18:26 +0900

    햄버거 정렬
    https://www.acmicpc.net/problem/32399

---
+++ bj_25_06_06/bj_25_06_06/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string s = isr.ReadLine();
+
+        if (s[0] == '(' && s[2] == ')')
+        {
+            Console.WriteLine(0);
+        }
+        else if (s[0] == ')' && s[2] == '(')
+        {
+            Console.WriteLine(2);
+        }
+        else
+        {
+            Console.WriteLine(1);
+        }
+    }
+}
