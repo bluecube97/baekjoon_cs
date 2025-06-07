b2c4cdb77b29ba47ee32ecb65ccb8f6be227774a
bluecube97 <jph04071@gmail.com>
2025-06-07 20:33:07 +0900

    등장하지 않는 문자의 합
    https://www.acmicpc.net/problem/3059

---
+++ bj_25_06_07/bj_25_06_07/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string s = isr.ReadLine();
+            int[] alphabet = new int[26];
+            int sum = 0;
+
+            foreach (var c in s)
+            {
+                alphabet[c - 'A']++;
+            }
+
+            for (int j = 0; j < 26; j++)
+            {
+                if (alphabet[j] == 0)
+                {
+                    sum += j + 'A';
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
