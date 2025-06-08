c2a49d71c40e5e2ea8496c49493f6a08564b83c6
bluecube97 <jph04071@gmail.com>
2025-06-09 07:33:27 +0900

    치킨 쿠폰
    https://www.acmicpc.net/problem/1673

---
+++ bj_25_06_09/bj_25_06_09/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (isr.Peek() != -1)
+        {
+            int[] nk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = nk[0], k = nk[1];
+            int chicken = n;
+
+            while (n > 0)
+            {
+                n -= k;
+                n++;
+                chicken++;
+            }
+
+            Console.WriteLine(--chicken);
+        }
+    }
+}
