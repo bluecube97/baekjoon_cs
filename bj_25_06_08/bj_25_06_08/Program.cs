80c216d21db74c61f4979b4d798fc194061c5b1a
bluecube97 <jph04071@gmail.com>
2025-06-08 22:24:32 +0900

    해밍 거리
    https://www.acmicpc.net/problem/3449

---
+++ bj_25_06_08/bj_25_06_08/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string a = isr.ReadLine();
+            string b = isr.ReadLine();
+
+            int hammingDistance = 0;
+            int len = a.Length;
+
+            for (int j = 0; j < len; j++)
+            {
+                if (a[j] != b[j])
+                {
+                    hammingDistance++;
+                }
+            }
+
+            Console.WriteLine("Hamming distance is {0}.", hammingDistance);
+        }
+    }
+}
