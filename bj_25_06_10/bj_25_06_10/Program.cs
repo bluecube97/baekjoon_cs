a42271431130179267f9ddc82d5fbae9555efafc
bluecube97 <jph04071@gmail.com>
2025-06-10 07:21:29 +0900

    긴급 회의
    https://www.acmicpc.net/problem/20113

---
+++ bj_25_06_10/bj_25_06_10/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] vote = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] voteResult = new int[n + 1];
+
+        for (int i = 0; i < n; i++)
+        {
+            voteResult[vote[i]]++;
+        }
+
+        int maxVoteCount = 0;
+        int maxVote = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (maxVote == voteResult[i])
+            {
+                maxVoteCount++;
+            }
+            else if (maxVote < voteResult[i])
+            {
+                maxVoteCount = 1;
+                maxVote = voteResult[i];
+            }
+        }
+
+        if (maxVoteCount > 1)
+        {
+            Console.WriteLine("skipped");
+        }
+        else
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (voteResult[i] == maxVote)
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+    }
+}
