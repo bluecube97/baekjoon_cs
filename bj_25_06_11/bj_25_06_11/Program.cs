a192db6c225fb146c65a270d7ca7250503a6448b
bluecube97 <jph04071@gmail.com>
2025-06-11 10:02:32 +0900

    팩토리얼 2
    https://www.acmicpc.net/problem/27433

---
+++ bj_25_06_11/bj_25_06_11/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        Console.WriteLine(factorial(n));
+    }
+
+    private static long factorial(int n)
+    {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+}
