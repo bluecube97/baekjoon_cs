0327e1c7762796c65f5e70f6765525a2b29d03db
bluecube97 <jph04071@gmail.com>
2025-06-12 07:34:04 +0900

    SciComLove (2025)
    https://www.acmicpc.net/problem/33810

---
+++ bj_25_06_12/bj_25_06_12/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string s = isr.ReadLine();
+        string trueString = "SciComLove";
+
+        int falseCount = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] != trueString[i])
+            {
+                falseCount++;
+            }
+        }
+
+        Console.WriteLine(falseCount);
+    }
+}
