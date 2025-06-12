6b5b22f38bffd3e55e5d3dc8b6dfb90014d78952
bluecube97 <jph04071@gmail.com>
2025-06-13 07:47:14 +0900

    괄호 없는 사칙연산
    https://www.acmicpc.net/problem/16503

---
+++ bj_25_06_13/bj_25_06_13/Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string[] input = isr.ReadLine().Split().ToArray();
+
+        int a = int.Parse(input[0]), b = int.Parse(input[2]), c = int.Parse(input[4]);
+        string oper1 = input[1], oper2 = input[3];
+
+        int result1, result2;
+
+        result1 = calc(a, b, oper1);
+        result1 = calc(result1, c, oper2);
+
+        result2 = calc(b, c, oper2);
+        result2 = calc(a, result2, oper1);
+
+        if (result1 > result2)
+        {
+            Console.WriteLine(result2);
+            Console.WriteLine(result1);
+        }
+        else
+        {
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+        }
+    }
+
+    private static int calc(int a, int b, string oper)
+    {
+        if (oper == "+")
+        {
+            return a + b;
+        }
+        else if (oper == "-")
+        {
+            return a - b;
+        }
+        else if (oper == "*")
+        {
+            return a * b;
+        }
+        else
+        {
+            return a / b;
+        }
+    }
+}
