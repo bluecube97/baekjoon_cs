30b883f4d7818c2737df75085492cd4761aa5c65
bluecube97 <jph04071@gmail.com>
2025-06-15 07:22:23 +0900

    받아올림
    https://www.acmicpc.net/problem/4388

---
+++ bj_25_06_15/bj_25_06_15/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            string[] input = isr.ReadLine().Split();
+            string a = input[0];
+            string b = input[1];
+
+            if (a == "0" && b == "0")
+            {
+                break;
+            }
+
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            int carry = 0;
+            int carryCount = 0;
+
+            while (i >= 0 || j >= 0)
+            {
+                int digitA = i >= 0 ? a[i] - '0' : 0;
+                int digitB = j >= 0 ? b[j] - '0' : 0;
+
+                if (digitA + digitB + carry >= 10)
+                {
+                    carryCount++;
+                    carry = 1;
+                }
+                else
+                {
+                    carry = 0;
+                }
+
+                i--;
+                j--;
+            }
+
+            Console.WriteLine(carryCount);
+        }
+    }
+}
