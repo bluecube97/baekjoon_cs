cd009ed0a38d4ea2c6de37648428aab7406ebf2b
bluecube97 <jph04071@gmail.com>
2025-06-16 06:48:42 +0900

    첫 글자를 대문자로
    https://www.acmicpc.net/problem/4458

---
+++ bj_25_06_16/bj_25_06_16/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine()!);
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = isr.ReadLine();
+            if (line[0] >= 'A' && line[0] <= 'Z')
+            {
+                Console.Write(line[0]);
+            }
+            else
+            {
+                Console.Write((char)(line[0] - ('a' - 'A')));
+            }
+            Console.WriteLine(line[1..]);
+        }
+    }
+}
