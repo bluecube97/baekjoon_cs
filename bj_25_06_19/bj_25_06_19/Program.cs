03efd0553377f64033fb2e9568609bcc727c15a8
bluecube97 <jph04071@gmail.com>
2025-06-19 07:33:45 +0900

    맞았는데 왜 틀리죠?
    https://www.acmicpc.net/problem/15820

---
+++ bj_25_06_19/bj_25_06_19/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] testCase = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int s1 = testCase[0];
+        int s2 = testCase[1];
+
+        for (int i = 0; i < s1; i++)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int cor = input[0];
+            int ans = input[1];
+
+            if (cor != ans)
+            {
+                Console.WriteLine("Wrong Answer");
+                return;
+            }
+        }
+
+        for (int i = 0; i < s2; i++)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int cor = input[0];
+            int ans = input[1];
+
+            if (cor != ans)
+            {
+                Console.WriteLine("Why Wrong!!!");
+                return;
+            }
+        }
+
+        Console.WriteLine("Accepted");
+    }
+}
