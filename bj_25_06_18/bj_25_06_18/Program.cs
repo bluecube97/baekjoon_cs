f42d9a61919ab566fa1c91b67d5962ba525b056e
bluecube97 <jph04071@gmail.com>
2025-06-18 09:14:53 +0900

    스위트콘 가격 구하기
    https://www.acmicpc.net/problem/30030

---
+++ bj_25_06_18/bj_25_06_18/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int b = int.Parse(isr.ReadLine());
+        Console.WriteLine((int)(b * 10 / 11));
+    }
+}
