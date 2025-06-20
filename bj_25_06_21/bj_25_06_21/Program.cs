acb034ee2d00ce22be5818153765657d13eb91a2
bluecube97 <jph04071@gmail.com>
2025-06-21 07:47:51 +0900

    홀짝 수열
    https://www.acmicpc.net/problem/25629

---
+++ bj_25_06_21/bj_25_06_21/Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] arr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int odd = 0; // 홀수
+        int even = 0; // 짝수
+
+        foreach (var el in arr)
+        {
+            if (el % 2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+
+        if (n % 2 == 0)
+        {
+            if (odd == even)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+        else
+        {
+            if (odd == even + 1)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+
+    }
+}
