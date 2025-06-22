c1d5f9fd28e50abdd71f42fbc0b30ccda4dca12e
bluecube97 <jph04071@gmail.com>
2025-06-23 08:16:02 +0900

    일반 화학 실험
    https://www.acmicpc.net/problem/4766

---
+++ bj_25_06_23/bj_25_06_23/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        float prev = -20;
+
+        while (true)
+        {
+            float temp = float.Parse(isr.ReadLine());
+
+            if (temp == 999)
+            {
+                break;
+            }
+
+            if (prev != -20)
+            {
+                Console.WriteLine("{0:F2}", temp - prev);
+            }
+            prev = temp;
+        }
+    }
+}
