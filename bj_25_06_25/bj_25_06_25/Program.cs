ee968aa5251f9e8f21e8ad339ec29e2916b47f5f
bluecube97 <jph04071@gmail.com>
2025-06-25 21:38:55 +0900

    제리와 톰
    https://www.acmicpc.net/problem/16430

---
+++ bj_25_06_25/bj_25_06_25/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = input[0];
+        int b = input[1];
+
+        Console.WriteLine("{0} {1}", b - a, b);
+    }
+}
