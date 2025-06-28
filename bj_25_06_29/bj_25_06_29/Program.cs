3312b01a4499c1a1c883824fe4de3685485736ef
bluecube97 <jph04071@gmail.com>
2025-06-29 04:12:08 +0900

    배수 찾기
    https://www.acmicpc.net/problem/4504

---
+++ bj_25_06_29/bj_25_06_29/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        while (true)
+        {
+            int num = int.Parse(isr.ReadLine());
+            if (num == 0)
+                break;
+
+            if (num % n != 0)
+            {
+                Console.WriteLine("{0} is NOT a multiple of {1}.", num, n);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a multiple of {1}.", num, n);
+            }
+        }
+    }
+}
