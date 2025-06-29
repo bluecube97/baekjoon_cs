53bfcfcc601f78d3ab3390d2dad42d0b3f6548de
bluecube97 <jph04071@gmail.com>
2025-06-30 07:44:36 +0900

    거울, 오! 거울
    https://www.acmicpc.net/problem/4740

---
+++ bj_25_06_30/bj_25_06_30/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            string str = isr.ReadLine();
+            if (str == "***")
+            {
+                break;
+            }
+
+            str = new string(str.Reverse().ToArray());
+
+            Console.WriteLine(str);
+        }
+    }
+}
