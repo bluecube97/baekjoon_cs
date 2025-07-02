12731f765f6e15148a255de2d11554cbc9a7c673
bluecube97 <jph04071@gmail.com>
2025-07-02 09:21:13 +0900

    17배
    https://www.acmicpc.net/problem/5893

---
+++ bj_25_07_02/bj_25_07_02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Numerics;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string binary = isr.ReadLine();
+
+        BigInteger decimalNum = BigInteger.Zero;
+        foreach (var c in binary)
+        {
+            decimalNum <<= 1;
+            if (c == '1')
+            {
+                decimalNum += 1;
+            }
+        }
+
+        BigInteger result = decimalNum * 17;
+
+        if (result.IsZero)
+        {
+            Console.WriteLine("0");
+            return;
+        }
+
+        var sb = new StringBuilder();
+        while (result > 0)
+        {
+            sb.Insert(0, (result % 2).ToString());
+            result /= 2;
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
