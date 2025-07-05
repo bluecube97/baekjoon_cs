c628cdd54373ae8f0fd85c5d56f2314719a29c5e
bluecube97 <jph04071@gmail.com>
2025-07-05 10:14:43 +0900

    삼각형과 세 변
    https://www.acmicpc.net/problem/5073

---
+++ bj_25_07_05/bj_25_07_05/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+            int c = input[2];
+
+            if (a == 0 && b == 0 && c == 0)
+                break;
+
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                if (a == b && b == c)
+                {
+                    Console.WriteLine("Equilateral");
+                }
+                else if (a == b || b == c || a == c)
+                {
+                    Console.WriteLine("Isosceles");
+                }
+                else
+                {
+                    Console.WriteLine("Scalene");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
