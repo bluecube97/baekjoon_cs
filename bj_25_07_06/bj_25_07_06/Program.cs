c93fea9f091a7d6d9de051a6ff02c5d67ac7e337
bluecube97 <jph04071@gmail.com>
2025-07-06 08:09:12 +0900

    줄 세기
    https://www.acmicpc.net/problem/4806

---
+++ bj_25_07_06/bj_25_07_06/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        int count = 0;
+
+        while (isr.Peek() != -1)
+        {
+            isr.ReadLine();
+            count++;
+        }
+
+        Console.WriteLine(count);
+    }
+}
