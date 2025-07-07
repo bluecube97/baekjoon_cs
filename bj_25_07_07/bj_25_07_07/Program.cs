4e29f91fcf8549b9670616cd9bd485d0ea754d8d
bluecube97 <jph04071@gmail.com>
2025-07-07 10:21:44 +0900

    루트
    https://www.acmicpc.net/problem/4619

---
+++ bj_25_07_07/bj_25_07_07/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            int[] bn = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int b = bn[0];
+            int n = bn[1];
+
+            if (b == 0 && n == 0)
+                break;
+            
+            int prev = 0;
+
+            for (int i = 1; i <= 1000000; i++)
+            {
+                int pow = (int) Math.Pow(i, n);
+                if (b <= pow)
+                {
+                    if (Math.Abs(prev - b) > Math.Abs(pow - b))
+                    {
+                        Console.WriteLine(i);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine(i - 1);
+                        break;
+                    }
+                }
+                prev = pow;
+            }
+        }
+    }
+}
