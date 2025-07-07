4fdb0d92c7d85ced7e847fede6af4d9a4647e3a6
bluecube97 <jph04071@gmail.com>
2025-07-08 07:30:12 +0900

    터널의 입구와 출구
    https://www.acmicpc.net/problem/5612

---
+++ bj_25_07_08/bj_25_07_08/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int m = int.Parse(isr.ReadLine());
+        int max = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] car = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int input = car[0];
+            int output = car[1];
+
+            int change = input - output;
+
+            if (m + change < 0)
+            {
+                Console.WriteLine(0);
+                break;
+            }
+
+            m += change;
+            max = Math.Max(max, m);
+        }
+
+        Console.WriteLine(max);
+    }
+}
---
+++ bj_25_07_08_1/bj_25_07_08_1/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        //var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int max = m;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] car = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int input = car[0];
+            int output = car[1];
+
+            int change = input - output;
+
+            if (m + change < 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            m += change;
+            max = Math.Max(max, m);
+        }
+
+        Console.WriteLine(max);
+    }
+}
