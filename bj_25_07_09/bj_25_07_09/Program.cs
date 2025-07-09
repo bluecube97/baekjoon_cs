1cb4f157465f4fcab2d36ded2148bed6bd3c905f
bluecube97 <jph04071@gmail.com>
2025-07-09 22:08:30 +0900

    골뱅이 찍기 - 돌아간 ㄹ
    https://www.acmicpc.net/problem/23805

---
+++ bj_25_07_09/bj_25_07_09/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        string top = new string('@', 3 * n) + new string(' ', n) + new string('@', n);
+        string middle = new string('@', n) + new string(' ', n) + new string('@', n) + new string(' ', n) + new string('@', n);
+        string bottom = new string('@', n) + new string(' ', n) + new string('@', 3 * n);
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(top);
+        }
+
+        for (int i = 0; i < 3 * n; i++)
+        {
+            Console.WriteLine(middle);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(bottom);
+        }
+    }
+}
