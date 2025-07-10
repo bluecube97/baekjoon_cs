b2a78d02e6cb25399de1a4f022ff68d37c676f8f
bluecube97 <jph04071@gmail.com>
2025-07-11 07:29:15 +0900

    과민성 대장 증후군
    https://www.acmicpc.net/problem/31831

---
+++ bj_25_07_11/bj_25_07_11/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        int stress = 0;
+        int stomach = 0;
+
+        int[] A = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        for (int i = 0; i < n; i++)
+        {
+            stress += A[i];
+            if (stress < 0)
+            {
+                stress = 0;
+            }
+
+            if (stress >= m)
+            {
+                stomach++;
+            }
+        }
+
+        Console.WriteLine(stomach);
+    }
+}
