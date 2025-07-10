32b22a73e77fccf60b9c962490e96b0ee6369df1
bluecube97 <jph04071@gmail.com>
2025-07-10 11:54:09 +0900

    빅데이터? 정보보호!
    https://www.acmicpc.net/problem/26264

---
+++ bj_25_07_10/bj_25_07_10/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string answer = isr.ReadLine();
+
+        int bigdata = answer.Count(c => c == 'b');
+        int security = answer.Count(c => c == 's');
+
+        if (bigdata == security)
+        {
+            Console.WriteLine("bigdata? security!");
+            return;
+        }
+        Console.WriteLine(bigdata > security ? "bigdata?" : "security!");
+    }
+}
