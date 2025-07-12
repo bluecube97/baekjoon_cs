0630c32c4f8e0523d9f08924603a58a181af49bc
bluecube97 <jph04071@gmail.com>
2025-07-13 00:03:05 +0900

    파인만
    https://www.acmicpc.net/problem/5724

---
+++ bj_25_07_12/bj_25_07_12/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            int n = int.Parse(isr.ReadLine());
+
+            // 1^2 / 2^2 + 1^2 / 3^2 + 2^2 + 1^2
+            if (n == 0)
+                break;
+
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i * i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
