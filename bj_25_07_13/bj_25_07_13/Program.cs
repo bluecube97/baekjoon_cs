d770af19792ba9b3fed40abb069ee8b09346b345
bluecube97 <jph04071@gmail.com>
2025-07-13 11:30:29 +0900

    직각 삼각형의 두 변
    https://www.acmicpc.net/problem/6322

---
+++ bj_25_07_13/bj_25_07_13/Program.cs
@@ -0,0 +1,72 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        int i = 1;
+
+        while (true)
+        {
+            int[] abc = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            if (abc[0] == 0 && abc[1] == 0 && abc[2] == 0)
+                break;
+
+            if (i != 1)
+                Console.WriteLine();
+
+            Console.WriteLine("Triangle #{0}", i);
+            i++;
+
+            int a = abc[0];
+            int b = abc[1];
+            int c = abc[2];
+
+            if (a == -1)
+            {
+                if (c <= b)
+                {
+                    Console.WriteLine("Impossible.");
+                    continue;
+                }
+                double val = Math.Sqrt(c * c - b * b);
+                if (double.IsNaN(val) || val <= 0)
+                {
+                    Console.WriteLine("Impossible.");
+                }
+                else
+                {
+                    Console.WriteLine($"a = {val:F3}");
+                }
+            }
+            else if (b == -1)
+            {
+                if (c <= a)
+                {
+                    Console.WriteLine("Impossible.");
+                    continue;
+                }
+                double val = Math.Sqrt(c * c - a * a);
+                if (double.IsNaN(val) || val <= 0)
+                {
+                    Console.WriteLine("Impossible.");
+                }
+                else
+                {
+                    Console.WriteLine($"b = {val:F3}");
+                }
+            }
+            else if (c == -1)
+            {
+                double val = Math.Sqrt(a * a + b * b);
+                if (double.IsNaN(val) || val <= 0)
+                {
+                    Console.WriteLine("Impossible.");
+                }
+                else
+                {
+                    Console.WriteLine($"c = {val:F3}");
+                }
+            }
+        }
+    }
+}
