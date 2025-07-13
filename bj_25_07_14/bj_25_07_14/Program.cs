4abdbf1628eb8ec39b2fa35440fddf1c71732fea
bluecube97 <jph04071@gmail.com>
2025-07-14 05:33:44 +0900

    IBM 빼기 1
    https://www.acmicpc.net/problem/6321

---
+++ bj_25_07_14/bj_25_07_14/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("String #{0}", i + 1);
+            string name = isr.ReadLine();
+
+            foreach (var c in name)
+            {
+                if (c == 'Z')
+                {
+                    Console.Write('A');
+                }
+                else
+                {
+                    Console.Write((char)(c + 1));
+                }
+            }
+            Console.WriteLine();
+            if (i < n - 1)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
