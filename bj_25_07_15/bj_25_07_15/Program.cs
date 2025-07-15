3802cba5f02abb2e9c7c688ae77e206e39b94c9a
bluecube97 <jph04071@gmail.com>
2025-07-15 09:08:33 +0900

    고급 수학
    https://www.acmicpc.net/problem/7510

---
+++ bj_25_07_15/bj_25_07_15/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Scenario #{0}:", i + 1);
+
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+            int c = input[2];
+
+            if (a * a + b * b == c * c ||
+                a * a + c * c == b * b ||
+                b * b + c * c == a * a)
+            {
+                Console.WriteLine("yes");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("no");
+                Console.WriteLine();
+            }
+        }
+    }
+}
