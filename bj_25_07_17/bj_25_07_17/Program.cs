ee8225fe17f64e1e89163d14d932ef2dc1cff997
bluecube97 <jph04071@gmail.com>
2025-07-17 07:45:13 +0900

    상근이의 친구들
    https://www.acmicpc.net/problem/5717

---
+++ bj_25_07_17/bj_25_07_17/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            if (input[0] == 0 && input[1] == 0)
+                break;
+
+            int m = input[0];
+            int f = input[1];
+
+            Console.WriteLine(m + f);
+        }
+    }
+}
