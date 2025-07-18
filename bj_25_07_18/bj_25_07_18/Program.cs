5cfc26381a1cd298419f269ecad60f79650a33c2
bluecube97 <jph04071@gmail.com>
2025-07-18 19:41:46 +0900

    인간은 무엇인가
    https://www.acmicpc.net/problem/32651

---
+++ bj_25_07_18/bj_25_07_18/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        if (n > 100000)
+        {
+            Console.WriteLine("No");
+            return;
+        }
+        Console.WriteLine(n % 2024 == 0 ? "Yes" : "No");
+    }
+}
