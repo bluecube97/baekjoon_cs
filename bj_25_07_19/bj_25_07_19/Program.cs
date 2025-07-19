f764fbdaff1600c866eec125d8d594678e816256
bluecube97 <jph04071@gmail.com>
2025-07-19 18:57:51 +0900

    구멍
    https://www.acmicpc.net/problem/33571

---
+++ bj_25_07_19/bj_25_07_19/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string str = isr.ReadLine();
+        int holeCount = 0;
+
+        foreach (var c in str)
+        {
+            switch (c)
+            {
+                case 'A':
+                case 'a':
+                case 'b':
+                case 'D':
+                case 'd':
+                case 'e':
+                case 'g':
+                case 'O':
+                case 'o':
+                case 'P':
+                case 'p':
+                case 'Q':
+                case 'q':
+                case 'R':
+                    holeCount += 1;
+                    break;
+                case 'B':
+                    holeCount += 2;
+                    break;
+                case '@':
+                    holeCount += 1;
+                    break;
+            }
+        }
+
+        Console.WriteLine(holeCount);
+    }
+}
