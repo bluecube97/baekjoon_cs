04975a039f5c0087c592c584d8b4e7f48e5ac3e0
bluecube97 <jph04071@gmail.com>
2025-07-20 21:47:18 +0900

    사칙연산
    https://www.acmicpc.net/problem/13420

---
+++ bj_25_07_20/bj_25_07_20/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string[] formula = isr.ReadLine().Split().ToArray();
+            long a = long.Parse(formula[0]);
+            long b = long.Parse(formula[2]);
+            string op = formula[1];
+            long result = long.Parse(formula[4]);
+
+            if (result == calculator(a, b, op))
+            {
+                Console.WriteLine("correct");
+            }
+            else
+            {
+                Console.WriteLine("wrong answer");
+            }
+        }
+    }
+
+    private static long calculator(long a, long b, string op)
+    {
+        switch (op)
+        {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                return 0;
+        }
+    }
+}
