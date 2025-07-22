479063ffc7d1bed4f6a7f16ceae29592b67b498f
bluecube97 <jph04071@gmail.com>
2025-07-23 06:16:41 +0900

    팬들에게 둘러싸인 홍준
    https://www.acmicpc.net/problem/14581

---
+++ bj_25_07_23/bj_25_07_23/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string name = isr.ReadLine();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(":fan:");
+        }
+        Console.WriteLine();
+
+        Console.Write(":fan::");
+        Console.Write(name);
+        Console.WriteLine("::fan:");
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write(":fan:");
+        }
+    }
+}
