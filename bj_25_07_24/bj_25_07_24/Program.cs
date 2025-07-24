e55b03fa9937c468c08d7d358f9261d5a8ffe5ef
bluecube97 <jph04071@gmail.com>
2025-07-24 19:43:47 +0900

    TGN
    https://www.acmicpc.net/problem/5063

---
+++ bj_25_07_24/bj_25_07_24/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int[] rec = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int r = rec[0];
+            int e = rec[1];
+            int c = rec[2];
+
+            if (r > e - c)
+            {
+                Console.WriteLine("do not advertise");
+            }
+            else if (r < e - c)
+            {
+                Console.WriteLine("advertise");
+            }
+            else
+            {
+                Console.WriteLine("does not matter");
+            }
+        }
+    }
+}
