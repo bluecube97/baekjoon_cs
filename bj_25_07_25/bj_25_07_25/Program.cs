9b156b1e97625924f8cee8de5bf4e88f3f788b52
bluecube97 <jph04071@gmail.com>
2025-07-25 20:04:23 +0900

    지속
    https://www.acmicpc.net/problem/11648

---
+++ bj_25_07_25/bj_25_07_25/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string num = isr.ReadLine();
+        int count = 0;
+
+        while (true)
+        {
+            int baseNum = 1;
+            if (num.Length == 1)
+            {
+                Console.WriteLine(count);
+                break;
+            }
+            foreach (var c in num)
+            {
+                baseNum *= int.Parse(c.ToString());
+            }
+            num = baseNum.ToString();
+            count++;
+        }
+    }
+}
