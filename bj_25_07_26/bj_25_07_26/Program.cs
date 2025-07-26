6859662223b168ed2bba8aad6600ddda5bdf6610
bluecube97 <jph04071@gmail.com>
2025-07-26 20:29:56 +0900

    수박 게임
    https://www.acmicpc.net/problem/31868

---
+++ bj_25_07_26/bj_25_07_26/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nk[0];
+        int k = nk[1];
+
+        int suika = (int)Math.Pow(2, n - 1);
+        Console.WriteLine(k / suika);
+    }
+}
