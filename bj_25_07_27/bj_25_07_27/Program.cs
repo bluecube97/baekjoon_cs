70064f722adf006752f8ac554e8212de6227d509
bluecube97 <jph04071@gmail.com>
2025-07-27 23:21:01 +0900

    꼬마 정민
    https://www.acmicpc.net/problem/11382

---
+++ bj_25_07_27/bj_25_07_27/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        long[] nums = isr.ReadLine().Split().Select(long.Parse).ToArray();
+
+        Console.WriteLine(nums[0] + nums[1] + nums[2]);
+    }
+}
