602c68965c0b961f7c76a7a73d008dbc10294a29
bluecube97 <jph04071@gmail.com>
2025-07-29 00:34:50 +0900

    사분면 고르기
    https://www.acmicpc.net/problem/14681

---
+++ bj_25_07_28_1/bj_25_07_28_1/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int x = int.Parse(isr.ReadLine()!);
+        int y = int.Parse(isr.ReadLine()!);
+
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine(1);
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine(2);
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine(3);
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine(4);
+        }
+    }
+}
