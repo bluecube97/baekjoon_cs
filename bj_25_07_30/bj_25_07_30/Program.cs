c184e524a3fabf5c80ee2b5f0fcb223bd034c657
bluecube97 <jph04071@gmail.com>
2025-07-30 07:06:38 +0900

    SciComLove (2024)
    https://www.acmicpc.net/problem/31746

---
+++ bj_25_07_30/bj_25_07_30/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("SciComLove");
+        }
+        else
+        {
+            Console.WriteLine("evoLmoCicS");
+        }
+    }
+}
