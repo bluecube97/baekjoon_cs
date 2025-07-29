40d0c8896cf175d8cda7802b1b5a72dc4cf3a221
bluecube97 <jph04071@gmail.com>
2025-07-29 23:28:18 +0900

    한다 안한다
    https://www.acmicpc.net/problem/5789

---
+++ bj_25_07_29/bj_25_07_29/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string T = isr.ReadLine();
+            int TSize = T.Length;
+            if (T[TSize / 2].Equals(T[TSize / 2 - 1]))
+            {
+                Console.WriteLine("Do-it");
+            }
+            else
+            {
+                Console.WriteLine("Do-it-Not");
+            }
+        }
+    }
+}
