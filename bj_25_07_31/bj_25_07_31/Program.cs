2dc1fc62ad91891a3435367872e41db00e66a333
bluecube97 <jph04071@gmail.com>
2025-07-31 07:39:55 +0900

    캥거루 세마리2
    https://www.acmicpc.net/problem/11034

---
+++ bj_25_07_31/bj_25_07_31/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (isr.Peek() != -1)
+        {
+            int[] abc = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = abc[0], b = abc[1], c = abc[2];
+
+            int max = Math.Max(b - a, c - b);
+            Console.WriteLine(max - 1);
+        }
+    }
+}
