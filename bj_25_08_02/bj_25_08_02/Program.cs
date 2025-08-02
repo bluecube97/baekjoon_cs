3ffed9f2f715b15cb37a7336ed8469ca38796f30
bluecube97 <jph04071@gmail.com>
2025-08-02 23:34:24 +0900

    단원평가
    https://www.acmicpc.net/problem/31798

---
+++ bj_25_08_02/bj_25_08_02/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] abc = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = abc[0], b = abc[1], c = abc[2];
+
+        if (a == 0)
+        {
+            a = c * c - b;
+            Console.WriteLine(a);
+        }
+        else if (b == 0)
+        {
+            b = c * c - a;
+            Console.WriteLine(b);
+        }
+        else if (c == 0)
+        {
+            c = (int)Math.Sqrt(a + b);
+            Console.WriteLine(c);
+        }
+    }
+}
