8d9845053339d05642c5e284cb5b92daadbd84c8
bluecube97 <jph04071@gmail.com>
2025-08-05 03:42:29 +0900

    무알콜 칵테일
    https://www.acmicpc.net/problem/2896

---
+++ bj_25_08_05/bj_25_08_05/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        double[] abc = isr.ReadLine().Split().Select(double.Parse).ToArray();
+        double a = abc[0], b = abc[1], c = abc[2];
+        double[] ijk = isr.ReadLine().Split().Select(double.Parse).ToArray();
+        double i = ijk[0], j = ijk[1], k = ijk[2];
+
+        double x = Math.Min(a / i, Math.Min(b / j, c / k));
+
+        double remainA = a - i * x;
+        double remainB = b - j * x;
+        double remainC = c - k * x;
+
+        Console.WriteLine($"{remainA:F6} {remainB:F6} {remainC:F6}");
+    }
+}
