7e882cba6ae66acd588db312ba13e9cfc5101177
bluecube97 <jph04071@gmail.com>
2025-08-06 02:34:55 +0900

    피드백
    https://www.acmicpc.net/problem/18856

---
+++ bj_25_08_06/bj_25_08_06/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] arr = new int[n];
+
+        arr[0] = 1;
+        arr[1] = 2;
+
+        for (int i = 2; i < n - 1; i++)
+        {
+            arr[i] = i + 1;
+        }
+
+        arr[n - 1] = 997;
+
+        Console.WriteLine(n);
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i]);
+            Console.Write(" ");
+        }
+    }
+}
