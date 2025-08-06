389530d2dd93bed83edbbe88b9672eef5796bf90
bluecube97 <jph04071@gmail.com>
2025-08-07 07:30:36 +0900

    SUAPC 2023 Summer
    https://www.acmicpc.net/problem/31429

---
+++ bj_25_08_07/bj_25_08_07/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        List<(int solved, int penalty)> teams = new List<(int, int)>
+        {
+            (12, 1600),
+            (11, 894),
+            (11, 1327),
+            (10, 1311),
+            (9, 1004),
+            (9, 1178),
+            (9, 1357),
+            (8, 837),
+            (7, 1055),
+            (6, 556),
+            (6, 773)
+        };
+
+        int n = int.Parse(isr.ReadLine());
+
+        (int, int) team = teams[n - 1];
+        Console.WriteLine($"{team.Item1} {team.Item2}");
+    }
+}
