c4582cd01fcbf57610c5817daf7678ba0ea8313c
bluecube97 <jph04071@gmail.com>
2025-08-06 19:44:35 +0900

    그래서 님 푼 문제 수가?
    https://www.acmicpc.net/problem/29720

---
+++ bj_25_08_06_01/bj_25_08_06_01/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nmk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nmk[0], m = nmk[1], k = nmk[2];
+
+        int minSolve = Math.Max(0, n - m * k);
+        int maxSolve = n - m * (k - 1) - 1;
+
+        Console.WriteLine("{0} {1}", minSolve, maxSolve);
+    }
+}
