c2f7e1e130828504be1826dcf8a4d1a81a3d5496
bluecube97 <jph04071@gmail.com>
2025-08-08 07:47:10 +0900

    양념 반 후라이드 반
    https://www.acmicpc.net/problem/16917

---
+++ bj_25_08_08/bj_25_08_08/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nums = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = nums[0], b = nums[1], c = nums[2], x = nums[3], y = nums[4];
+
+        int result = int.MaxValue;
+
+        for (int i = 0; i <= Math.Max(x, y); i++)
+        {
+            int half = i;
+            int needA = Math.Max(0, x - half);
+            int needB = Math.Max(0, y - half);
+
+            int cost = needA * a + needB * b + half * 2 * c;
+            result = Math.Min(result, cost);
+        }
+
+        Console.WriteLine(result);
+    }
+}
