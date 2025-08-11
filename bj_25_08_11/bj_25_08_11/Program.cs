c0ac8d46d9690ecfcd34737615d67bb129846b37
bluecube97 <jph04071@gmail.com>
2025-08-11 22:25:13 +0900

    영수증
    https://www.acmicpc.net/problem/5565

---
+++ bj_25_08_11/bj_25_08_11/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int totalValue = int.Parse(isr.ReadLine());
+        int possibleValue = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            possibleValue += int.Parse(isr.ReadLine());
+        }
+
+        Console.WriteLine(totalValue - possibleValue);
+    }
+}
