bb2c6d9f376c440aff8586ab82c743885d53e6f0
bluecube97 <jph04071@gmail.com>
2025-08-13 06:16:18 +0900

    녹색거탑
    https://www.acmicpc.net/problem/24723

---
+++ bj_25_08_13/bj_25_08_13/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int result = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            result *= 2;
+        }
+
+        Console.WriteLine(result);
+    }
+}
