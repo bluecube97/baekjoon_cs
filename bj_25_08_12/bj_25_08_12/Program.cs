2a53d11510979223cb95faa7c8fe3f068c0c79f7
bluecube97 <jph04071@gmail.com>
2025-08-12 19:45:09 +0900

    평균 점수
    https://www.acmicpc.net/problem/10039

---
+++ bj_25_08_12/bj_25_08_12/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int sum = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            int score = int.Parse(isr.ReadLine());
+            if (score < 40)
+            {
+                score = 40;
+            }
+            sum += score;
+        }
+
+        Console.WriteLine(sum / 5);
+    }
+}
