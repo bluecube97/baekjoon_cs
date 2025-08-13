8cc915df82eace4a84293aeb32438eea25a62d0b
bluecube97 <jph04071@gmail.com>
2025-08-14 08:06:26 +0900

    공백 없는 A+B
    https://www.acmicpc.net/problem/15873

---
+++ bj_25_08_14/bj_25_08_14/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string ab = isr.ReadLine();
+        int a, b;
+
+        if (ab[0] == '1' && ab[1] == '0')
+        {
+            a = 10;
+            if (ab.Length == 3)
+            {
+                b = int.Parse(ab[2].ToString());
+            }
+            else
+            {
+                b = 10;
+            }
+        }
+        else
+        {
+            a = int.Parse(ab[0].ToString());
+            if (ab.Length == 2)
+            {
+                b = int.Parse(ab[1].ToString());
+            }
+            else
+            {
+                b = 10;
+            }
+        }
+
+        Console.WriteLine(a + b);
+    }
+}
