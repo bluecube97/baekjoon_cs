13573a7ebcb73afe6453f7d7662481eb27152b7a
bluecube97 <jph04071@gmail.com>
2025-08-15 07:44:46 +0900

    비교 연산자
    https://www.acmicpc.net/problem/5656

---
+++ bj_25_08_15/bj_25_08_15/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int num = 1;
+
+        while (true)
+        {
+            string[] input = isr.ReadLine().Split().ToArray();
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[2]);
+            string op = input[1];
+
+            if (op == "E")
+                break;
+
+            string result = operating(a, b, op);
+            Console.WriteLine($"Case {num}: {result}");
+            num++;
+        }
+    }
+
+    private static string operating(int a, int b, string op)
+    {
+        return op switch
+        {
+            ">" => a > b ? "true" : "false",
+            ">=" => a >= b ? "true" : "false",
+            "<" => a < b ? "true" : "false",
+            "<=" => a <= b ? "true" : "false",
+            "==" => a == b ? "true" : "false",
+            "!=" => a != b ? "true" : "false",
+            _ => "inv"
+        };
+    }
+}
