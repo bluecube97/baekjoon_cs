3e9e225924800ecb74ab31149f91ce9ecc15d73e
bluecube97 <jph04071@gmail.com>
2025-08-16 10:15:15 +0900

    꿍 가라사대
    https://www.acmicpc.net/problem/11094

---
+++ bj_25_08_16/bj_25_08_16/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = isr.ReadLine();
+
+            if (line.Contains("Simon says"))
+            {
+                string response = line.Substring("Simon says".Length);
+                Console.WriteLine(response);
+            }
+        }
+    }
+}
