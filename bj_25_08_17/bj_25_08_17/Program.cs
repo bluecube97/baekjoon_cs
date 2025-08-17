bd656ed87c6c1ff3fe2375222da1b9489cfc6c99
bluecube97 <jph04071@gmail.com>
2025-08-17 09:43:43 +0900

    텔레프라임
    https://www.acmicpc.net/problem/7789

---
+++ bj_25_08_17/bj_25_08_17/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string[] phone = isr.ReadLine().Split().ToArray();
+        string oldPhone = phone[0];
+        string newPhone = phone[1] + oldPhone;
+
+        int oldPhoneInt = int.Parse(oldPhone);
+        int newPhoneInt = int.Parse(newPhone);
+
+        if (isPrime(oldPhoneInt) && isPrime(newPhoneInt))
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+
+    private static bool isPrime(int num)
+    {
+        if (num % 2 == 0) return false;
+
+        for (int i = 3; i * i <= num; i += 2)
+        {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
