136a919e3b4cdf0a2b2be3f53e599ffe5f75e95c
bluecube97 <jph04071@gmail.com>
2025-08-18 20:12:07 +0900

    과목선택
    https://www.acmicpc.net/problem/11948

---
+++ bj_25_08_18/bj_25_08_18/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        List<int> science = new List<int>();
+        List<int> social = new List<int>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            science.Add(int.Parse(isr.ReadLine()));
+        }
+        for (int i = 0; i < 2; i++)
+        {
+            social.Add(int.Parse(isr.ReadLine()));
+        }
+
+        science.Sort();
+        social.Sort();
+
+        int total = science[1] + science[2] + science[3] + social[1];
+
+        Console.WriteLine(total);
+    }
+}
