a59a6d191b33ed0083b34a121f9a964dcf918d0e
bluecube97 <jph04071@gmail.com>
2025-08-19 23:10:43 +0900

    치킨댄스를 추는 곰곰이를 본 임스
    https://www.acmicpc.net/problem/25191

---
+++ bj_25_08_19/bj_25_08_19/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = ab[0];
+        int b = ab[1];
+
+        a /= 2;
+
+        if (a + b < n)
+        {
+            Console.WriteLine(a + b);
+        }
+        else
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
