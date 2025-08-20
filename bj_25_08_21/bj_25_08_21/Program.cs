795df6f2e2a7448ed8124dce80e9e2e668441921
bluecube97 <jph04071@gmail.com>
2025-08-21 07:42:19 +0900

    특별한 작은 분수
    https://www.acmicpc.net/problem/27890

---
+++ bj_25_08_21/bj_25_08_21/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] xn = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int x = xn[0];
+        int n = xn[1];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (x % 2 == 0)
+            {
+                x /= 2;
+                x ^= 6;
+            }
+            else
+            {
+                x *= 2;
+                x ^= 6;
+            }
+        }
+
+        Console.WriteLine(x);
+    }
+}
