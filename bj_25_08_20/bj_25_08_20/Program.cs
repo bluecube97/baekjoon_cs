0be8c43881414fa50c3a818620b797216485d9c1
bluecube97 <jph04071@gmail.com>
2025-08-20 20:48:09 +0900

    :chino_shock:
    https://www.acmicpc.net/problem/27310

---
+++ bj_25_08_20/bj_25_08_20/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string emoji = isr.ReadLine();
+        int len = emoji.Length + 2;
+        int underBar = 0;
+
+        foreach (var c in emoji)
+        {
+            if (c == '_')
+            {
+                underBar++;
+            }
+        }
+
+        Console.WriteLine(len + underBar * 5);
+    }
+}
