a5fcd5f84fab65f9d5b922c92cd167c1e3af2cfd
bluecube97 <jph04071@gmail.com>
2025-08-23 17:32:31 +0900

    폭죽쇼
    https://www.acmicpc.net/problem/1773

---
+++ bj_25_08_23/bj_25_08_23/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nc = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nc[0], c = nc[1];
+
+        bool[] isUsed = new bool[c + 1];
+
+        for (int i = 0; i < n; i++)
+        {
+            int interval = int.Parse(isr.ReadLine());
+
+            for (int j = 1; j <= c; j++)
+            {
+                if (j % interval == 0)
+                {
+                    isUsed[j] = true;
+                }
+            }
+        }
+
+        int boomCount = isUsed.Count(x => x);
+        Console.WriteLine(boomCount);
+    }
+}
