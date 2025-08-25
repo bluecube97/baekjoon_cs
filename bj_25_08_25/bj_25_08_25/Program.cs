796a0d177f720b3631fee19aac0c20a3a0345b1a
bluecube97 <jph04071@gmail.com>
2025-08-25 09:49:21 +0900

    모범생 포닉스
    https://www.acmicpc.net/problem/28097

---
+++ bj_25_08_25/bj_25_08_25/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] T = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int total = T.Sum() + 8 * (n - 1);
+
+        Console.WriteLine($"{total / 24} {total % 24}");
+    }
+}
