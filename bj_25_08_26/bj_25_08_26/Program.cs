b64357a5da13f3e2ec5c7454014653bcb45c32ab
bluecube97 <jph04071@gmail.com>
2025-08-26 18:47:12 +0900

    영수증
    https://www.acmicpc.net/problem/25304

---
+++ bj_25_08_26/bj_25_08_26/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int x = int.Parse(isr.ReadLine());
+        int n = int.Parse(isr.ReadLine());
+
+        int priceSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] bill = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int price = bill[0];
+            int ea = bill[1];
+
+            int total = price * ea;
+            priceSum += total;
+        }
+
+        Console.WriteLine(priceSum == x ? "Yes" : "No");
+    }
+}
