160c071c8aee6847c9c099c4370830bc43fd4ddd
bluecube97 <jph04071@gmail.com>
2025-08-28 07:26:43 +0900

    팀 나누기
    https://www.acmicpc.net/problem/13866

---
+++ bj_25_08_28/bj_25_08_28/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] player = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = player[0], b = player[1], c = player[2], d = player[3];
+
+        int minDiff = int.MaxValue;
+
+        minDiff = Math.Min(Math.Abs((a + b) - (c + d)), minDiff);
+        minDiff = Math.Min(Math.Abs((a + c) - (b + d)), minDiff);
+        minDiff = Math.Min(Math.Abs((a + d) - (b + c)), minDiff);
+
+        Console.WriteLine(minDiff);
+    }
+}
