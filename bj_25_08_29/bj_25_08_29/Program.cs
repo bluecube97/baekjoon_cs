07575187efc97922d5cc0e222086c320f819e9c9
bluecube97 <jph04071@gmail.com>
2025-08-29 07:16:20 +0900

    중앙 이동 알고리즘
    https://www.acmicpc.net/problem/2903

---
+++ bj_25_08_29/bj_25_08_29/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int result = 0;
+        int init = 2;
+
+        for (int i = 1; i <= n; i++)
+        {
+            init = init * 2 - 1;
+            result = init * init;
+        }
+
+        Console.WriteLine(result);
+    }
+}
