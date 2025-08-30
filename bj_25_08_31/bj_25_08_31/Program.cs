313bd6482640da9b957bdda686a19d6d668c17ff
bluecube97 <jph04071@gmail.com>
2025-08-31 04:35:14 +0900

    간지(干支)
    https://www.acmicpc.net/problem/7572

---
+++ bj_25_08_31/bj_25_08_31/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int year = int.Parse(isr.ReadLine()) - 3;
+
+        if (year == 0)
+        {
+            Console.WriteLine("L9");
+            return;
+        }
+        if (year == -1)
+        {
+            Console.WriteLine("K8");
+            return;
+        }
+        if (year == -2)
+        {
+            Console.WriteLine("J7");
+            return;
+        }
+
+        int twelveji = (year - 1) % 12;
+        int tengan = (year - 1) % 10;
+
+        string a = ((char) (twelveji + 'A')).ToString();
+        Console.WriteLine(a + tengan);
+    }
+}
