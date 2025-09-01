f8be2863b02535536333b2ddb7594ff88d33413d
bluecube97 <jph04071@gmail.com>
2025-09-02 02:38:04 +0900

    이진수 합
    https://www.acmicpc.net/problem/8741

---
+++ bj_25_09_01/bj_25_09_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int k = int.Parse(isr.ReadLine());
+
+        if (k == 1)
+        {
+            Console.WriteLine("1");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.Append('1', k);
+        sb.Append('0', k - 1);
+
+        Console.WriteLine(sb.ToString());
+    }
+}
