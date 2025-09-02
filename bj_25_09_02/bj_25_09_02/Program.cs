8752e1b70df68d60f82b2981818ef2d3e876aa23
bluecube97 <jph04071@gmail.com>
2025-09-03 01:18:35 +0900

    AFC 윔블던
    https://www.acmicpc.net/problem/4299

---
+++ bj_25_09_02/bj_25_09_02/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int AsumB = input[0];
+        int AdifB = input[1];
+
+        if ((AsumB + AdifB) % 2 != 0 || AsumB < AdifB)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        int a = (AsumB + AdifB) / 2;
+        int b = AsumB - a;
+
+        Console.WriteLine(a > b ? $"{a} {b}" : $"{b} {a}");
+    }
+}
