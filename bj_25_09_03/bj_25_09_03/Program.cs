7cc075689b1a618a32983448bbcb66816882e5af
bluecube97 <jph04071@gmail.com>
2025-09-03 21:25:43 +0900

    정보섬의 대중교통
    https://www.acmicpc.net/problem/28113

---
+++ bj_25_09_03/bj_25_09_03/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = input[0];
+        int a = input[1];
+        int b = input[2];
+
+        if (a == b)
+        {
+            Console.WriteLine("Anything");
+            return;
+        }
+        Console.WriteLine(a < b ? "Bus" : "Subway");
+    }
+}
