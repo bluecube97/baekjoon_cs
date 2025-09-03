fd8e367b685b7c535ef01478c68521506bab42b7
bluecube97 <jph04071@gmail.com>
2025-09-04 07:54:47 +0900

    Do Not Touch Anything
    https://www.acmicpc.net/problem/13136

---
+++ bj_25_09_04/bj_25_09_04/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int r = input[0];
+        int c = input[1];
+        int n = input[2];
+        
+        long divR = r / n;
+        long divC = c / n;
+
+        if (r % n != 0)
+            divR++;
+        if (c % n != 0)
+            divC++;
+
+        Console.WriteLine(divR * divC);
+    }
+}
