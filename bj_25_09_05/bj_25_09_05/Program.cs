4da608dacf2bd33bc7bf899b215ef6e5094fc40d
bluecube97 <jph04071@gmail.com>
2025-09-05 07:56:09 +0900

    글로벌 포닉스
    https://www.acmicpc.net/problem/31775

---
+++ bj_25_09_05/bj_25_09_05/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        bool[] isGlobal = new bool[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            string sentence = isr.ReadLine();
+            if (sentence[0] == 'l' && !isGlobal[0])
+            {
+                isGlobal[0] = true;
+            }
+            if (sentence[0] == 'k' && !isGlobal[1])
+            {
+                isGlobal[1] = true;
+            }
+            if (sentence[0] == 'p' && !isGlobal[2])
+            {
+                isGlobal[2] = true;
+            }
+        }
+
+        Console.WriteLine(isGlobal.All(x => x) ? "GLOBAL" : "PONIX");
+    }
+}
