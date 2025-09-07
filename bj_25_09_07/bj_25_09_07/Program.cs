3c38bd50d9502d276bd22aa8b85b1442ee4fb1fc
bluecube97 <jph04071@gmail.com>
2025-09-07 20:31:35 +0900

    손가락 게임
    https://www.acmicpc.net/problem/31866

---
+++ bj_25_09_07/bj_25_09_07/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] finger = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int jun = finger[0];
+        int ik = finger[1];
+
+        if (jun != 0 && jun != 2 && jun != 5 && ik != 0 && ik != 2 && ik != 5)
+        {
+            Console.WriteLine("=");
+            return;
+        }
+
+        if (jun != 0 && jun != 2 && jun != 5)
+        {
+            Console.WriteLine("<");
+            return;
+        }
+
+        if (ik != 0 && ik != 2 && ik != 5)
+        {
+            Console.WriteLine(">");
+            return;
+        }
+
+        if (jun == ik)
+        {
+            Console.WriteLine("=");
+            return;
+        }
+
+        if ((jun == 0 && ik == 2) || (jun == 2 && ik == 5) || (jun == 5 && ik == 0))
+        {
+            Console.WriteLine(">");
+        }
+        else
+        {
+            Console.WriteLine("<");
+        }
+    }
+}
