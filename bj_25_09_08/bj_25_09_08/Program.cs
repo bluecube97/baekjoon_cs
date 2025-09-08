fe0bdf6b5616d15527171487760086a4ea9983c7
bluecube97 <jph04071@gmail.com>
2025-09-08 19:27:06 +0900

    학식 사주기
    https://www.acmicpc.net/problem/31821

---
+++ bj_25_09_08/bj_25_09_08/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] menuPrices = new int[n];
+        int priceSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int menuPrice = int.Parse(isr.ReadLine());
+            menuPrices[i] = menuPrice;
+        }
+
+        int m = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < m; i++)
+        {
+            int cornerNum = int.Parse(isr.ReadLine());
+            priceSum += menuPrices[cornerNum - 1];
+        }
+
+        Console.WriteLine(priceSum);
+    }
+}
