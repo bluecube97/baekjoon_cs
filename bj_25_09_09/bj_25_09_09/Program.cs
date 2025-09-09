8b97184e7ea0bc55f8e20f8d61efe4dcc6fa340e
bluecube97 <jph04071@gmail.com>
2025-09-09 21:14:23 +0900

    재수강
    https://www.acmicpc.net/problem/31822

---
+++ bj_25_09_09/bj_25_09_09/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string retake = isr.ReadLine();
+        retake = retake[..5];
+        int n = int.Parse(isr.ReadLine());
+        int canRetake = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            string course = isr.ReadLine();
+            course = course[..5];
+
+            if (retake == course)
+            {
+                canRetake++;
+            }
+        }
+
+        Console.WriteLine(canRetake);
+    }
+}
