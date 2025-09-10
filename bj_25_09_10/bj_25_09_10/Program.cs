c9a2e7c5731089155f9e4eb58dc6ed02a88f1696
bluecube97 <jph04071@gmail.com>
2025-09-10 22:09:56 +0900

    몇개고?
    https://www.acmicpc.net/problem/27294

---
+++ bj_25_09_10/bj_25_09_10/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] ts = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int t = ts[0];
+        int s = ts[1];
+
+        if (t >= 12 && t <= 16 && s == 0)
+        {
+            Console.WriteLine(320);
+        }
+        else
+        {
+            Console.WriteLine(280);
+        }
+    }
+}
