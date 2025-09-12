1491bf963b74a6e02a488baf206e8840a1e00cac
bluecube97 <jph04071@gmail.com>
2025-09-12 18:46:15 +0900

    고려대학교에는 공식 와인이 있다
    https://www.acmicpc.net/problem/16673

---
+++ bj_25_09_12/bj_25_09_12/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] ckp = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int c = ckp[0];
+        int k = ckp[1];
+        int p = ckp[2];
+
+        long sum = (long)k * c * (c + 1) / 2 + (long)p * c * (c + 1) * (2 * c + 1) / 6;
+
+        Console.WriteLine(sum);
+    }
+}
