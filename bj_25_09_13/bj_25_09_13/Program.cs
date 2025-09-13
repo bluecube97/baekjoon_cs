2b30383f2e10ab18e897a610e53269f064432f61
bluecube97 <jph04071@gmail.com>
2025-09-13 16:43:29 +0900

    골뱅이 찍기 - ㅁ
    https://www.acmicpc.net/problem/23806

---
+++ bj_25_09_13/bj_25_09_13/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        StringBuilder sb = new StringBuilder();
+
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n * 5; j++)
+            {
+                sb.Append('@');
+            }
+            sb.Append('\n');
+        }
+
+        for (int i = 0; i < n * 3; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                sb.Append('@');
+            }
+            for (int j = 0; j < n * 3; j++)
+            {
+                sb.Append(' ');
+            }
+            for (int j = 0; j < n; j++)
+            {
+                sb.Append('@');
+            }
+            sb.Append('\n');
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n * 5; j++)
+            {
+                sb.Append('@');
+            }
+            sb.Append('\n');
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
