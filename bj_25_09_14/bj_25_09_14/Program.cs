7de5e575b3dc5c07687b3d3ae6b06cdbbc3c9201
bluecube97 <jph04071@gmail.com>
2025-09-14 15:44:11 +0900

    암호 만들기
    https://www.acmicpc.net/problem/21553

---
+++ bj_25_09_14/bj_25_09_14/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string a = isr.ReadLine();
+        string p = isr.ReadLine();
+
+        Console.WriteLine(p);
+    }
+}
