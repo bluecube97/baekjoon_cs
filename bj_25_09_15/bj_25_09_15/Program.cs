4930f4a2731c45638309a202aa663e8e9d135f33
bluecube97 <jph04071@gmail.com>
2025-09-15 20:27:12 +0900

    홀짝홀짝
    https://www.acmicpc.net/problem/31867

---
+++ bj_25_09_15/bj_25_09_15/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string k = isr.ReadLine();
+
+        int hol = 0;
+        int jjak = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (int.Parse(k[i].ToString()) % 2 == 0)
+            {
+                jjak++;
+            }
+            else
+                hol++;
+        }
+
+        if (hol == jjak)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+
+        Console.WriteLine(hol > jjak ? 1 : 0);
+    }
+}
