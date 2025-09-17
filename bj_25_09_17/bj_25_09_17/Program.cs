02673028d62e9fa35da6374db1280e69509cafc7
bluecube97 <jph04071@gmail.com>
2025-09-17 12:26:26 +0900

    유치원생 파댕이 돌보기
    https://www.acmicpc.net/problem/30979

---
+++ bj_25_09_17/bj_25_09_17/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+        int n = int.Parse(isr.ReadLine());
+        int[] f = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine(t <= f.Sum() ? "Padaeng_i Happy" : "Padaeng_i Cry");
+    }
+}
