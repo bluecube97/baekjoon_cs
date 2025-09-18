295545637100ec2ffde1fd6e7fb748cab2e43736
bluecube97 <jph04071@gmail.com>
2025-09-19 07:02:57 +0900

    단어 길이 재기
    https://www.acmicpc.net/problem/2743

---
+++ bj_25_09_19/bj_25_09_19/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string str = isr.ReadLine();
+        Console.WriteLine(str.Length);
+    }
+}
