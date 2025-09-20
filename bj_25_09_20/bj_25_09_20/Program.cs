4e81098c815d4681d195f642bca801456b9374eb
bluecube97 <jph04071@gmail.com>
2025-09-20 16:44:47 +0900

    어게인 포닉스
    https://www.acmicpc.net/problem/33950

---
+++ bj_25_09_20/bj_25_09_20/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string str = isr.ReadLine();
+
+            str = str.Replace("PO", "PHO");
+            Console.WriteLine(str);
+        }
+    }
+}
