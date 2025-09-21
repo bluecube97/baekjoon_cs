46172c071f945c2bb656e20dfba72cff80deca73
bluecube97 <jph04071@gmail.com>
2025-09-21 21:25:27 +0900

    대소문자 바꾸기
    https://www.acmicpc.net/problem/2744

---
+++ bj_25_09_21/bj_25_09_21/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string str = isr.ReadLine();
+        foreach (var c in str)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                Console.Write((char)(c - 32));
+            }
+            else if (c >= 'A' && c <= 'Z')
+            {
+                Console.Write((char)(c + 32));
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
