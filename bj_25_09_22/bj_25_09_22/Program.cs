7fa9260d89749a0c79874e4b2d5f9d0dc7e2d63a
bluecube97 <jph04071@gmail.com>
2025-09-22 21:38:04 +0900

    선물
    https://www.acmicpc.net/problem/28281

---
+++ bj_25_09_22/bj_25_09_22/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nx = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nx[0];
+        int x = nx[1];
+
+        int[] socks = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int minValue = int.MaxValue;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int sum = socks[i] + socks[i + 1];
+            minValue = Math.Min(sum, minValue);
+        }
+
+        Console.WriteLine(minValue * x);
+    }
+}
