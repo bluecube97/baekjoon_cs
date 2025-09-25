bae30f632ef40e016a79d2c2e94b9a5067caedb3
bluecube97 <jph04071@gmail.com>
2025-09-25 10:13:29 +0900

    임스의 메이플컵
    https://www.acmicpc.net/problem/29790

---
+++ bj_25_09_25/bj_25_09_25/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nul = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nul[0];
+        int u = nul[1];
+        int l = nul[2];
+
+        if (n >= 1000 && (u >= 8000 || l >= 260))
+        {
+            Console.WriteLine("Very Good");
+        }
+        else if (n >= 1000)
+        {
+            Console.WriteLine("Good");
+        }
+        else
+        {
+            Console.WriteLine("Bad");
+        }
+    }
+}
