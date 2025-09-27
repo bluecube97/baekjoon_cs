675b91f01d8926ebb5dfa1f99f44b1112a5dbc6b
bluecube97 <jph04071@gmail.com>
2025-09-27 19:33:40 +0900

    수열의 비밀 (Easy)
    https://www.acmicpc.net/problem/32279

---
+++ bj_25_09_27/bj_25_09_27/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] pqrs = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int p = pqrs[0];
+        int q = pqrs[1];
+        int r = pqrs[2];
+        int s = pqrs[3];
+
+        int a = int.Parse(isr.ReadLine());
+
+        long[] S = new long[n + 1];
+        S[1] = a;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (2 * i <= n)
+                S[2 * i] = p * S[i] + q;
+            if (2 * i + 1 <= n)
+                S[2 * i + 1] = r * S[i] + s;
+        }
+
+        long sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum += S[i];
+        }
+
+        Console.WriteLine(sum);
+    }
+}
