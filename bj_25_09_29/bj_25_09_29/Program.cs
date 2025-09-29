048f025407fe8f10fee9d1a8dd769b13998126dc
bluecube97 <jph04071@gmail.com>
2025-09-29 20:45:47 +0900

    양말 짝 맞추기
    https://www.acmicpc.net/problem/28431

---
+++ bj_25_09_29/bj_25_09_29/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        Dictionary<int, int> count = new Dictionary<int, int>();
+        for (int i = 0; i < 5; i++)
+        {
+            int sock = int.Parse(isr.ReadLine());
+            count.TryAdd(sock, 0);
+            count[sock]++;
+        }
+
+        foreach (var sock in count)
+        {
+            if (sock.Value % 2 == 1)
+            {
+                Console.WriteLine(sock.Key);
+                break;
+            }
+        }
+    }
+}
