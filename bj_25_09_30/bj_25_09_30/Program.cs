4613ff255d3c0e8b6bea4946c32df8af30ac11e8
bluecube97 <jph04071@gmail.com>
2025-09-30 22:22:10 +0900

    국방시계
    https://www.acmicpc.net/problem/33277

---
+++ bj_25_09_30/bj_25_09_30/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        double n = nm[0];
+        double m = nm[1];
+
+        int minute = (int) (m / n * 24.0 * 60.0);
+        int hour = minute / 60;
+        minute %= 60;
+
+        string sHour = hour < 10 ? "0" + hour : hour.ToString();
+        string sMinute = minute < 10 ? "0" + minute : minute.ToString();
+
+        Console.WriteLine($"{sHour}:{sMinute}");
+    }
+}
