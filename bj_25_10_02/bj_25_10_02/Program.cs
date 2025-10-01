9e1fae163d68c5a505f5c971757082e63ed83a26
bluecube97 <jph04071@gmail.com>
2025-10-02 06:48:47 +0900

    열 개씩 끊어 출력하기
    https://www.acmicpc.net/problem/11721

---
+++ bj_25_10_02/bj_25_10_02/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string str = isr.ReadLine();
+        int i = 0;
+
+        foreach (var c in str)
+        {
+            if (i % 10 == 0 && i != 0)
+            {
+                Console.WriteLine();
+            }
+            Console.Write(c);
+            i++;
+        }
+    }
+}
