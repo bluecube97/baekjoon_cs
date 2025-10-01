a337bf1bb5e46a7bdf8013376887c63124211c93
bluecube97 <jph04071@gmail.com>
2025-10-01 21:32:45 +0900

    피갤컵
    https://www.acmicpc.net/problem/33612

---
+++ bj_25_10_01/bj_25_10_01/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        n--;
+
+        int defaultValue = 2024 * 12 + 8;
+        int result = defaultValue + n * 7;
+        int year = result / 12;
+        int month = result % 12;
+        if (month == 0)
+        {
+            year--;
+            month = 12;
+        }
+
+        Console.WriteLine($"{year} {month}");
+    }
+}
