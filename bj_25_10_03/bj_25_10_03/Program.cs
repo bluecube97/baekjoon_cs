d9fc5b2bc5f35be105925e5aed1c7e4508f0c646
bluecube97 <jph04071@gmail.com>
2025-10-03 07:11:41 +0900

    e 계산
    https://www.acmicpc.net/problem/6376

---
+++ bj_25_10_03/bj_25_10_03/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        // var isr = new StreamReader(Console.OpenStandardInput());
+
+        Console.WriteLine("n e");
+        Console.WriteLine("- -----------");
+
+        double e = 0;
+        int f = 1;
+
+        for (int n = 0; n <= 9; n++)
+        {
+            if (n > 0) f *= n;
+            e += 1.0 / f;
+
+            if (n < 3)
+                Console.WriteLine($"{n} {e}");
+            else
+                Console.WriteLine($"{n} {e:F9}");
+        }
+    }
+}
