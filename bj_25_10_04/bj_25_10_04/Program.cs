9bd7b04251a60f9d709d260f0ee07afe8d27a08d
bluecube97 <jph04071@gmail.com>
2025-10-04 15:52:39 +0900

    ACM 대회 채점
    https://www.acmicpc.net/problem/11531

---
+++ bj_25_10_04/bj_25_10_04/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        Dictionary<string, (bool solved, int wrongCount, int solvedTime)> problems = new();
+
+        int solvedCount = 0;
+        int penalty = 0;
+
+        while (true)
+        {
+            string log = isr.ReadLine();
+            if (log == "-1") break;
+
+            string[] logs = log.Split().ToArray();
+            int time = int.Parse(logs[0]);
+            string problem = logs[1];
+            string isSolved = logs[2];
+
+            if (!problems.ContainsKey(problem))
+            {
+                problems[problem] = (false, 0, 0);
+            }
+
+            var state = problems[problem];
+
+            if (isSolved == "wrong")
+            {
+                if (!state.solved)
+                {
+                    state.wrongCount++;
+                }
+            }
+            else if (isSolved == "right")
+            {
+                if (!state.solved)
+                {
+                    state.solved = true;
+                    state.solvedTime = time;
+
+                    solvedCount++;
+                    penalty += time + state.wrongCount * 20;
+                }
+            }
+
+            problems[problem] = state;
+        }
+        Console.WriteLine($"{solvedCount} {penalty}");
+    }
+}
