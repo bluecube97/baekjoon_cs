25ef3b81cc6ca398f5c50b5738cbefc36deb5fa0
bluecube97 <jph04071@gmail.com>
2025-10-05 17:33:04 +0900

    두~~부 두부 두부
    https://www.acmicpc.net/problem/25175

---
+++ bj_25_10_05/bj_25_10_05/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nmk = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nmk[0];
+        int m = nmk[1];
+        int k = nmk[2];
+
+        int t = k - 3;
+        int next = (m - 1 + t) % n;
+
+        if (next < 0)
+        {
+            next += n;
+        }
+
+        Console.WriteLine(next + 1);
+    }
+}
