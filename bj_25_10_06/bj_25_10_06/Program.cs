15accda4c84f02c1a72e2d54efc0684c4941eb3d
bluecube97 <jph04071@gmail.com>
2025-10-06 18:58:08 +0900

    특별한 학교 탈출
    https://www.acmicpc.net/problem/31669

---
+++ bj_25_10_06/bj_25_10_06/Program.cs
@@ -0,0 +1,45 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        bool[][] isPatrol = new bool[n][];
+
+        for (int i = 0; i < n; i++)
+        {
+            string patrol = isr.ReadLine();
+            isPatrol[i] = new bool[m];
+            for (int j = 0; j < m; j++)
+            {
+                isPatrol[i][j] = patrol[j] == 'O';
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            bool canEscape = true;
+
+            for (int j = 0; j < n; j++)
+            {
+                if (isPatrol[j][i])
+                {
+                    canEscape = false;
+                    break;
+                }
+            }
+
+            if (canEscape)
+            {
+                Console.WriteLine(i + 1);
+                return;
+            }
+        }
+
+        Console.WriteLine("ESCAPE FAILED");
+    }
+}
