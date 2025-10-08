cd9d830a1dfd311965b2ebb96058a0c2265847b1
bluecube97 <jph04071@gmail.com>
2025-10-08 19:00:02 +0900

    코딩은 체육과목 입니다
    https://www.acmicpc.net/problem/25314

---
+++ bj_25_10_09/bj_25_10_09/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        int count = (n - 1) / 4 + 1;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("long ");
+        }
+        Console.WriteLine("int");
+    }
+}
