e50054e55ac42bd2403a3ee11409f01558068a5f
bluecube97 <jph04071@gmail.com>
2025-10-09 06:54:20 +0900

    브실이와 친구가 되고 싶어
    https://www.acmicpc.net/problem/29736

---
+++ bj_25_10_09_01/bj_25_10_09_01/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = ab[0];
+        int b = ab[1];
+
+        int[] kx = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int k = kx[0];
+        int x = kx[1];
+
+        int start = Math.Max(a, k - x);
+        int end = Math.Min(b, k + x);
+
+        int count = end - start + 1;
+
+        if (count <= 0)
+        {
+            Console.WriteLine("IMPOSSIBLE");
+        }
+        else
+        {
+            Console.WriteLine(count);
+        }
+    }
+}
