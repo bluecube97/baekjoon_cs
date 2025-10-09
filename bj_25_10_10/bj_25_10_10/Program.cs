63afa80782fc0ce61a8d63db6a8fb6b3e1d03ecc
bluecube97 <jph04071@gmail.com>
2025-10-10 07:10:51 +0900

    A+B - 10 (제1편)
    https://www.acmicpc.net/problem/30917

---
+++ bj_25_10_10/bj_25_10_10/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string nowQuestion = "A";
+        int a = 0;
+        int b = 0;
+
+        for (int i = 1; i < 10; i++)
+        {
+            Console.WriteLine("? {0} {1}", nowQuestion, i);
+            Console.Out.Flush();
+            int ans = int.Parse(isr.ReadLine());
+
+            if (ans == 1 && nowQuestion == "A")
+            {
+                nowQuestion = "B";
+                a = i;
+                i = 0;
+            }
+            else if (ans == 1 && nowQuestion == "B")
+            {
+                b = i;
+                break;
+            }
+        }
+
+        Console.WriteLine("! {0}", a + b);
+    }
+}
