eb318897b8fa406e44cebc0dd4aaf793f244db3f
bluecube97 <jph04071@gmail.com>
2025-10-12 17:41:45 +0900

    와우산 스탬프 투어
    https://www.acmicpc.net/problem/34543

---
+++ bj_25_10_12/bj_25_10_12/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int w = int.Parse(isr.ReadLine());
+
+        int score = n * 10;
+
+        if (n >= 3)
+        {
+            score += 20;
+        }
+
+        if (n == 5)
+        {
+            score += 50;
+        }
+
+        if (w > 1000)
+        {
+            score -= 15;
+        }
+
+        if (score < 0)
+        {
+            score = 0;
+        }
+
+        Console.WriteLine(score);
+    }
+}
