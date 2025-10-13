d73e5b219bb6731ce91d9bc173e0f903a896522f
bluecube97 <jph04071@gmail.com>
2025-10-13 20:58:52 +0900

    이상한 암호코드
    https://www.acmicpc.net/problem/18129

---
+++ bj_25_10_13/bj_25_10_13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string[] input = isr.ReadLine().Split().ToArray();
+        string s = input[0];
+        int k = int.Parse(input[1]);
+
+        s = s.ToLower();
+
+        StringBuilder result = new StringBuilder();
+        HashSet<char> used = new HashSet<char>();
+
+        int i = 0;
+        while (i < s.Length)
+        {
+            char current = s[i];
+            int count = 0;
+
+            while (i < s.Length && s[i] == current)
+            {
+                count++;
+                i++;
+            }
+
+            if (used.Contains(current))
+                continue;
+
+            if (count >= k)
+                result.Append('1');
+            else
+                result.Append('0');
+
+            used.Add(current);
+        }
+
+        Console.WriteLine(result.ToString());
+    }
+}
