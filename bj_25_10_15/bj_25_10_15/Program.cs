cd1f9b2b1d30c24d560bdd055bac6328e646b892
bluecube97 <jph04071@gmail.com>
2025-10-15 08:07:07 +0900

    CASIO
    https://www.acmicpc.net/problem/15963

---
+++ bj_25_10_15/bj_25_10_15/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        long[] nm = isr.ReadLine().Split().Select(long.Parse).ToArray();
+        long n = nm[0];
+        long m = nm[1];
+
+        Console.WriteLine(n == m ? 1 : 0);
+    }
+}
