22a7e6292be6441041b9404021630a5d8002791d
bluecube97 <jph04071@gmail.com>
2025-10-14 16:47:09 +0900

    알파벳 점수 계산기
    https://www.acmicpc.net/problem/34553

---
+++ bj_25_10_14/bj_25_10_14/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string s = isr.ReadLine();
+        int score = 0;
+        int curScore = 0;
+        char curChar = 'a';
+
+        foreach (char c in s)
+        {
+            if (c > curChar)
+            {
+                curScore++;
+                score += curScore;
+                curChar = c;
+            }
+            else
+            {
+                curScore = 1;
+                score += curScore;
+                curChar = c;
+            }
+        }
+
+        Console.WriteLine(score);
+    }
+}
