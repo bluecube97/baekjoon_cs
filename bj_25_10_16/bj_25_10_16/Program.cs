e91194ee06118daec459df37eec1cddcaf17e76c
bluecube97 <jph04071@gmail.com>
2025-10-16 08:28:19 +0900

    오늘의 날짜는?
    https://www.acmicpc.net/problem/16170

---
+++ bj_25_10_16/bj_25_10_16/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        Console.WriteLine(DateTime.Now.ToString("yyyy"));
+        Console.WriteLine(DateTime.Now.ToString("MM"));
+        Console.WriteLine(DateTime.Now.ToString("dd"));
+    }
+}
