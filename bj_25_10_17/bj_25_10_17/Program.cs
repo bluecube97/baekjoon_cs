9c44055f76de1be2cf2e3f630cdae15939821705
bluecube97 <jph04071@gmail.com>
2025-10-17 07:33:46 +0900

    Disposable Cup
    https://www.acmicpc.net/problem/13129

---
+++ bj_25_10_17/bj_25_10_17/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] abn = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = abn[0];
+        int b = abn[1];
+        int n = abn[2];
+
+        List<int> height = new List<int>();
+        int baseHeight = a * n;
+
+        for (int i = 1; i <= n; i++)
+        {
+            height.Add(baseHeight + b * i);
+        }
+
+        height.Sort();
+        Console.WriteLine(string.Join(" ", height));
+    }
+}
