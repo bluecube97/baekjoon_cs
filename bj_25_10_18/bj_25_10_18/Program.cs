5cf14ccbad590abb3c38b5804881e931a598e8e5
bluecube97 <jph04071@gmail.com>
2025-10-18 14:38:41 +0900

    스티커 나눠주기
    https://www.acmicpc.net/problem/33868

---
+++ bj_25_10_18/bj_25_10_18/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        int maxT = int.MinValue;
+        int minB = int.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] tb = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int t = tb[0];
+            int b = tb[1];
+
+            maxT = Math.Max(maxT, t);
+            minB = Math.Min(minB, b);
+        }
+
+        Console.WriteLine((maxT * minB) % 7 + 1);
+    }
+}
