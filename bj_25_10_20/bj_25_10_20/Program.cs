5eac9e8e6c6953db35e84c8cca09618837e57a77
bluecube97 <jph04071@gmail.com>
2025-10-20 15:00:47 +0900

    3대 512
    https://www.acmicpc.net/problem/33990

---
+++ bj_25_10_20/bj_25_10_20/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        int closest = int.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] iron = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = iron.Sum();
+            if (sum >= 512)
+            {
+                closest = Math.Min(closest, sum);
+            }
+        }
+
+        Console.WriteLine(closest == int.MaxValue ? -1 : closest);
+    }
+}
