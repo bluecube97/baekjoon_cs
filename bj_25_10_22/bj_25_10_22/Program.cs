6bf0c452447181c17f53cf717fdf905b3cdd0289
bluecube97 <jph04071@gmail.com>
2025-10-22 06:44:35 +0900

    뉴비의 기준은 뭘까?
    https://www.acmicpc.net/problem/19944

---
+++ bj_25_10_22/bj_25_10_22/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        if (m == 1 || m == 2)
+        {
+            Console.WriteLine("NEWBIE!");
+        }
+        else if (m <= n)
+        {
+            Console.WriteLine("OLDBIE!");
+        }
+        else
+        {
+            Console.WriteLine("TLE!");
+        }
+    }
+}
