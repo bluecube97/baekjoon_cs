ebc3d22047332e743e324d483e9727d7e65b0764
bluecube97 <jph04071@gmail.com>
2025-10-23 06:44:34 +0900

    이칙연산
    https://www.acmicpc.net/problem/15726

---
+++ bj_25_10_23/bj_25_10_23/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        double[] abc = isr.ReadLine().Split().Select(double.Parse).ToArray();
+        double a = abc[0];
+        double b = abc[1];
+        double c = abc[2];
+
+        double maxValue = double.MinValue;
+
+        maxValue = Math.Max(maxValue, a * b / c);
+        maxValue = Math.Max(maxValue, a / b * c);
+
+        Console.WriteLine((int)maxValue);
+    }
+}
