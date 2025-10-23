2df8c679efcb24e45f405df153012dfd20b5f08f
bluecube97 <jph04071@gmail.com>
2025-10-24 06:53:37 +0900

    최장 스트릭
    https://www.acmicpc.net/problem/29752

---
+++ bj_25_10_24/bj_25_10_24/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] s = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        int strig = 0;
+        int maxStrig = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] > 0)
+            {
+                strig++;
+                maxStrig = Math.Max(maxStrig, strig);
+            }
+            else
+            {
+                strig = 0;
+            }
+        }
+
+        Console.WriteLine(maxStrig);
+    }
+}
