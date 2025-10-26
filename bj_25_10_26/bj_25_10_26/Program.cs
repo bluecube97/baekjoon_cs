9b518a2e5a1df654573041774d24deb7814cef67
bluecube97 <jph04071@gmail.com>
2025-10-26 19:43:35 +0900

    디딤돌 장학금
    https://www.acmicpc.net/problem/34552

---
+++ bj_25_10_26/bj_25_10_26/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] m = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(isr.ReadLine());
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            double[] bls = isr.ReadLine().Split().Select(double.Parse).ToArray();
+            int b = (int)bls[0];
+            double l = bls[1];
+            int s = (int)bls[2];
+
+            if (l >= 2 && s >= 17)
+            {
+                sum += m[b];
+            }
+        }
+
+        Console.WriteLine(sum);
+    }
+}
