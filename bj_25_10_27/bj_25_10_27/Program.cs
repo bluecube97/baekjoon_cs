89e2e9be759576e78c8f507d1703c152b81c8c66
bluecube97 <jph04071@gmail.com>
2025-10-27 23:22:04 +0900

    ZOAC 2
    https://www.acmicpc.net/problem/18238

---
+++ bj_25_10_27/bj_25_10_27/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        int time = 0;
+        char current = 'A';
+
+        foreach (char target in input)
+        {
+            int curIndex = current - 'A';
+            int targetIndex = target - 'A';
+
+            int forward = (targetIndex - curIndex + 26) % 26;
+            int backward = (curIndex - targetIndex + 26) % 26;
+
+            time += Math.Min(forward, backward);
+            current = target;
+        }
+
+        Console.WriteLine(time);
+    }
+}
