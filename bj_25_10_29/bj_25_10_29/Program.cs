b75c19784dd44a1bb9100570a3f2ae490d9a8b5b
bluecube97 <jph04071@gmail.com>
2025-10-29 20:14:15 +0900

    민규의 서카디안 리듬
    https://www.acmicpc.net/problem/34400

---
+++ bj_25_10_29/bj_25_10_29/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int testCase = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < testCase; i++)
+        {
+            int t = int.Parse(isr.ReadLine());
+            int endTime = t % 25;
+            Console.WriteLine(endTime >= 17 ? "OFFLINE" : "ONLINE");
+        }
+    }
+}
