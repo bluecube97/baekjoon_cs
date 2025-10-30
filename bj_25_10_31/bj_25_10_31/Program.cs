e26d423b868a29eee468d854c4d48bd66b3e28de
bluecube97 <jph04071@gmail.com>
2025-10-31 06:57:18 +0900

    상수
    https://www.acmicpc.net/problem/2908

---
+++ bj_25_10_31/bj_25_10_31/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string[] input = isr.ReadLine().Split().ToArray();
+
+        string strA = input[0];
+        string strB = input[1];
+
+        int a = int.Parse(new string(strA.Reverse().ToArray()));
+        int b = int.Parse(new string(strB.Reverse().ToArray()));
+
+        Console.WriteLine(a > b ? a : b);
+    }
+}
