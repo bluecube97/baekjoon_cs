078d3bcb262f1b0f8b5a5f155b3ac876b4052252
bluecube97 <jph04071@gmail.com>
2025-11-02 21:37:46 +0900

    와글와글 숭고한
    https://www.acmicpc.net/problem/17388

---
+++ bj_25_11_02/bj_25_11_02/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] skh = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int s = skh[0];
+        int k = skh[1];
+        int h = skh[2];
+
+        if (s + k + h >= 100)
+        {
+            Console.WriteLine("OK");
+        }
+        else
+        {
+            int min = Math.Min(s, Math.Min(k, h));
+            if (min == s)
+            {
+                Console.WriteLine("Soongsil");
+            }
+            else if (min == k)
+            {
+                Console.WriteLine("Korea");
+            }
+            else
+            {
+                Console.WriteLine("Hanyang");
+            }
+        }
+    }
+}
