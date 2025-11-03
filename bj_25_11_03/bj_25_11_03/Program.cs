f763cf7b3129dae81c61b770f6a99627b254b819
bluecube97 <jph04071@gmail.com>
2025-11-04 00:03:29 +0900

    고무오리 디버깅
    https://www.acmicpc.net/problem/20001

---
+++ bj_25_11_03/bj_25_11_03/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        isr.ReadLine();
+        int problemCount = 0;
+
+        while (true)
+        {
+            string query = isr.ReadLine();
+            switch (query)
+            {
+                case "고무오리":
+                    if (problemCount == 0)
+                        problemCount += 2;
+                    else
+                        problemCount--;
+                    break;
+                case "문제":
+                    problemCount++;
+                    break;
+                case "고무오리 디버깅 끝":
+                    Console.WriteLine(problemCount == 0 ? "고무오리야 사랑해" : "힝구");
+                    return;
+            }
+        }
+    }
+}
