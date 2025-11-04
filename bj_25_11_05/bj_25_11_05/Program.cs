5be7aaf97e43c4833a2fb7ff14505f97e496bd40
bluecube97 <jph04071@gmail.com>
2025-11-05 00:48:54 +0900

    피자는 나눌 수록 커지잖아요
    https://www.acmicpc.net/problem/17946

---
+++ bj_25_11_05/bj_25_11_05/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            isr.ReadLine();
+            Console.WriteLine(1);
+        }
+    }
+}
