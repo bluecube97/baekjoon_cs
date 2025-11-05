02e2cfd0d8668b06c8492c3b9f89632cb2d36cba
bluecube97 <jph04071@gmail.com>
2025-11-06 07:23:29 +0900

    뉴턴과 사과
    https://www.acmicpc.net/problem/13118

---
+++ bj_25_11_06/bj_25_11_06/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] p = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] points = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int index = 1;
+
+        foreach (var person in p)
+        {
+            if (person == points[0])
+            {
+                Console.WriteLine(index);
+                return;
+            }
+            index++;
+        }
+
+        Console.WriteLine(0);
+    }
+}
