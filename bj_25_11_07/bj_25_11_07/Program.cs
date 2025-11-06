3deedd8a176db048ca02d0a02268af8f72eba594
bluecube97 <jph04071@gmail.com>
2025-11-07 08:29:02 +0900

    전체 계산 횟수
    https://www.acmicpc.net/problem/17174

---
+++ bj_25_11_07/bj_25_11_07/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] nm = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = nm[0];
+        int m = nm[1];
+
+        int count = 0;
+        int current = n;
+
+        while (current > 0)
+        {
+            count += current;
+            current /= m;
+        }
+
+        Console.WriteLine(count);
+    }
+}
