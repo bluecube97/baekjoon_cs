ca070c85746bd48d4a4a0fe70fa8d8b2b306ed34
bluecube97 <jph04071@gmail.com>
2025-11-10 08:38:10 +0900

    끝말잇기
    https://www.acmicpc.net/problem/20528

---
+++ bj_25_11_10/bj_25_11_10/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string[] s = isr.ReadLine().Split().ToArray();
+
+        char firstChar = s[0][0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i][0] != firstChar)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+        }
+
+        Console.WriteLine(1);
+    }
+}
