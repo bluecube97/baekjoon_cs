265c3b3b34aaed63886a3686eeb8f920d52fb1e5
bluecube97 <jph04071@gmail.com>
2025-11-09 12:40:00 +0900

    Since 1973
    https://www.acmicpc.net/problem/28135

---
+++ bj_25_11_09/bj_25_11_09/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int count = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (i.ToString().Contains("50"))
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count + n);
+    }
+}
