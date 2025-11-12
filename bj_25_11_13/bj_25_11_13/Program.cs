8a8fb3a708edee712e590dfdb59e4a13139dbb2f
bluecube97 <jph04071@gmail.com>
2025-11-13 07:43:54 +0900

    나머지
    https://www.acmicpc.net/problem/10430

---
+++ bj_25_11_13/bj_25_11_13/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = input[0];
+        int b = input[1];
+        int c = input[2];
+
+        Console.WriteLine((a + b) % c);
+        Console.WriteLine((a % c + b % c) % c);
+        Console.WriteLine(a * b % c);
+        Console.WriteLine(a % c * (b % c) % c);
+    }
+}
