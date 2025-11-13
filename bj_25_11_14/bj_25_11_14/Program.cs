5dd1ef77fa0eef537e363eddf6a073ec0afec8af
bluecube97 <jph04071@gmail.com>
2025-11-14 07:46:07 +0900

    .

---
+++ bj_25_11_14/bj_25_11_14/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int t1 = input[0];
+        int t2 = input[1];
+
+        Console.WriteLine(Math.Min(t1, t2));
+    }
+}
