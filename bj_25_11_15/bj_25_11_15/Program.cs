e0af1768fba56dd43119e898daf61b17893b27a5
bluecube97 <jph04071@gmail.com>
2025-11-15 08:15:47 +0900

    개표
    https://www.acmicpc.net/problem/30868

---
+++ bj_25_11_15/bj_25_11_15/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(isr.ReadLine());
+            int hor = n / 5;
+            int ver = n % 5;
+
+            for (int j = 0; j < hor; j++)
+            {
+                Console.Write("++++ ");
+            }
+
+            for (int j = 0; j < ver; j++)
+            {
+                Console.Write("|");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
