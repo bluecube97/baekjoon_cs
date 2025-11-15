c6b8a6f1db7dc892c586542f191c639ebbf3fa1e
bluecube97 <jph04071@gmail.com>
2025-11-16 06:55:17 +0900

    파일 완전 삭제
    https://www.acmicpc.net/problem/9243

---
+++ bj_25_11_16/bj_25_11_16/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string file = isr.ReadLine();
+        string result = isr.ReadLine();
+
+        if (n % 2 == 0 && file.Equals(result))
+        {
+            Console.WriteLine("Deletion succeeded");
+        }
+        else if (n % 2 == 0)
+        {
+            Console.WriteLine("Deletion failed");
+        }
+        else if (n % 2 == 1)
+        {
+            for (int i = 0; i < file.Length; i++)
+            {
+                if (file[i].Equals(result[i]))
+                {
+                    Console.WriteLine("Deletion failed");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Deletion succeeded");
+        }
+    }
+}
