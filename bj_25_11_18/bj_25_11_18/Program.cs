cad9575d3e64d28e203f6d6a1e5a014fb65cb3d0
bluecube97 <jph04071@gmail.com>
2025-11-18 23:54:20 +0900

    안밖? 밖안? 계단? 역계단?
    https://www.acmicpc.net/problem/28290

---
+++ bj_25_11_18/bj_25_11_18/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string s = isr.ReadLine();
+
+        switch (s)
+        {
+            case "fdsajkl;":
+                Console.WriteLine("in-out");
+                break;
+            case "jkl;fdsa":
+                Console.WriteLine("in-out");
+                break;
+            case "asdf;lkj":
+                Console.WriteLine("out-in");
+                break;
+            case ";lkjasdf":
+                Console.WriteLine("out-in");
+                break;
+            case "asdfjkl;":
+                Console.WriteLine("stairs");
+                break;
+            case ";lkjfdsa":
+                Console.WriteLine("reverse");
+                break;
+            default:
+                Console.WriteLine("molu");
+                break;
+        }
+    }
+}
