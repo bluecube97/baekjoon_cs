bad9a80fa015000b408ab8e64d8c3778d64fd7f1
bluecube97 <jph04071@gmail.com>
2025-11-20 07:56:09 +0900

    욱제는 건축왕이야!!
    https://www.acmicpc.net/problem/15923

---
+++ bj_25_11_20/bj_25_11_20/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int perimeter = 0;
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int firstX = input[0];
+        int firstY = input[1];
+        int prevX = firstX;
+        int prevY = firstY;
+
+        for (int i = 1; i < n; i++)
+        {
+            input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int x = input[0];
+            int y = input[1];
+
+            int distance = Math.Abs(x - prevX) + Math.Abs(y - prevY);
+            perimeter += distance;
+
+            prevX = x;
+            prevY = y;
+        }
+
+        perimeter += Math.Abs(firstX - prevX) + Math.Abs(firstY - prevY);
+
+        Console.WriteLine(perimeter);
+    }
+}
