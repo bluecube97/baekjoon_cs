e44d15d13721fb2a1c138aa0aaf2dec0aa402ee7
bluecube97 <jph04071@gmail.com>
2025-11-22 00:19:11 +0900

    갈래의 색종이 자르기
    https://www.acmicpc.net/problem/31472

---
+++ bj_25_11_22/bj_25_11_22/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int w = int.Parse(isr.ReadLine());
+
+        for (int i = 2; i < 500; i++)
+        {
+            if (i * i / 2 == w)
+            {
+                Console.WriteLine(i * 4);
+            }
+        }
+    }
+}
