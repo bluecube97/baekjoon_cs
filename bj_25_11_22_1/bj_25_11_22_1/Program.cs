00b2bae0daa7b866a6fe34e8d6b5a4931297ea09
bluecube97 <jph04071@gmail.com>
2025-11-22 06:12:32 +0900

    Tautogram
    https://www.acmicpc.net/problem/5698

---
+++ bj_25_11_22_1/bj_25_11_22_1/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        while (true)
+        {
+            string[] input = isr.ReadLine().Split().ToArray();
+            if (input[0].Equals("*")) break;
+            bool isTrue = true;
+
+            char first = char.ToUpper(input[0][0]);
+
+            foreach (var s in input)
+            {
+                if (!char.ToUpper(s[0]).Equals(first))
+                {
+                    Console.WriteLine("N");
+                    isTrue = false;
+                    break;
+                }
+            }
+
+            if (isTrue) Console.WriteLine("Y");
+        }
+    }
+}
