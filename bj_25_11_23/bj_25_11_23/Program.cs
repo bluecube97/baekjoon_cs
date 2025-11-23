a01507218781b18e1c0bd89797cae9b1583634fd
bluecube97 <jph04071@gmail.com>
2025-11-23 10:38:13 +0900

    2033년 밈 투표
    https://www.acmicpc.net/problem/29731

---
+++ bj_25_11_23/bj_25_11_23/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        List<string> promise = new List<string>
+        {
+            "Never gonna give you up", "Never gonna let you down", "Never gonna run around and desert you",
+            "Never gonna make you cry", "Never gonna say goodbye", "Never gonna tell a lie and hurt you",
+            "Never gonna stop"
+        };
+
+        bool isHacked = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            string s = isr.ReadLine();
+
+            if (!promise.Contains(s))
+            {
+                isHacked = true;
+            }
+        }
+
+        Console.WriteLine(isHacked ? "Yes" : "No");
+    }
+}
