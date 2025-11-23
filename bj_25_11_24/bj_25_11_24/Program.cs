32752d9a928cb2ef58ee7fd53f969ea6259a0a1e
bluecube97 <jph04071@gmail.com>
2025-11-24 06:51:43 +0900

    86 ─에이티식스─ 2
    https://www.acmicpc.net/problem/25595

---
+++ bj_25_11_24/bj_25_11_24/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string shinei = "";
+        List<string> region = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int j = 0; j < n; j++)
+            {
+                if (input[j] == 1)
+                {
+                    region.Add((i + j) % 2 == 0 ? "W" : "B");
+                }
+                else if (input[j] == 2)
+                {
+                    shinei = (i + j) % 2 == 0 ? "W" : "B";
+                }
+            }
+        }
+
+        if (region.Contains(shinei))
+        {
+            Console.WriteLine("Kiriya");
+        }
+        else
+        {
+            Console.WriteLine("Lena");
+        }
+    }
+}
