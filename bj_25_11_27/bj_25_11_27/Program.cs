77b7539ede9ba5b50616fb94d0708cbc18abc9d2
bluecube97 <jph04071@gmail.com>
2025-11-27 07:21:48 +0900

    레퓨닛의 덧셈
    https://www.acmicpc.net/problem/33964

---
+++ bj_25_11_27/bj_25_11_27/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] xy = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int x = xy[0];
+        int y = xy[1];
+
+        string a = "", b = "";
+
+        for (int i = 0; i < x; i++)
+        {
+            a += "1";
+        }
+        for (int i = 0; i < y; i++)
+        {
+            b += "1";
+        }
+
+        Console.WriteLine(int.Parse(a) + int.Parse(b));
+    }
+}
