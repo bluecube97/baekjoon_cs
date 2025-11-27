c90a07b998c7a013021e8ecf2c6e9b5762656459
bluecube97 <jph04071@gmail.com>
2025-11-28 06:44:06 +0900

    완전 제곱수
    https://www.acmicpc.net/problem/6131

---
+++ bj_25_11_28/bj_25_11_28/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int count = 0;
+
+        for (int x = 1; x * x <= n; x++)
+        {
+            if (n % x != 0) continue;
+
+            int y = n / x;
+
+            if ((x + y) % 2 != 0) continue;
+
+            int a = (x + y) / 2;
+            int b = (y - x) / 2;
+
+            if (b >= 1 && a >= b && a <= 500) count++;
+        }
+
+        Console.WriteLine(count);
+    }
+}
