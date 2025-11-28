7b2a1485fe3eec9742082bb25b505b503b1a0aab
bluecube97 <jph04071@gmail.com>
2025-11-29 08:10:14 +0900

    쿠키애호가
    https://www.acmicpc.net/problem/11134

---
+++ bj_25_11_29/bj_25_11_29/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            double[] nc = isr.ReadLine().Split().Select(double.Parse).ToArray();
+            double n = nc[0];
+            double c = nc[1];
+
+            Console.WriteLine(Math.Ceiling(n / c));
+        }
+    }
+}
