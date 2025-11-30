a304f110d1a78ce47c3644e4609a95b64264881f
bluecube97 <jph04071@gmail.com>
2025-11-30 19:29:06 +0900

    서울사이버대학을 다니고
    https://www.acmicpc.net/problem/30958

---
+++ bj_25_11_30/bj_25_11_30/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        string s = isr.ReadLine();
+        int[] alpha = new int[26];
+
+        foreach (var c in s)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                alpha[c - 'a']++;
+            }
+        }
+
+        Console.WriteLine(alpha.Max());
+    }
+}
