4eda11515480ac2ea399003a51a66a9a85df9110
bluecube97 <jph04071@gmail.com>
2025-12-01 22:54:12 +0900

    세로읽기
    https://www.acmicpc.net/problem/10798

---
+++ bj_25_12_01/bj_25_12_01/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        List<List<char>> board = new List<List<char>>();
+
+        for (int i = 0; i < 5; i++)
+        {
+            var line = isr.ReadLine();
+            board.Add(line.ToList());
+        }
+
+        for (int col = 0; col < 15; col++)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                if (col < board[row].Count)
+                {
+                    Console.Write(board[row][col]);
+                }
+            }
+        }
+    }
+}
