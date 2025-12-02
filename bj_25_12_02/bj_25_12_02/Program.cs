04d8ae185e56d922ba0b00d2719f94e4d6fba534
bluecube97 <jph04071@gmail.com>
2025-12-03 01:36:50 +0900

    전화 요금
    https://www.acmicpc.net/problem/3226

---
+++ bj_25_12_02/bj_25_12_02/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int totalCost = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            string info = isr.ReadLine();
+            string[] call = info.Split();
+            string[] time = call[0].Split(':');
+
+            int hour = int.Parse(time[0]);
+            int minute = int.Parse(time[1]);
+            int duration = int.Parse(call[1]);
+
+            for (int d = 0; d < duration; d++)
+            {
+                if (hour >= 7 && hour < 19)
+                    totalCost += 10;
+                else
+                    totalCost += 5;
+
+                minute++;
+
+                if (minute == 60)
+                {
+                    minute = 0;
+                    hour++;
+                    if (hour == 24)
+                        hour = 0;
+                }
+            }
+        }
+
+        Console.WriteLine(totalCost);
+    }
+}
