085f200380a3116dc7c21cfa8ae477b1b5ea6d19
bluecube97 <jph04071@gmail.com>
2025-12-03 16:23:44 +0900

    DORO
    https://www.acmicpc.net/problem/34073

---
+++ bj_25_12_03/bj_25_12_03/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string[] words = isr.ReadLine().Split();
+
+        foreach (var s in words)
+        {
+            Console.Write(s + "DORO ");
+        }
+    }
+}
