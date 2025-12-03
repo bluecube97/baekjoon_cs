94aa10e48c700c02e3d8fafec8e430264e78e80f
bluecube97 <jph04071@gmail.com>
2025-12-04 07:59:05 +0900

    행복
    https://www.acmicpc.net/problem/15969

---
+++ bj_25_12_04/bj_25_12_04/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        int[] score = isr.ReadLine().Split().Select(int.Parse).ToArray();
+
+        Console.WriteLine(score.Max() - score.Min());
+    }
+}
