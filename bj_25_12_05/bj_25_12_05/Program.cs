714dc6728bde6b5d9423703cf55bc48b87df42d5
bluecube97 <jph04071@gmail.com>
2025-12-05 07:12:44 +0900

    첨탑 밀어서 부수기
    https://www.acmicpc.net/problem/28014

---
+++ bj_25_12_05/bj_25_12_05/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string[] tower = isr.ReadLine().Split();
+
+        int pushCount = 1;
+        int prevHeight = int.Parse(tower[0]);
+
+        for (int i = 1; i < n; i++)
+        {
+            int cur = int.Parse(tower[i]);
+
+            if (prevHeight <= cur)
+                pushCount++;
+
+            prevHeight = cur;
+        }
+
+        Console.WriteLine(pushCount);
+    }
+}
