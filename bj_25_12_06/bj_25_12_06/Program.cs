eaa49332ea71bc9bbee30be7e15825cab11a8506
bluecube97 <jph04071@gmail.com>
2025-12-06 23:10:33 +0900

    창영이의 일기장
    https://www.acmicpc.net/problem/2954

---
+++ bj_25_12_06/bj_25_12_06/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string line = isr.ReadLine();
+        string vowels = "aeiou";
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            Console.Write(line[i]);
+
+            if (vowels.Contains(line[i]))
+            {
+                i += 2;
+            }
+        }
+    }
+}
