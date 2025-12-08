cf9bb4c329c68982f185f584250310f2c19ce2bc
bluecube97 <jph04071@gmail.com>
2025-12-08 22:43:21 +0900

    여름나기
    https://www.acmicpc.net/problem/18130

---
+++ bj_25_12_08/bj_25_12_08/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        long[] nq = isr.ReadLine().Split().Select(long.Parse).ToArray();
+        long n = nq[0];
+        long q = nq[1];
+        long minPrice = long.MaxValue;
+        int index = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            long[] pkc = isr.ReadLine().Split().Select(long.Parse).ToArray();
+            long p = pkc[0];
+            long k = pkc[1];
+            long c = pkc[2];
+
+            long T = (q - 1) / k;
+            long extra = c * (T * (T + 1) / 2);
+            long price = p + extra;
+
+            if (price < minPrice)
+            {
+                minPrice = price;
+                index = i + 1;
+            }
+        }
+
+        Console.WriteLine("{0} {1}", index, minPrice);
+    }
+}
