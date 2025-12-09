72fb50b9af819e9138e41d854bbc5a388ae3ae25
bluecube97 <jph04071@gmail.com>
2025-12-09 22:18:42 +0900

    solved.ac
    https://www.acmicpc.net/problem/18110

---
+++ bj_25_12_09/bj_25_12_09/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        if (n == 0)
+        {
+            Console.WriteLine(0);
+            return;
+        }
+
+        int[] scoreArr = new int[n];
+        int cut = (int)Math.Round(n * 0.15, MidpointRounding.AwayFromZero);
+
+        for (int i = 0; i < n; i++)
+        {
+            scoreArr[i] = int.Parse(isr.ReadLine());
+        }
+
+        Array.Sort(scoreArr);
+        int sum = 0;
+
+        for (int i = cut; i < n - cut; i++)
+        {
+            sum += scoreArr[i];
+        }
+
+        double average = (double)sum / (n - cut * 2);
+        Console.WriteLine((int)Math.Round(average, MidpointRounding.AwayFromZero));
+    }
+}
