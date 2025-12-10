5895b53648032081236a871eed184a40ae67387b
bluecube97 <jph04071@gmail.com>
2025-12-11 06:52:51 +0900

    SUAPC 의자 준비하기
    https://www.acmicpc.net/problem/34183

---
+++ bj_25_12_11/bj_25_12_11/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] inputs = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = inputs[0];
+        int m = inputs[1];
+        int a = inputs[2];
+        int b = inputs[3];
+
+        int cost = (n * 3 - m) * a + b;
+        Console.WriteLine(n * 3 - m <= 0 ? 0 : cost);
+    }
+}
