e7634b549273c5010aa8fb905191d17f4abf8563
bluecube97 <jph04071@gmail.com>
2025-12-12 07:04:15 +0900

    정육각형과 삼각형
    https://www.acmicpc.net/problem/14264

---
+++ bj_25_12_12/bj_25_12_12/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int l = int.Parse(isr.ReadLine());
+
+        double area = (Math.Sqrt(3) / 4) * l * l;
+
+        Console.WriteLine($"{area:0.#########}");
+    }
+}
