824c78773039f3102886a526073bedfb8ba146dd
bluecube97 <jph04071@gmail.com>
2025-12-17 20:27:41 +0900

    WARBOY
    https://www.acmicpc.net/problem/26082

---
+++ bj_25_12_17/bj_25_12_17/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] abc = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = abc[0];
+        int b = abc[1];
+        int c = abc[2];
+
+        Console.WriteLine(b * c * 3 / a);
+    }
+}
