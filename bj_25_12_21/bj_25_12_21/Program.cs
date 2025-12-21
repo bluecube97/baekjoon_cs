13c9a687cf7ea98e39289615e588afc99950b8f8
bluecube97 <jph04071@gmail.com>
2025-12-21 20:23:38 +0900

    큰 수 A+B
    https://www.acmicpc.net/problem/10757

---
+++ bj_25_12_21/bj_25_12_21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        string[] input = isr.ReadLine().Split().ToArray();
+        BigInteger a = BigInteger.Parse(input[0]);
+        BigInteger b = BigInteger.Parse(input[1]);
+
+        BigInteger result = a + b;
+        Console.WriteLine(result);
+    }
+}
