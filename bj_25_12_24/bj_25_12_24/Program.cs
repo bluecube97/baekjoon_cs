82ecc46acc09c26449503d30d427478f720971f0
bluecube97 <jph04071@gmail.com>
2025-12-24 07:49:33 +0900

    그게 무슨 코드니..
    https://www.acmicpc.net/problem/31495

---
+++ bj_25_12_24/bj_25_12_24/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+        string s = isr.ReadLine();
+
+        if (s.Length < 2)
+        {
+            Console.WriteLine("CE");
+            return;
+        }
+
+        if (s[0] == '"' && s[^1] == '"')
+        {
+            string inner = s.Substring(1, s.Length - 2);
+
+            if (inner.Length == 0)
+            {
+                Console.WriteLine("CE");
+            }
+            else
+            {
+                Console.WriteLine(inner);
+            }
+        }
+        else
+        {
+            Console.WriteLine("CE");
+        }
+    }
+}
