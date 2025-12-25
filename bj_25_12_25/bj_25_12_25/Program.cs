830885399068ae16a35e004877b5e87b885fbb7c
bluecube97 <jph04071@gmail.com>
2025-12-25 09:06:38 +0900

    접시 안의 원
    https://www.acmicpc.net/problem/16483

---
+++ bj_25_12_25/bj_25_12_25/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+        int answer = (int)Math.Round((t * 1.0 / 2) * (t * 1.0 / 2), MidpointRounding.AwayFromZero);
+
+        Console.WriteLine(answer);
+    }
+}
