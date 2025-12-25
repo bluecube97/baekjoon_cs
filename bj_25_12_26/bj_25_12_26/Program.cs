5411cd4a6e63f952b9bc924ebd57b4865af4a909
bluecube97 <jph04071@gmail.com>
2025-12-26 07:20:23 +0900

    문자열을 만들어요
    https://www.acmicpc.net/problem/32297

---
+++ bj_25_12_26/bj_25_12_26/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string s = isr.ReadLine();
+
+        if (s.Contains("gori"))
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
