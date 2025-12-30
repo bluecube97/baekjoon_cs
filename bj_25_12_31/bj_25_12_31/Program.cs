345dc96eb6686961ff052efd48714a863a12fbe1
bluecube97 <jph04071@gmail.com>
2025-12-31 07:25:52 +0900

    YCPC 점수
    https://www.acmicpc.net/problem/34823

---
+++ bj_25_12_31/bj_25_12_31/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] ycp = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int y = ycp[0] * 2;
+        int c = ycp[1];
+        int p = ycp[2] * 2;
+
+        int min = Math.Min(y, Math.Min(c, p));
+
+        Console.WriteLine(min / 2);
+    }
+}
