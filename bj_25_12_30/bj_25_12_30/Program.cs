dde18eccddafc45389dac27a18b995c0edb0d4aa
bluecube97 <jph04071@gmail.com>
2025-12-30 20:14:09 +0900

    가뭄(Small)
    https://www.acmicpc.net/problem/19572

---
+++ bj_25_12_30/bj_25_12_30/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        double[] d = isr.ReadLine().Split().Select(double.Parse).ToArray();
+        double d1 = d[0];
+        double d2 = d[1];
+        double d3 = d[2];
+
+        double a = (d1 + d2 - d3) / 2.0;
+        double b = (d1 + d3 - d2) / 2.0;
+        double c = (d2 + d3 - d1) / 2.0;
+
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            Console.WriteLine("-1");
+            return;
+        }
+
+        Console.WriteLine("1");
+        Console.WriteLine(
+            $"{Math.Round(a, 1):F1} {Math.Round(b, 1):F1} {Math.Round(c, 1):F1}"
+        );
+    }
+}
