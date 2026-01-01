d8a151e88fa39e872a1e8c8f6179582ca287ab3e
bluecube97 <jph04071@gmail.com>
2026-01-02 07:28:19 +0900

    대표값
    https://www.acmicpc.net/problem/2592

---
+++ bj_26_01_02/bj_26_01_02/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] count = new int[100];
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            int num = int.Parse(isr.ReadLine());
+            count[num / 10]++;
+            sum += num;
+        }
+
+        (int, int) mode = (0, 0);
+        for (int i = 0; i < count.Length; i++)
+        {
+            if (count[i] > mode.Item2)
+            {
+                mode = (i, count[i]);
+            }
+        }
+
+        Console.WriteLine($"{sum / 10}\n{mode.Item1 * 10}");
+    }
+}
