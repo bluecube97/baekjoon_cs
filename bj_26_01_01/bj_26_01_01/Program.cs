c2020838305ebb7c706320c9cea7db01253bc64a
bluecube97 <jph04071@gmail.com>
2026-01-01 21:57:21 +0900

    와우와 쿼리
    https://www.acmicpc.net/problem/32342

---
+++ bj_26_01_01/bj_26_01_01/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int q = int.Parse(isr.ReadLine());
+        for (int i = 0; i < q; i++)
+        {
+            string s = isr.ReadLine();
+            int cnt = 0;
+
+            for (int j = 0; j < s.Length - 2; j++)
+            {
+                if (s[j].Equals('W') && s[j + 1].Equals('O') && s[j + 2].Equals('W'))
+                {
+                    cnt++;
+                }
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
