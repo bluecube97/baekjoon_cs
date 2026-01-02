2c21376bed9c4267f5ccce1b0a05f691e2077f6d
bluecube97 <jph04071@gmail.com>
2026-01-03 07:50:41 +0900

    체스
    https://www.acmicpc.net/problem/17122

---
+++ bj_26_01_03/bj_26_01_03/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string[] input = isr.ReadLine().Split();
+            string s = input[0];
+            int n = int.Parse(input[1]);
+
+            int col1 = s[0] - 'A' + 1;
+            int row1 = s[1] - '0';
+
+            int row2 = (n - 1) / 8 + 1;
+            int col2 = (n - 1) % 8 + 1;
+
+            bool color1 = (row1 + col1) % 2 == 0;
+            bool color2 = (row2 + col2) % 2 == 0;
+
+            Console.WriteLine(color1 == color2 ? "YES" : "NO");
+        }
+    }
+}
