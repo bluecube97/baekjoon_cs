2a2e85942d3cdab1f696b26639b5aa02c59ed356
bluecube97 <jph04071@gmail.com>
2026-01-04 19:52:21 +0900

    No Brainer
    https://www.acmicpc.net/problem/4562

---
+++ bj_26_01_04/bj_26_01_04/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int a = input[0];
+            int b = input[1];
+
+            Console.WriteLine(a < b ? "NO BRAINS" : "MMM BRAINS");
+        }
+    }
+}
