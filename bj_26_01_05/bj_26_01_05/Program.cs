4d2c57d136a247ee4a231a51d62603fcceaa5a4e
bluecube97 <jph04071@gmail.com>
2026-01-05 22:18:04 +0900

    Next in line
    https://www.acmicpc.net/problem/6749

---
+++ bj_26_01_05/bj_26_01_05/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int a = int.Parse(isr.ReadLine());
+        int b = int.Parse(isr.ReadLine());
+
+        Console.WriteLine(b + b - a);
+    }
+}
