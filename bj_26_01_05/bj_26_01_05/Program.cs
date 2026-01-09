eb8ae56db878477cf78b8a321387f0c9195d432f
bluecube97 <jph04071@gmail.com>
2026-01-10 06:32:50 +0900

    폰 노이만
    https://www.acmicpc.net/problem/9469

--- bj_26_01_05/bj_26_01_05/Program.cs
+++ bj_26_01_05/bj_26_01_05/Program.cs
@@ -1,16 +1,25 @@
-﻿using System.Numerics;
-
-internal class Program
+﻿internal class Program
 {
     static void Main()
     {
         var isr = new StreamReader(Console.OpenStandardInput());
 
-        string[] input = isr.ReadLine().Split().ToArray();
-        BigInteger a = BigInteger.Parse(input[0]);
-        BigInteger b = BigInteger.Parse(input[1]);
+        int p = int.Parse(isr.ReadLine());
 
-        BigInteger result = a * b;
-        Console.WriteLine(result);
+        for (int i = 0; i < p; i++)
+        {
+            string[] input = isr.ReadLine().Split().ToArray();
+            int n = int.Parse(input[0]);
+            double d = double.Parse(input[1]);  // 철로의 길이
+            double a = double.Parse(input[2]);  // a기차의 속도
+            double b = double.Parse(input[3]);  // b기차의 속도
+            double f = double.Parse(input[4]);  // 파리의 속도
+
+            // a기차와 b기차가 충돌하는 데 걸리는 시간
+            double time = d / (a + b);
+            // 파리가 이동한 거리
+            double distance = f * time;
+            Console.WriteLine($"{n} {distance:F6}");
+        }
     }
 }
