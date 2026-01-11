3ec8f65af26dbc908e27d21092b4a7648c3d5a9a
bluecube97 <jph04071@gmail.com>
2026-01-11 19:14:05 +0900

    그릇
    https://www.acmicpc.net/problem/7567

---
+++ bj_26_01_11/bj_26_01_11/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int height = 5;
+        string bowls = isr.ReadLine();
+        char prev = bowls[0];
+
+        foreach (var bowl in bowls)
+        {
+            if (bowl != prev)
+            {
+                height += 5;
+                prev = bowl;
+            }
+            height += 5;
+        }
+
+        Console.WriteLine(height);
+    }
+}
