4fd2c3d092dbe1cb383a2c221f2d3575ec1835d0
bluecube97 <jph04071@gmail.com>
2026-01-13 08:33:57 +0900

    Telemarketer or not?
    https://www.acmicpc.net/problem/16017

---
+++ bj_26_01_12/bj_26_01_12/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int a = int.Parse(isr.ReadLine());
+        int b = int.Parse(isr.ReadLine());
+        int c = int.Parse(isr.ReadLine());
+        int d = int.Parse(isr.ReadLine());
+
+        if ((a == 8 || a == 9) && (d == 8 || d == 9) && b == c)
+        {
+            Console.WriteLine("ignore");
+        }
+        else
+        {
+            Console.WriteLine("answer");
+        }
+    }
+}
