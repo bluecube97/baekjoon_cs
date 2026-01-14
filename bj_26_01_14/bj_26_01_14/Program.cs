97ce6b340ed1cdfd44f1c3ec655380e0a5d5705e
bluecube97 <jph04071@gmail.com>
2026-01-14 11:03:17 +0900

    Fan Death
    https://www.acmicpc.net/problem/15633

---
+++ bj_26_01_14/bj_26_01_14/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        List<int> list = new List<int>();
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                list.Add(i);
+            }
+        }
+
+        int sum = list.Sum();
+        Console.WriteLine(sum * 5 - 24);
+    }
+}
