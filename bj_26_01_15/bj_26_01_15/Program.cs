10feabd24da6f4bacd40bf7aeb293b785f70e9b8
bluecube97 <jph04071@gmail.com>
2026-01-15 10:25:53 +0900

    덧셈과 곱셈
    https://www.acmicpc.net/problem/14579

---
+++ bj_26_01_15/bj_26_01_15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] ab = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = ab[0];
+        int b = ab[1];
+
+        BigInteger result = 1;
+
+        for (int i = a; i <= b; i++)
+        {
+            result = BigInteger.Multiply(result, i * (i + 1) / 2);
+        }
+
+        Console.WriteLine(BigInteger.Remainder(result, 14579));
+    }
+}
