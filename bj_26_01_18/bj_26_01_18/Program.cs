f7224b99536181734cd28595c415dd9064658b11
bluecube97 <jph04071@gmail.com>
2026-01-24 08:00:14 +0900

    인생은 한 방
    https://www.acmicpc.net/problem/25183

---
+++ bj_26_01_18/bj_26_01_18/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+        string s = isr.ReadLine();
+        
+        int count = 1;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (Math.Abs(s[i] - s[i + 1]) == 1)
+            {
+                count++;
+                if (count >= 5)
+                {
+                    Console.WriteLine("YES");
+                    return;
+                }
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+
+        Console.WriteLine("NO");
+    }
+}
