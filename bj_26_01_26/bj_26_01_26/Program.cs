8158d591ee06a46c48d1a0887a34cc81b0498f96
bluecube97 <jph04071@gmail.com>
2026-02-06 07:21:47 +0900

    Knob
    https://www.acmicpc.net/problem/30617

---
+++ bj_26_01_26/bj_26_01_26/Program.cs
@@ -0,0 +1,46 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int t = int.Parse(isr.ReadLine());
+        int funny = 0;
+
+        int[] init = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        int prevL = init[0];
+        int prevR = init[1];
+
+        if (prevL == prevR && prevL != 0)
+        {
+            funny++;
+        }
+
+        for (int i = 1; i < t; i++)
+        {
+            int[] lr = isr.ReadLine().Split().Select(int.Parse).ToArray();
+            int l = lr[0];
+            int r = lr[1];
+
+            if (prevL == l && l != 0)
+            {
+                funny++;
+            }
+
+            if (prevR == r && r != 0)
+            {
+                funny++;
+            }
+
+            if (l == r && l != 0)
+            {
+                funny++;
+            }
+
+            prevL = l;
+            prevR = r;
+        }
+
+        Console.WriteLine(funny);
+    }
+}
