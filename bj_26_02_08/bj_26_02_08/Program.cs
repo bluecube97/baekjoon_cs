68c1a849f2a13c632e131676408a757d09cda5db
bluecube97 <jph04071@gmail.com>
2026-02-09 06:48:16 +0900

    공사장 표지판
    https://www.acmicpc.net/problem/23055

---
+++ bj_26_02_08/bj_26_02_08/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        if (n == 1)
+        {
+            Console.Write("*");
+            return;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("*");
+        }
+
+        Console.WriteLine();
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            Console.Write("*");
+
+            for (int j = 0; j < n - 2; j++)
+            {
+                if (i == j || j == (n - 3 - i))
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.WriteLine("*");
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("*");
+        }
+
+        Console.WriteLine();
+    }
+}
