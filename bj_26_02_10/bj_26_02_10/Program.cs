0927dd70c621d102d4e74d08d9db99cd9eced680
bluecube97 <jph04071@gmail.com>
2026-02-11 09:07:01 +0900

    모르고리즘 회장 정하기
    https://www.acmicpc.net/problem/30684

---
+++ bj_26_02_10/bj_26_02_10/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int n = int.Parse(isr.ReadLine());
+
+        string prev = string.Empty;
+
+        while (true)
+        {
+            prev = isr.ReadLine();
+            if (prev.Length == 3)
+                break;
+            n--;
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            string name = isr.ReadLine();
+            if (name.Length != 3)
+                continue;
+
+            if (string.Compare(prev, name) > 0)
+            {
+                prev = name;
+            }
+        }
+
+        Console.WriteLine(prev);
+    }
+}
