9fc23dfc00d3e3cf3b277f6faff0744d71dddf55
bluecube97 <jph04071@gmail.com>
2026-02-18 12:11:20 +0900

    베시와 데이지
    https://www.acmicpc.net/problem/16431

---
+++ bj_26_02_11/bj_26_02_11/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        int[] input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        (int, int) bessie = (input[0], input[1]);
+        input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        (int, int) daisy = (input[0], input[1]);
+        input = isr.ReadLine().Split().Select(int.Parse).ToArray();
+        (int, int) target = (input[0], input[1]);
+
+        int bessieToTarget = Math.Max(Math.Abs(bessie.Item1 - target.Item1), Math.Abs(bessie.Item2 - target.Item2));
+        int daisyToTarget = Math.Abs(daisy.Item1 - target.Item1) + Math.Abs(daisy.Item2 - target.Item2);
+
+        if (bessieToTarget < daisyToTarget)
+        {
+            Console.WriteLine("bessie");
+        }
+        else if (bessieToTarget > daisyToTarget)
+        {
+            Console.WriteLine("daisy");
+        }
+        else
+        {
+            Console.WriteLine("tie");
+        }
+    }
+}
