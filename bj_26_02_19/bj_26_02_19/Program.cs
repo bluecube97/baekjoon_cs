60186ad584a87b0ef8484768b92ae40f18734032
bluecube97 <jph04071@gmail.com>
2026-02-19 12:29:24 +0900

    백발백준하는 명사수
    https://www.acmicpc.net/problem/22938

---
+++ bj_26_02_19/bj_26_02_19/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main()
+    {
+        var isr = new StreamReader(Console.OpenStandardInput());
+
+        long[] input = isr.ReadLine().Split().Select(long.Parse).ToArray();
+        long x1 = input[0];
+        long y1 = input[1];
+        long r1 = input[2];
+
+        input = isr.ReadLine().Split().Select(long.Parse).ToArray();
+        long x2 = input[0];
+        long y2 = input[1];
+        long r2 = input[2];
+
+        long distance = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+
+        Console.WriteLine(distance >= (r2 + r1) * (r2 + r1) ? "NO" : "YES");
+    }
+}
